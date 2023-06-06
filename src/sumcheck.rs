e40a6e34f8b3a97cab0cca4bcd1097511361f69f
sam <sammage@gmail.com>
2023-06-06 12:32:31 -0700

    Clean up sumcheck.rs; document

--- src/sumcheck.rs
+++ src/sumcheck.rs
@@ -14,176 +14,6 @@ use ark_std::{One, Zero};
 
 use merlin::Transcript;
 
-#[derive(CanonicalSerialize, CanonicalDeserialize, Debug)]
-pub struct SumcheckInstanceProof<F: PrimeField> {
-  compressed_polys: Vec<CompressedUniPoly<F>>,
-}
-
-impl<F: PrimeField> SumcheckInstanceProof<F> {
-  pub fn new(compressed_polys: Vec<CompressedUniPoly<F>>) -> SumcheckInstanceProof<F> {
-    SumcheckInstanceProof { compressed_polys }
-  }
-
-  pub fn verify<G, T: ProofTranscript<G>>(
-    &self,
-    claim: F,
-    num_rounds: usize,
-    degree_bound: usize,
-    transcript: &mut T,
-  ) -> Result<(F, Vec<F>), ProofVerifyError>
-  where
-    G: CurveGroup<ScalarField = F>,
-  {
-    let mut e = claim;
-    let mut r: Vec<F> = Vec::new();
-
-    // verify that there is a univariate polynomial for each round
-    assert_eq!(self.compressed_polys.len(), num_rounds);
-    for i in 0..self.compressed_polys.len() {
-      let poly = self.compressed_polys[i].decompress(&e);
-
-      // verify degree bound
-      assert_eq!(poly.degree(), degree_bound);
-
-      // check if G_k(0) + G_k(1) = e
-      assert_eq!(poly.eval_at_zero() + poly.eval_at_one(), e);
-
-      // append the prover's message to the transcript
-      <UniPoly<F> as AppendToTranscript<G>>::append_to_transcript(&poly, b"poly", transcript);
-
-      //derive the verifier's challenge for the next round
-      let r_i = transcript.challenge_scalar(b"challenge_nextround");
-
-      r.push(r_i);
-
-      // evaluate the claimed degree-ell polynomial at r_i
-      e = poly.evaluate(&r_i);
-    }
-
-    // TODO: Doesn't execute the final check against g_v(r_v) = oracle_g(r)
-
-    Ok((e, r))
-  }
-}
-
-#[derive(CanonicalSerialize, CanonicalDeserialize, Debug)]
-pub struct ZKSumcheckInstanceProof<G: CurveGroup> {
-  comm_polys: Vec<G>,
-  comm_evals: Vec<G>,
-  proofs: Vec<DotProductProof<G>>,
-}
-
-impl<G: CurveGroup> ZKSumcheckInstanceProof<G> {
-  pub fn new(comm_polys: Vec<G>, comm_evals: Vec<G>, proofs: Vec<DotProductProof<G>>) -> Self {
-    ZKSumcheckInstanceProof {
-      comm_polys,
-      comm_evals,
-      proofs,
-    }
-  }
-
-  pub fn verify(
-    &self,
-    comm_claim: &G,
-    num_rounds: usize,
-    degree_bound: usize,
-    gens_1: &MultiCommitGens<G>,
-    gens_n: &MultiCommitGens<G>,
-    transcript: &mut Transcript,
-  ) -> Result<(G, Vec<G::ScalarField>), ProofVerifyError> {
-    // verify degree bound
-    assert_eq!(gens_n.n, degree_bound + 1);
-
-    // verify that there is a univariate polynomial for each round
-    assert_eq!(self.comm_polys.len(), num_rounds);
-    assert_eq!(self.comm_evals.len(), num_rounds);
-
-    let mut r: Vec<G::ScalarField> = Vec::new();
-    for i in 0..self.comm_polys.len() {
-      let comm_poly = &self.comm_polys[i];
-
-      // append the prover's polynomial to the transcript
-      <Transcript as ProofTranscript<G>>::append_point(transcript, b"comm_poly", comm_poly);
-
-      //derive the verifier's challenge for the next round
-      let r_i =
-        <Transcript as ProofTranscript<G>>::challenge_scalar(transcript, b"challenge_nextround");
-
-      // verify the proof of sum-check and evals
-      let res = {
-        let comm_claim_per_round = if i == 0 {
-          comm_claim
-        } else {
-          &self.comm_evals[i - 1]
-        };
-        let comm_eval = &self.comm_evals[i];
-
-        // add two claims to transcript
-        <Transcript as ProofTranscript<G>>::append_point(
-          transcript,
-          b"comm_claim_per_round",
-          comm_claim_per_round,
-        );
-        <Transcript as ProofTranscript<G>>::append_point(transcript, b"comm_eval", comm_eval);
-
-        // produce two weights
-        let w = <Transcript as ProofTranscript<G>>::challenge_vector(
-          transcript,
-          b"combine_two_claims_to_one",
-          2,
-        );
-
-        // compute a weighted sum of the RHS
-        let bases = vec![comm_claim_per_round.into_affine(), comm_eval.into_affine()];
-
-        let comm_target = VariableBaseMSM::msm(bases.as_ref(), w.as_ref()).unwrap();
-
-        let a = {
-          // the vector to use to decommit for sum-check test
-          let a_sc = {
-            let mut a = vec![G::ScalarField::one(); degree_bound + 1];
-            a[0] += G::ScalarField::one();
-            a
-          };
-
-          // the vector to use to decommit for evaluation
-          let a_eval = {
-            let mut a = vec![G::ScalarField::one(); degree_bound + 1];
-            for j in 1..a.len() {
-              a[j] = a[j - 1] * r_i;
-            }
-            a
-          };
-
-          // take weighted sum of the two vectors using w
-          assert_eq!(a_sc.len(), a_eval.len());
-          (0..a_sc.len())
-            .map(|i| w[0] * a_sc[i] + w[1] * a_eval[i])
-            .collect::<Vec<G::ScalarField>>()
-        };
-
-        self.proofs[i]
-          .verify(
-            gens_1,
-            gens_n,
-            transcript,
-            &a,
-            &self.comm_polys[i],
-            &comm_target,
-          )
-          .is_ok()
-      };
-      if !res {
-        return Err(ProofVerifyError::InternalError);
-      }
-
-      r.push(r_i);
-    }
-
-    Ok((self.comm_evals[self.comm_evals.len() - 1], r))
-  }
-}
-
 impl<F: PrimeField> SumcheckInstanceProof<F> {
   pub fn prove_cubic<Func, G, T: ProofTranscript<G>>(
     claim: &F,
@@ -360,6 +190,12 @@ impl<F: PrimeField> SumcheckInstanceProof<F> {
     (SumcheckInstanceProof::new(cubic_polys), r, claims_prod)
   }
 
+  /// Create a sumcheck proof of an arbitrary number of polynomials.
+  /// - `claim`: Claimed sumcheck evaluation (note: currently unused)
+  /// - `num_rounds`: Number of rounds of sumcheck, or number of variables to bind
+  /// - `polys`: Dense polynomials to combine and sumcheck
+  /// - `comb_func`: Function used to combine each polynomial evaluation
+  /// - `transcript`: Fiat-shamir transcript
   pub fn prove_arbitrary<Func, G, T: ProofTranscript<G>>(
     claim: &F,
     num_rounds: usize,
@@ -371,7 +207,7 @@ impl<F: PrimeField> SumcheckInstanceProof<F> {
     Func: Fn(&Vec<F>) -> F,
     G: CurveGroup<ScalarField = F>,
   {
-    let mut e = *claim;
+    let mut e = *claim; // TODO: Currently unused but could make poly evals marginally more efficient
     let mut r: Vec<F> = Vec::new();
     let mut compressed_polys: Vec<CompressedUniPoly<F>> = Vec::new();
 
@@ -388,7 +224,7 @@ impl<F: PrimeField> SumcheckInstanceProof<F> {
         // Evaluate P({0, ..., |g(r)|})
         // Tricks can be used here for low order bits {0,1} but general premise is a running sum for each
         // of the m terms in the Dense multilinear polynomials. Formula is:
-        // half = | D_{n-1}  | / 2
+        // half = | D_{n-1} | / 2
         // D_n(index, r) = D_{n-1}[half + index] + r * (D_{n-1}[half + index] - D_{n-1}[index])
 
         // eval 0: bound_func is A(low)
@@ -399,9 +235,7 @@ impl<F: PrimeField> SumcheckInstanceProof<F> {
           .iter()
           .map(|poly| poly[mle_half + poly_term_i])
           .collect();
-        // println!("[{_round}] Eval at one {:?}", eval_at_one);
         eval_points[1] += comb_func(&eval_at_one);
-        // println!("COMBINED: {:?}", eval_points[1]);
 
         // D_n(index, r) = D_{n-1}[half + index] + r * (D_{n-1}[half + index] - D_{n-1}[index])
         // D_n(index, 0) = D_{n-1} +
@@ -447,6 +281,184 @@ impl<F: PrimeField> SumcheckInstanceProof<F> {
     (SumcheckInstanceProof::new(compressed_polys), r, final_evals)
   }
 }
+
+#[derive(CanonicalSerialize, CanonicalDeserialize, Debug)]
+pub struct SumcheckInstanceProof<F: PrimeField> {
+  compressed_polys: Vec<CompressedUniPoly<F>>,
+}
+
+impl<F: PrimeField> SumcheckInstanceProof<F> {
+  pub fn new(compressed_polys: Vec<CompressedUniPoly<F>>) -> SumcheckInstanceProof<F> {
+    SumcheckInstanceProof { compressed_polys }
+  }
+
+  /// Verify this sumcheck proof.
+  /// Note: Verification does not execute the final check of sumcheck protocol: g_v(r_v) = oracle_g(r),
+  /// as the oracle is not passed in. Expected that the caller will implement.
+  /// - `claim`: Claimed evaluation
+  /// - `num_rounds`: Number of rounds of sumcheck, or number of variables to bind
+  /// - `degree_bound`: Maximum allowed degree of the combined univariate polynomial
+  /// - `transcript`: Fiat-shamir transcript
+  pub fn verify<G, T: ProofTranscript<G>>(
+    &self,
+    claim: F,
+    num_rounds: usize,
+    degree_bound: usize,
+    transcript: &mut T,
+  ) -> Result<(F, Vec<F>), ProofVerifyError>
+  where
+    G: CurveGroup<ScalarField = F>,
+  {
+    let mut e = claim;
+    let mut r: Vec<F> = Vec::new();
+
+    // verify that there is a univariate polynomial for each round
+    assert_eq!(self.compressed_polys.len(), num_rounds);
+    for i in 0..self.compressed_polys.len() {
+      let poly = self.compressed_polys[i].decompress(&e);
+
+      // verify degree bound
+      assert_eq!(poly.degree(), degree_bound);
+
+      // check if G_k(0) + G_k(1) = e
+      assert_eq!(poly.eval_at_zero() + poly.eval_at_one(), e);
+
+      // append the prover's message to the transcript
+      <UniPoly<F> as AppendToTranscript<G>>::append_to_transcript(&poly, b"poly", transcript);
+
+      //derive the verifier's challenge for the next round
+      let r_i = transcript.challenge_scalar(b"challenge_nextround");
+
+      r.push(r_i);
+
+      // evaluate the claimed degree-ell polynomial at r_i
+      e = poly.evaluate(&r_i);
+    }
+
+    Ok((e, r))
+  }
+}
+
+
+#[derive(CanonicalSerialize, CanonicalDeserialize, Debug)]
+pub struct ZKSumcheckInstanceProof<G: CurveGroup> {
+  comm_polys: Vec<G>,
+  comm_evals: Vec<G>,
+  proofs: Vec<DotProductProof<G>>,
+}
+
+impl<G: CurveGroup> ZKSumcheckInstanceProof<G> {
+  pub fn new(comm_polys: Vec<G>, comm_evals: Vec<G>, proofs: Vec<DotProductProof<G>>) -> Self {
+    ZKSumcheckInstanceProof {
+      comm_polys,
+      comm_evals,
+      proofs,
+    }
+  }
+
+  pub fn verify(
+    &self,
+    comm_claim: &G,
+    num_rounds: usize,
+    degree_bound: usize,
+    gens_1: &MultiCommitGens<G>,
+    gens_n: &MultiCommitGens<G>,
+    transcript: &mut Transcript,
+  ) -> Result<(G, Vec<G::ScalarField>), ProofVerifyError> {
+    // verify degree bound
+    assert_eq!(gens_n.n, degree_bound + 1);
+
+    // verify that there is a univariate polynomial for each round
+    assert_eq!(self.comm_polys.len(), num_rounds);
+    assert_eq!(self.comm_evals.len(), num_rounds);
+
+    let mut r: Vec<G::ScalarField> = Vec::new();
+    for i in 0..self.comm_polys.len() {
+      let comm_poly = &self.comm_polys[i];
+
+      // append the prover's polynomial to the transcript
+      <Transcript as ProofTranscript<G>>::append_point(transcript, b"comm_poly", comm_poly);
+
+      //derive the verifier's challenge for the next round
+      let r_i =
+        <Transcript as ProofTranscript<G>>::challenge_scalar(transcript, b"challenge_nextround");
+
+      // verify the proof of sum-check and evals
+      let res = {
+        let comm_claim_per_round = if i == 0 {
+          comm_claim
+        } else {
+          &self.comm_evals[i - 1]
+        };
+        let comm_eval = &self.comm_evals[i];
+
+        // add two claims to transcript
+        <Transcript as ProofTranscript<G>>::append_point(
+          transcript,
+          b"comm_claim_per_round",
+          comm_claim_per_round,
+        );
+        <Transcript as ProofTranscript<G>>::append_point(transcript, b"comm_eval", comm_eval);
+
+        // produce two weights
+        let w = <Transcript as ProofTranscript<G>>::challenge_vector(
+          transcript,
+          b"combine_two_claims_to_one",
+          2,
+        );
+
+        // compute a weighted sum of the RHS
+        let bases = vec![comm_claim_per_round.into_affine(), comm_eval.into_affine()];
+
+        let comm_target = VariableBaseMSM::msm(bases.as_ref(), w.as_ref()).unwrap();
+
+        let a = {
+          // the vector to use to decommit for sum-check test
+          let a_sc = {
+            let mut a = vec![G::ScalarField::one(); degree_bound + 1];
+            a[0] += G::ScalarField::one();
+            a
+          };
+
+          // the vector to use to decommit for evaluation
+          let a_eval = {
+            let mut a = vec![G::ScalarField::one(); degree_bound + 1];
+            for j in 1..a.len() {
+              a[j] = a[j - 1] * r_i;
+            }
+            a
+          };
+
+          // take weighted sum of the two vectors using w
+          assert_eq!(a_sc.len(), a_eval.len());
+          (0..a_sc.len())
+            .map(|i| w[0] * a_sc[i] + w[1] * a_eval[i])
+            .collect::<Vec<G::ScalarField>>()
+        };
+
+        self.proofs[i]
+          .verify(
+            gens_1,
+            gens_n,
+            transcript,
+            &a,
+            &self.comm_polys[i],
+            &comm_target,
+          )
+          .is_ok()
+      };
+      if !res {
+        return Err(ProofVerifyError::InternalError);
+      }
+
+      r.push(r_i);
+    }
+
+    Ok((self.comm_evals[self.comm_evals.len() - 1], r))
+  }
+}
+
+
 
 #[cfg(test)]
 mod test {
