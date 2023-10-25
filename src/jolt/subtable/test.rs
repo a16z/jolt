6b57f6736baa0e35ff2cf18cdeb60aef96c40e3f
Arasu Arun <arasuarun@Arasus-MBP.cable.rcn.com>
2023-10-25 15:57:18 -0400

    variadic for test macro

--- src/jolt/subtable/test.rs
+++ src/jolt/subtable/test.rs
@@ -1,30 +1,12 @@
 #[macro_export]
 macro_rules! subtable_materialize_mle_parity_test {
-  ($test_name:ident, $subtable_type:ty, $F:ty, $M:expr) => {
+  ($test_name:ident, $subtable_type:ty, $F:ty, $M:expr $(, $constructor_args:expr)*) => {
     #[test]
     fn $test_name() {
       const M: usize = $M;
       let log_M = ark_std::log2(M) as usize;
 
-      let subtable = <$subtable_type>::new();
-      let materialized: Vec<_> = subtable.materialize(M);
-      for i in 0..M {
-        assert_eq!(
-          materialized[i],
-          subtable.evaluate_mle(&crate::utils::index_to_field_bitvector(i, log_M)),
-          "MLE did not match materialized subtable at index {}",
-          i
-        );
-      }
-    }
-  };
-  ($test_name:ident, $subtable_type:ty, $F:ty, $M:expr, $A:expr) => {
-    #[test]
-    fn $test_name() {
-      const M: usize = $M;
-      let log_M = ark_std::log2(M) as usize;
-
-      let subtable = <$subtable_type>::new($A);
+      let subtable = <$subtable_type>::new($($constructor_args)?);
       let materialized: Vec<_> = subtable.materialize(M);
       for i in 0..M {
         assert_eq!(
