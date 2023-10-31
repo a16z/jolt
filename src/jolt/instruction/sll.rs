59c7c5ab09c6cdb48c894d0c1080cdd9a0364fdf
Michael Zhu <mchl.zhu.96@gmail.com>
2023-10-31 09:20:03 -0400

    Use const generic CHUNK_INDEX for SLLSubtable

--- src/jolt/instruction/sll.rs
+++ src/jolt/instruction/sll.rs
@@ -1,39 +1,30 @@
-// use core::slice::SlicePattern;
 use ark_ff::PrimeField;
 use ark_std::log2;
 
 use super::JoltInstruction;
 use crate::jolt::subtable::{
-  identity::IdentitySubtable, truncate_overflow::TruncateOverflowSubtable, LassoSubtable,
-  sll::SllSubtable,
+  identity::IdentitySubtable, sll::SllSubtable, truncate_overflow::TruncateOverflowSubtable,
+  LassoSubtable,
 };
-use crate::utils::instruction_utils::{
-  chunk_and_concatenate_for_shift, concatenate_lookups,
-};
+use crate::utils::instruction_utils::{chunk_and_concatenate_for_shift, concatenate_lookups};
 
 #[derive(Copy, Clone, Default, Debug)]
 pub struct SLLInstruction(pub u64, pub u64);
 
 impl JoltInstruction for SLLInstruction {
   fn combine_lookups<F: PrimeField>(&self, vals: &[F], C: usize, M: usize) -> F {
+    // TODO(JOLT-45): make this more robust
+    assert!(C <= 6);
     assert!(vals.len() == 6 * C);
 
-    let mut vals_by_subtable = vals.chunks_exact(C);
+    let mut subtable_vals = vals.chunks_exact(C);
+    let mut vals_filtered: Vec<F> = Vec::with_capacity(C);
+    for i in 0..C {
+      let subtable_val = subtable_vals.next().unwrap();
+      vals_filtered.extend_from_slice(&subtable_val[i..i + 1]);
+    }
 
-    concatenate_lookups(
-      [
-        &vals_by_subtable.next().unwrap()[0..1],
-        &vals_by_subtable.next().unwrap()[1..2],
-        &vals_by_subtable.next().unwrap()[2..3],
-        &vals_by_subtable.next().unwrap()[3..4],
-        &vals_by_subtable.next().unwrap()[4..5],
-        &vals_by_subtable.next().unwrap()[5..6],
-      ]
-      .concat()
-      .as_slice(),
-      C,
-      (log2(M)/2) as usize,
-    )
+    concatenate_lookups(&vals_filtered, C, (log2(M) / 2) as usize)
   }
 
   fn g_poly_degree(&self, _: usize) -> usize {
@@ -42,12 +33,12 @@ impl JoltInstruction for SLLInstruction {
 
   fn subtables<F: PrimeField>(&self) -> Vec<Box<dyn LassoSubtable<F>>> {
     vec![
-      Box::new(SllSubtable::new(5)),
-      Box::new(SllSubtable::new(4)),
-      Box::new(SllSubtable::new(3)),
-      Box::new(SllSubtable::new(2)),
-      Box::new(SllSubtable::new(1)),
-      Box::new(SllSubtable::new(0)),
+      Box::new(SllSubtable::<F, 5>::new()),
+      Box::new(SllSubtable::<F, 4>::new()),
+      Box::new(SllSubtable::<F, 3>::new()),
+      Box::new(SllSubtable::<F, 2>::new()),
+      Box::new(SllSubtable::<F, 1>::new()),
+      Box::new(SllSubtable::<F, 0>::new()),
     ]
   }
 
@@ -72,10 +63,10 @@ mod test {
     const C: usize = 6;
     const M: usize = 1 << 22;
 
-    for _ in 0..256 {
+    for _ in 0..8 {
       let (x, y) = (rng.next_u64(), rng.next_u64());
 
-      let entry: u64 = x.checked_shl((y%64) as u32).unwrap_or(0);
+      let entry: u64 = x.checked_shl((y % 64) as u32).unwrap_or(0);
 
       jolt_instruction_test!(SLLInstruction(x, y), entry.into());
     }
--- src/jolt/subtable/sll.rs
+++ src/jolt/subtable/sll.rs
@@ -4,37 +4,37 @@ use std::marker::PhantomData;
 use std::sync::Arc;
 
 use super::LassoSubtable;
-use crate::utils::split_bits;
 use crate::utils::math::Math;
+use crate::utils::split_bits;
 
 #[derive(Default)]
-pub struct SllSubtable<F: PrimeField> {
+pub struct SllSubtable<F: PrimeField, const CHUNK_INDEX: usize> {
   _field: PhantomData<F>,
-  chunk_idx: usize,
 }
 
-impl<F: PrimeField> SllSubtable<F> {
-  pub fn new(chunk_idx: usize) -> Self {
+impl<F: PrimeField, const CHUNK_INDEX: usize> SllSubtable<F, CHUNK_INDEX> {
+  pub fn new() -> Self {
     Self {
       _field: PhantomData,
-      chunk_idx: chunk_idx,
     }
   }
 }
 
-impl<F: PrimeField> LassoSubtable<F> for SllSubtable<F> {
+impl<F: PrimeField, const CHUNK_INDEX: usize> LassoSubtable<F> for SllSubtable<F, CHUNK_INDEX> {
   fn materialize(&self, M: usize) -> Vec<F> {
     let mut entries: Vec<F> = Vec::with_capacity(M);
 
     let operand_chunk_width: usize = (log2(M) / 2) as usize;
-    let suffix_length = operand_chunk_width * self.chunk_idx;
+    let suffix_length = operand_chunk_width * CHUNK_INDEX;
 
     for idx in 0..M {
       let (x, y) = split_bits(idx, operand_chunk_width);
 
       let row = x
-        .checked_shl((y as u32) %64 + suffix_length as u32).unwrap_or(0)
-        .checked_shr(suffix_length as u32).unwrap_or(0);
+        .checked_shl((y as u32) % 64 + suffix_length as u32)
+        .unwrap_or(0)
+        .checked_shr(suffix_length as u32)
+        .unwrap_or(0);
 
       entries.push(F::from(row as u64));
     }
@@ -42,11 +42,11 @@ impl<F: PrimeField> LassoSubtable<F> for SllSubtable<F> {
   }
 
   fn evaluate_mle(&self, point: &[F]) -> F {
-    // first half is chunk X_i 
+    // first half is chunk X_i
     // and second half is always chunk Y_0
     debug_assert!(point.len() % 2 == 0);
 
-    let MAX_SHIFT= 64;
+    let MAX_SHIFT = 64;
     let log_MAX_SHIFT = log2(MAX_SHIFT) as usize;
 
     let b = point.len() / 2;
@@ -54,28 +54,31 @@ impl<F: PrimeField> LassoSubtable<F> for SllSubtable<F> {
 
     let mut result = F::zero();
 
-    // min with 1 << b is included for test cases with subtables of bit-length smaller than 6 
-    for k in 0..std::cmp::min(MAX_SHIFT, 1<<b) {
+    // min with 1 << b is included for test cases with subtables of bit-length smaller than 6
+    for k in 0..std::cmp::min(MAX_SHIFT, 1 << b) {
       let k_bits = (k as usize)
-        .get_bits(log_MAX_SHIFT).iter()
+        .get_bits(log_MAX_SHIFT)
+        .iter()
         .map(|bit| F::from(*bit as u64))
         .collect::<Vec<F>>(); // big-endian
 
       let mut eq_term = F::one();
-      // again, min with b is included when subtables of bit-length less than 6 are used 
+      // again, min with b is included when subtables of bit-length less than 6 are used
       for i in 0..std::cmp::min(log_MAX_SHIFT, b) {
-        eq_term *= k_bits[log_MAX_SHIFT-1-i] * y[b-1-i] + (F::one() - k_bits[log_MAX_SHIFT-1-i]) * (F::one() - y[b-1-i]);
+        eq_term *= k_bits[log_MAX_SHIFT - 1 - i] * y[b - 1 - i]
+          + (F::one() - k_bits[log_MAX_SHIFT - 1 - i]) * (F::one() - y[b - 1 - i]);
       }
 
-      let m = if (k + b * (self.chunk_idx+1)) > 64 {
-        std::cmp::min(b, (k + b * (self.chunk_idx+1)) - 64)
+      let m = if (k + b * (CHUNK_INDEX + 1)) > 64 {
+        std::cmp::min(b, (k + b * (CHUNK_INDEX + 1)) - 64)
       } else {
         0
       };
 
       let m_prime = b - (m as usize);
-      
-      let shift_x_by_k = (0..m_prime).enumerate()
+
+      let shift_x_by_k = (0..m_prime)
+        .enumerate()
         .map(|(j, _)| F::from(1_u64 << (j + k)) * x[b - 1 - j])
         .fold(F::zero(), |acc, val| acc + val);
 
@@ -94,5 +97,5 @@ mod test {
     subtable_materialize_mle_parity_test,
   };
 
-  subtable_materialize_mle_parity_test!(sll_materialize_mle_parity, SllSubtable<Fr>, Fr, 256, 0);
+  subtable_materialize_mle_parity_test!(sll_materialize_mle_parity, SllSubtable<Fr, 0>, Fr, 256);
 }
