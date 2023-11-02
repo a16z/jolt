55608f2b00b987a787e34c6f045d30ddedbf62f9
Michael Zhu <mchl.zhu.96@gmail.com>
2023-11-02 18:01:18 -0400

    delete unused code

--- src/jolt/mod.rs
+++ src/jolt/mod.rs
@@ -1,8 +1,3 @@
-
-// pub mod and;
-// pub mod lt;
-pub mod jolt_strategy;
-pub mod test;
 pub mod instruction;
 pub mod subtable;
 pub mod vm;
--- src/lasso/mod.rs
+++ src/lasso/mod.rs
@@ -1,6 +1,4 @@
-pub mod densified;
 pub mod memory_checking;
-pub mod surge;
 pub mod fingerprint_strategy;
 pub mod gp_evals;
 pub mod surge_2;
