07562862ccbef9633ce33eeeeef64cfe09e7fc05
Arasu Arun <arasu.arun5@gmail.com>
2023-11-10 15:55:38 -0500

    instruction flags file

--- jolt-core/src/jolt/vm/instruction_set.rs
+++ jolt-core/src/jolt/vm/instruction_set.rs
@@ -61,33 +61,33 @@ trait R1CSFlags {
 impl R1CSFlags for RV32IM {
   fn to_circuit_flags<F: PrimeField>(&self) -> Vec<F> {
 
-    // TODO(sragss):
-    // - What are we doing with imm if we have a flag for the sign bit?
-    // - What is 10 below?
-
     // Jolt Appendix A.1
-    // 0: first_operand == rs2
-    // 1: second_operand == imm
+    // 0: first_operand == rs1 (1 if PC)
+    // 1: second_operand == rs2 (1 if imm)
     // 2: Load instruction
     // 3: Store instruciton
     // 4: Jump instruction 
     // 5: Branch instruciton
-    // 6: Instruction writes to rd
-    // 7: Instruction adds operands
+    // 6: Instruction writes lookup output to rd
+    // 7: Instruction adds operands (ie, and uses the ADD lookup table)
     // 8: Instruction subtracts operands
     // 9: Instruction multiplies operands
     // 10: Instruction involves non-deterministic advice?
     // 11: Instruction asserts lookup output as false
     // 12: Instruction asserts lookup output as true
     // 13: Sign-bit of imm
+    // 14: Instruction is lui 
 
     let flag_0 = match self {
-        _ => true
+        RV32IM::JAL | RV32IM::JALR | RV32IM::LUI | RV32IM::AUIPC => true,
+        _ => false
     };
 
     let flag_1 = match self {
-        _ => true
-    }
+        RV32IM::ADDI | RV32IM::XORI | RV32IM::ORI | RV32IM::ANDI | RV32IM::SLLI 
+            | RV32IM::SRLI | RV32IM::SRAI | RV32IM::SLTI | RV32IM::SLTIU => true,
+        _ => false,
+    };
 
     let flag_2 = match self {
         RV32IM::LB | RV32IM::LH | RV32IM::LW | RV32IM::LBU | RV32IM::LHU => true,
@@ -109,15 +109,53 @@ impl R1CSFlags for RV32IM {
         _ => false
     };
 
+    // loads, stores, branches, jumps do not store the lookup output to rd (they may update rd in other ways)
     let flag_6 = match self {
-        RV32IM::ADD | RV32IM::SUB | RV32IM::SLL | RV32IM::SLT | RV32IM::SLTU | RV32IM::XOR | RV32IM::SRL | RV32IM::SRA | RV32IM::OR | RV32IM::AND => true,
-        _ => false
+        RV32IM::LB | RV32IM::LH | RV32IM::LW | RV32IM::LBU | RV32IM::LHU | RV32IM::SB | RV32IM::SH | RV32IM::SW |
+        RV32IM::BEQ | RV32IM::BNE | RV32IM::BLT | RV32IM::BGE | RV32IM::BLTU | RV32IM::BGEU |
+        RV32IM::JAL | RV32IM::JALR | RV32IM::LUI  => false,
+        _ => true,
     };
 
     let flag_7 = match self {
-        // RV32IM::ADD | RV32IM::SUB | RV32IM::SLL | RV32IM::SLT | RV32IM::SLTU | RV32IM::XOR | RV32IM::SRL | RV32IM::SRA | RV32IM::OR | RV32IM::AND => true,
-        // _ => false
+        RV32IM::ADD | RV32IM::ADDI | RV32IM::JAL | RV32IM::JALR | RV32IM::AUIPC => true,
+        _ => false
     };
-    todo!("fill out  all flags")
+
+    let flag_8 = match self {
+        RV32IM::SUB => true,
+        _ => false
+    };
+
+    let flag_9 = match self {
+        RV32IM::MUL | RV32IM::MULU | RV32IM::MULH | RV32IM::MULSU => true,
+        _ => false
+    };
+
+    // not incorporating advice instructions yet
+    let flag_10 = match self {
+        _ => false
+    };
+
+    // not incorporating assert true instructions yet
+    let flag_11 = match self {
+        _ => false
+    };
+
+    // not incorporating assert false instructions yet
+    let flag_12 = match self {
+        _ => false
+    };
+
+    // not incorporating advice instructions yet
+    let flag_13 = match self {
+        _ => false
+    };
+
+    let flag_14 = match self {
+        RV32IM::LUI => true,
+        _ => false
+    };
+
   }
 }
