961fe5f95772f9f35fa60ee00a7962db926e1ddb
Noah Citron <noah@jeff.org>
2023-11-17 16:32:53 -0500

    feat: add example programs

---
+++ examples/fibonacci/src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use core::arch::global_asm;
+use core::panic::PanicInfo;
+
+global_asm!(include_str!("entry.s"));
+
+#[no_mangle]
+pub extern "C" fn main() {
+    fib(5);
+}
+
+fn fib(n: u32) -> u32 {
+    if n <= 1 {
+        1
+    } else {
+        fib(n-1) + fib(n-2)
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+   loop {}
+}
---
+++ examples/hash/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("cargo:rustc-link-arg-bin=hash=-Texamples/hash/linker.ld");
+}
