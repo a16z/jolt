d9590bedd1b872f139561c25e875bbd24537e1db
Michael Zhu <mchl.zhu.96@gmail.com>
2023-11-20 15:53:07 -0500

    Fix random_surge_test

--- jolt-core/src/benches/bench.rs
+++ jolt-core/src/benches/bench.rs
@@ -1,6 +1,6 @@
 use crate::jolt::instruction::xor::XORInstruction;
-use crate::lasso::surge::SurgeProof;
-use ark_curve25519::EdwardsProjective;
+use crate::lasso::surge::Surge;
+use ark_curve25519::{EdwardsProjective, Fr};
 use ark_std::{log2, test_rng};
 use merlin::Transcript;
 use rand_chacha::rand_core::RngCore;
@@ -21,155 +21,99 @@ pub fn benchmarks(bench_type: BenchType) -> Vec<(tracing::Span, Box<dyn FnOnce()
 }
 
 fn jolt_demo_benchmarks() -> Vec<(tracing::Span, Box<dyn FnOnce()>)> {
+  const C: usize = 8;
+  const M: usize = 1 << 16;
   vec![
     (
       tracing::info_span!("EQ(2^10)"),
-      random_surge_test(
-        /* num_ops */ 1 << 10,
-        /* C= */ 8,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 10),
     ),
     (
       tracing::info_span!("EQ(2^12)"),
-      random_surge_test(
-        /* num_ops */ 1 << 12,
-        /* C= */ 8,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 12),
     ),
     (
       tracing::info_span!("EQ(2^14)"),
-      random_surge_test(
-        /* num_ops */ 1 << 14,
-        /* C= */ 8,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 14),
     ),
     (
       tracing::info_span!("EQ(2^16)"),
-      random_surge_test(
-        /* num_ops */ 1 << 16,
-        /* C= */ 8,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 16),
     ),
     (
       tracing::info_span!("EQ(2^18)"),
-      random_surge_test(
-        /* num_ops */ 1 << 18,
-        /* C= */ 8,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 18),
     ),
     (
       tracing::info_span!("EQ(2^20)"),
-      random_surge_test(
-        /* num_ops */ 1 << 20,
-        /* C= */ 8,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 20),
     ),
     (
       tracing::info_span!("EQ(2^22)"),
-      random_surge_test(
-        /* num_ops */ 1 << 22,
-        /* C= */ 8,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 22),
     ),
   ]
 }
 
 fn halo2_comparison_benchmarks() -> Vec<(tracing::Span, Box<dyn FnOnce()>)> {
+  const C: usize = 1;
+  const M: usize = 1 << 16;
   vec![
     (
       tracing::info_span!("EQ(2^10)"),
-      random_surge_test(
-        /* num_ops */ 1 << 10,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 10),
     ),
     (
       tracing::info_span!("EQ(2^12)"),
-      random_surge_test(
-        /* num_ops */ 1 << 12,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 12),
     ),
     (
       tracing::info_span!("EQ(2^14)"),
-      random_surge_test(
-        /* num_ops */ 1 << 14,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 14),
     ),
     (
       tracing::info_span!("EQ(2^16)"),
-      random_surge_test(
-        /* num_ops */ 1 << 16,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 16),
     ),
     (
       tracing::info_span!("EQ(2^18)"),
-      random_surge_test(
-        /* num_ops */ 1 << 18,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 18),
     ),
     (
       tracing::info_span!("EQ(2^20)"),
-      random_surge_test(
-        /* num_ops */ 1 << 20,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 20),
     ),
     (
       tracing::info_span!("EQ(2^22)"),
-      random_surge_test(
-        /* num_ops */ 1 << 22,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 22),
     ),
     (
       tracing::info_span!("EQ(2^24)"),
-      random_surge_test(
-        /* num_ops */ 1 << 24,
-        /* C= */ 1,
-        /* M= */ 1 << 16,
-      ),
+      random_surge_test::<C, M>(/* num_ops */ 1 << 24),
     ),
   ]
 }
 
-fn random_surge_test(num_ops: usize, C: usize, M: usize) -> Box<dyn FnOnce()> {
-  todo!();
-  // let mut rng = test_rng();
+fn random_surge_test<const C: usize, const M: usize>(num_ops: usize) -> Box<dyn FnOnce()> {
+  let mut rng = test_rng();
 
-  // let mut ops: Vec<XORInstruction> = Vec::with_capacity(num_ops);
-  // let operand_max: u64 = (1 << (log2(M) as usize * C - 1)).try_into().unwrap();
-  // for _ in 0..num_ops {
-  //   let a = rng.next_u64() % operand_max;
-  //   let b = rng.next_u64() % operand_max;
-  //   ops.push(XORInstruction(a, b));
-  // }
+  let mut ops: Vec<XORInstruction> = Vec::with_capacity(num_ops);
+  let operand_max: u64 = (1 << (log2(M) as usize * C - 1)).try_into().unwrap();
+  for _ in 0..num_ops {
+    let a = rng.next_u64() % operand_max;
+    let b = rng.next_u64() % operand_max;
+    ops.push(XORInstruction(a, b));
+  }
 
-  // let func = move || {
-  //   let mut prover_transcript = Transcript::new(b"test_transcript");
-  //   let proof: SurgeProof<_, EdwardsProjective> =
-  //     SurgeProof::prove(ops.clone(), C, M, &mut prover_transcript);
+  let func = move || {
+    let mut prover_transcript = Transcript::new(b"test_transcript");
+    let surge = <Surge<Fr, EdwardsProjective, XORInstruction, C, M>>::new(ops.clone());
+    let proof = surge.prove(&mut prover_transcript);
 
-  //   let mut verifier_transcript = Transcript::new(b"test_transcript");
-  //   proof.verify(&mut verifier_transcript).expect("should work");
-  // };
+    let mut verifier_transcript = Transcript::new(b"test_transcript");
+    <Surge<Fr, EdwardsProjective, XORInstruction, C, M>>::verify(proof, &mut verifier_transcript)
+      .expect("should work");
+  };
 
-  // Box::new(func)
+  Box::new(func)
 }
