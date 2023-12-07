4857a469d4bc9aab5ff14ca47a1f9fe32cdb2937
Michael Zhu <mchl.zhu.96@gmail.com>
2023-12-07 13:10:04 -0500

    Add flamegraph option to benchmarks

--- jolt-core/src/main.rs
+++ jolt-core/src/main.rs
@@ -1,38 +1,72 @@
+use clap::{Parser, ValueEnum};
 use liblasso::benches::bench::{benchmarks, BenchType};
-use tracing_subscriber::{self, fmt::format::FmtSpan};
-
-use clap::Parser;
+use std::{fs::File, io::BufWriter};
+use tracing_flame::FlameLayer;
+use tracing_subscriber::{self, fmt, fmt::format::FmtSpan, prelude::*, registry::Registry};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser, Debug)]
 struct Cli {
-    /// Whether to present in chart format
-    #[clap(long, short, action)]
-    chart: bool,
+    /// Output format
+    #[clap(short, long, value_enum, default_value_t = Format::Default)]
+    format: Format,
 
     /// Type of benchmark to run
     #[clap(long, value_enum)]
     name: BenchType,
 }
 
+#[derive(Debug, Clone, ValueEnum)]
+enum Format {
+    Default,
+    Texray,
+    Flamegraph,
+}
+
+fn setup_global_subscriber() -> impl Drop {
+    let fmt_layer = fmt::Layer::default();
+
+    let (flame_layer, _guard) = FlameLayer::with_file("./tracing.folded").unwrap();
+
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(flame_layer)
+        .init();
+
+    _guard
+}
+
 fn main() {
     let args = Cli::parse();
-    if args.chart {
-        tracing_texray::init();
-        for (span, bench) in benchmarks(args.name).into_iter() {
-            tracing_texray::examine(span.to_owned()).in_scope(bench);
+    match args.format {
+        Format::Default => {
+            let collector = tracing_subscriber::fmt()
+                .with_max_level(tracing::Level::TRACE)
+                .with_span_events(FmtSpan::CLOSE)
+                .finish();
+            tracing::subscriber::set_global_default(collector)
+                .expect("setting tracing default failed");
+            for (span, bench) in benchmarks(args.name).into_iter() {
+                span.to_owned().in_scope(|| {
+                    bench();
+                    tracing::info!("Bench Complete");
+                });
+            }
         }
-    } else {
-        let collector = tracing_subscriber::fmt()
-            .with_max_level(tracing::Level::TRACE)
-            .with_span_events(FmtSpan::CLOSE)
-            .finish();
-        tracing::subscriber::set_global_default(collector).expect("setting tracing default failed");
-        for (span, bench) in benchmarks(args.name).into_iter() {
-            span.to_owned().in_scope(|| {
-                bench();
-                tracing::info!("Bench Complete");
-            });
+        Format::Texray => {
+            tracing_texray::init();
+            for (span, bench) in benchmarks(args.name).into_iter() {
+                tracing_texray::examine(span.to_owned()).in_scope(bench);
+            }
+        }
+        Format::Flamegraph => {
+            let _guard = setup_global_subscriber();
+            for (span, bench) in benchmarks(args.name).into_iter() {
+                span.to_owned().in_scope(|| {
+                    bench();
+                    tracing::info!("Bench Complete");
+                });
+            }
         }
     }
 }
