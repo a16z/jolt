7ca52221f5c0260611a0961d1f11ce1ad919c402
Sam Ragsdale <sammage@gmail.com>
2023-12-29 12:15:51 -0500

    init integration tests

---
+++ integration-tests/tests/fib.rs
@@ -0,0 +1,7 @@
+#[test]
+fn fib_e2e() {
+    // Run the compiler
+
+    // Run the integration test
+    unimplemented!();
+}
---
+++ integration-tests/tests/hash.rs
@@ -0,0 +1,7 @@
+#[test]
+fn hash_e2e() {
+    // Run the compiler
+
+    // Run the integration test
+    unimplemented!();
+}
