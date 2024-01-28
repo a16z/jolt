6dea601b17f0d41147d65ecf61d88c8d85df9227
Sam Ragsdale <sammage@gmail.com>
2024-01-27 16:17:49 -0800

    working SparseGrandProductCircuit::construct + tests

--- jolt-core/src/subprotocols/sparse.rs
+++ jolt-core/src/subprotocols/sparse.rs
@@ -3,7 +3,7 @@ use ark_ff::PrimeField;
 use crate::{poly::dense_mlpoly::DensePolynomial, utils::math::Math};
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct SparseEntry<F> {
     value: F,
     index: usize
@@ -15,7 +15,7 @@ impl<F> SparseEntry<F> {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct SparsePoly<F> {
     low_entries: Vec<SparseEntry<F>>,
     high_entries: Vec<SparseEntry<F>>,
@@ -42,8 +42,8 @@ impl<F> std::ops::Index<usize> for SparsePoly<F> {
 impl<F: PrimeField> SparsePoly<F> {
     pub fn new(low_entries: Vec<SparseEntry<F>>, high_entries: Vec<SparseEntry<F>>, dense_len: usize) -> Self {
         let mid = dense_len / 2;
-        assert!(low_entries.len() < dense_len);
-        assert!(high_entries.len() < dense_len);
+        assert!(low_entries.len() <= dense_len);
+        assert!(high_entries.len() <= dense_len);
         let num_vars = dense_len.log_2();
 
         Self { low_entries, high_entries, num_vars, dense_len, low_sparse_index: 0, high_sparse_index: 0 }
@@ -174,6 +174,7 @@ pub struct SparseGrandProductCircuit<F> {
 
 impl<F: PrimeField> SparseGrandProductCircuit<F> {
     pub fn construct(leaves: Vec<F>, flags: Vec<bool>) -> Self {
+        assert_eq!(leaves.len(), flags.len());
         let num_leaves = leaves.len();
         let num_layers = num_leaves.log_2(); 
         let leaf_half = num_leaves / 2;
@@ -181,23 +182,24 @@ impl<F: PrimeField> SparseGrandProductCircuit<F> {
         let mut lefts: Vec<SparsePoly<F>> = Vec::with_capacity(num_layers);
         let mut rights: Vec<SparsePoly<F>> = Vec::with_capacity(num_layers);
 
-        // TODO(sragss): Attempt rough capacity planning here? We could construct metadata from initial flag scan.
-        let mut left_low: Vec<SparseEntry<F>> = Vec::new();
-        let mut left_high: Vec<SparseEntry<F>> = Vec::new();
-        let mut right_low: Vec<SparseEntry<F>> = Vec::new();
-        let mut right_high: Vec<SparseEntry<F>> = Vec::new();
+        let max_capacity = num_leaves / 4;
+        let mut left_low: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
+        let mut left_high: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
+        let mut right_low: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
+        let mut right_high: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
 
         // First layer
+        let new_leaf_half = leaf_half / 2;
         for leaf_index in 0..num_leaves {
             if flags[leaf_index] { 
-                if leaf_index < leaf_half / 2 {
+                if leaf_index < new_leaf_half {
                     left_low.push(SparseEntry::new(leaves[leaf_index], leaf_index));
-                } else if leaf_index < leaf_half {
-                    left_high.push(SparseEntry::new(leaves[leaf_index], leaf_index));
-                } else if leaf_index < leaf_half + (leaf_half / 2) {
-                    right_low.push(SparseEntry::new(leaves[leaf_index], leaf_index));
+                } else if leaf_index < 2 * new_leaf_half {
+                    left_high.push(SparseEntry::new(leaves[leaf_index], leaf_index - new_leaf_half));
+                } else if leaf_index < 3 * new_leaf_half{
+                    right_low.push(SparseEntry::new(leaves[leaf_index], leaf_index - 2 * new_leaf_half));
                 } else {
-                    right_high.push(SparseEntry::new(leaves[leaf_index], leaf_index));
+                    right_high.push(SparseEntry::new(leaves[leaf_index], leaf_index - 3 * new_leaf_half));
                 }
             }
         }
@@ -225,35 +227,31 @@ impl<F: PrimeField> SparseGrandProductCircuit<F> {
             prior_left: &SparsePoly<F>, 
             prior_right: &SparsePoly<F>, 
             prior_len: usize) -> (SparsePoly<F>, SparsePoly<F>) {
+
+        // TODO(sragss): Dedupe below -- it's gross.
+
         // Capacity has the potential to overshoot by a factor 2
         let max_capacity = prior_left.sparse_len();
         let mut left_low: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
         let mut left_high: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
-        let max_capacity = prior_right.sparse_len();
-        let mut right_low: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
-        let mut right_high: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
 
         let mut left_sparse_index: usize = 0;
         let mut right_sparse_index: usize = 0;
+        while left_sparse_index < prior_left.low_entries.len() && right_sparse_index < prior_right.low_entries.len() {
+            let left_index = prior_left.low_entries[left_sparse_index].index;
+            let right_index = prior_right.low_entries[right_sparse_index].index;
 
-        while left_sparse_index < prior_left.sparse_len() && right_sparse_index < prior_right.sparse_len() {
-            // Mere existence of these indices means they're "non-sparse": not equal to 1.
-            let left_index = prior_left[left_sparse_index].index;
-            let right_index = prior_right[right_sparse_index].index;
-
-            todo!("sam deal with the index updates");
-
-            let entry = if left_index == right_index {
-                let value = prior_left[left_sparse_index].value * prior_right[right_sparse_index].value;
+            let mut entry = if left_index == right_index {
+                let value = prior_left.low_entries[left_sparse_index].value * prior_right.low_entries[right_sparse_index].value;
                 left_sparse_index += 1;
                 right_sparse_index += 1;
                 SparseEntry::new(value, left_index)
             } else if left_index < right_index {
-                let entry = prior_left[left_sparse_index].clone();
+                let entry = prior_left.low_entries[left_sparse_index].clone();
                 left_sparse_index += 1;
                 entry
             } else if right_index < left_index {
-                let entry = prior_right[right_sparse_index].clone();
+                let entry = prior_right.low_entries[right_sparse_index].clone();
                 right_sparse_index += 1;
                 entry
             } else {
@@ -263,17 +261,52 @@ impl<F: PrimeField> SparseGrandProductCircuit<F> {
             // prior_left.dense_len() + prior_right.dense_len() == prior_len
             // dense_left.len() == dense_right.len() == prior_len / 4
             // dense_left_low.len() == dense_left_high.len() == ... == prior.len / 8
-            if entry.index < prior_len / 8 {
+            if entry.index < prior_len / 8 || prior_len == 4 {
                 left_low.push(entry);
-            } else if entry.index < prior_len / 4 {
+            } else {
+                entry.index -= prior_len / 8;
                 left_high.push(entry);
-            } else if entry.index < (prior_len / 4) + (prior_len / 8) {
+            }
+        }
+
+        let max_capacity = prior_right.sparse_len();
+        let mut right_low: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
+        let mut right_high: Vec<SparseEntry<F>> = Vec::with_capacity(max_capacity);
+        let mut left_sparse_index: usize = 0;
+        let mut right_sparse_index: usize = 0;
+        while left_sparse_index < prior_left.high_entries.len() && right_sparse_index < prior_right.high_entries.len() {
+            let left_index = prior_left.high_entries[left_sparse_index].index;
+            let right_index = prior_right.high_entries[right_sparse_index].index;
+
+            let mut entry = if left_index == right_index {
+                let value = prior_left.high_entries[left_sparse_index].value * prior_right.high_entries[right_sparse_index].value;
+                left_sparse_index += 1;
+                right_sparse_index += 1;
+                SparseEntry::new(value, left_index)
+            } else if left_index < right_index {
+                let entry = prior_left.high_entries[left_sparse_index].clone();
+                left_sparse_index += 1;
+                entry
+            } else if right_index < left_index {
+                let entry = prior_right.high_entries[right_sparse_index].clone();
+                right_sparse_index += 1;
+                entry
+            } else {
+                unreachable!();
+            };
+
+            // prior_left.dense_len() + prior_right.dense_len() == prior_len
+            // dense_left.len() == dense_right.len() == prior_len / 4
+            // dense_left_low.len() == dense_left_high.len() == ... == prior.len / 8
+            if entry.index < prior_len / 8 || prior_len == 4 {
                 right_low.push(entry);
             } else {
+                entry.index -= prior_len / 8;
                 right_high.push(entry);
             }
         }
-        let new_len = prior_len / 2;
+
+        let new_len = prior_len / 4;
         let left = SparsePoly::new(left_low, left_high, new_len);
         let right = SparsePoly::new(right_low, right_high, new_len);
         (left, right)
@@ -283,9 +316,11 @@ impl<F: PrimeField> SparseGrandProductCircuit<F> {
 
 #[cfg(test)]
 mod tests {
+    use crate::subprotocols::grand_product::GrandProductCircuit;
+
     use super::{*, bench::init_bind_bench};
     use ark_curve25519::Fr;
-    use ark_std::{One, test_rng};
+    use ark_std::{One, test_rng, rand::Rng};
 
     #[test]
     fn sparse_conversion() {
@@ -413,6 +448,106 @@ mod tests {
         dense.bound_poly_var_top(&r_3);
 
         assert_eq!(dense[0], sparse.final_eval());
+    }
+
+    #[test]
+    fn sparse_circuit_construction_dense() {
+        let leaves: Vec<Fr> = vec![Fr::from(0), Fr::from(1), Fr::from(2), Fr::from(3), Fr::from(4), Fr::from(5), Fr::from(6), Fr::from(7)];
+        let flags: Vec<bool> = vec![true, true, true, true, true, true, true, true];
+
+        let circuit = SparseGrandProductCircuit::construct(leaves, flags);
+
+        // Example:
+        // 0: LEFT = A, B, C, D     RIGHT = E, F, G, H
+        // 1: LEFT = AE, BF         RIGHT = CG, DH
+        // 2: LEFT = AEBF           RIGHT = CGDH
+
+        // (indices) 0  1  0  1             0  1  0  1
+        // 0: LEFT = A, B, C, D     RIGHT = E, F, G, H
+        //           0   0                  0   0 
+        // 1: LEFT = AE, BF         RIGHT = CG, DH
+        // 2: LEFT = AEBF           RIGHT = CGDH
+
+        // layer 0
+        let expected_left = SparsePoly::new(
+            vec![SparseEntry::new(Fr::from(0), 0), SparseEntry::new(Fr::from(1), 1)], vec![SparseEntry::new(Fr::from(2), 0), SparseEntry::new(Fr::from(3), 1)], 
+            4
+        );
+        assert_eq!(circuit.left[0], expected_left);
+        let expected_right= SparsePoly::new(
+            vec![SparseEntry::new(Fr::from(4), 0), SparseEntry::new(Fr::from(5), 1)], vec![SparseEntry::new(Fr::from(6), 0), SparseEntry::new(Fr::from(7), 1)], 
+            4
+        );
+        assert_eq!(circuit.right[0], expected_right);
+
+        // layer 1
+        let expected_left = SparsePoly::new(
+            vec![SparseEntry::new(Fr::from(0) * Fr::from(4), 0)], 
+            vec![SparseEntry::new(Fr::from(1) * Fr::from(5), 0)], 
+            2
+        );
+        let expected_right = SparsePoly::new(
+            vec![SparseEntry::new(Fr::from(2) * Fr::from(6), 0)], 
+            vec![SparseEntry::new(Fr::from(3) * Fr::from(7), 0)], 
+            2
+        );
+        assert_eq!(circuit.left[1], expected_left);
+        assert_eq!(circuit.right[1], expected_right);
+
+        let combo: Fr = Fr::from(0) * Fr::from(4) * Fr::from(2) * Fr::from(6);
+        let expected_left = SparsePoly::new(
+            vec![SparseEntry::new(combo, 0)],
+            vec![],
+            1
+        );
+        let combo: Fr = Fr::from(1) * Fr::from(5) * Fr::from(3) * Fr::from(7);
+        let expected_right= SparsePoly::new(
+            vec![SparseEntry::new(combo, 0)],
+            vec![],
+            1
+        );
+        assert_eq!(circuit.left[2], expected_left);
+        assert_eq!(circuit.right[2], expected_right);
+
+    }
+
+    fn gen_leaves_flags<F: PrimeField>(log_size: usize, pct_ones: f64) -> (Vec<F>, Vec<bool>) {
+        let mut rng = test_rng();
+        let size = 1 << log_size;
+        let mut leaves = Vec::new();
+        let mut flags = Vec::new();
+
+        for _ in 0..size {
+            leaves.push(F::rand(&mut rng));
+
+            if rng.gen::<f64>() > pct_ones {
+                flags.push(true);
+            } else {
+                flags.push(false);
+            }
+        }
+        (leaves, flags)
+    }
+
+    #[test]
+    fn sparse_circuit_parity() {
+        let log_size = 8;
+        let (leaves, flags) = gen_leaves_flags::<Fr>(log_size, 0.8);
+
+        let dense_leaves_toggled: Vec<Fr> = leaves.iter().zip(flags.iter()).map(|(leaf, flag)| {
+            if *flag {
+                *leaf
+            } else {
+                Fr::one()
+            }
+        }).collect();
+
+        let mut dense_circuit = GrandProductCircuit::new(&DensePolynomial::new(dense_leaves_toggled));
+        let sparse_circuit = SparseGrandProductCircuit::construct(leaves, flags);
+
+        let (dense_layer_0_left, dense_layer_0_right) = dense_circuit.take_layer(0);
+        assert_eq!(dense_layer_0_left, sparse_circuit.left[0].clone().to_dense());
+        assert_eq!(dense_layer_0_right, sparse_circuit.right[0].clone().to_dense());
     }
 }
 
