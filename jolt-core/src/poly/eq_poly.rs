cb5b7eaa5ad84a69fb0d33f811153621f3096f4e
Michael Zhu <mchl.zhu.96@gmail.com>
2024-02-14 20:23:11 +0900

    Parallelize EqPolynomial::evals

--- jolt-core/src/poly/eq_poly.rs
+++ jolt-core/src/poly/eq_poly.rs
@@ -1,10 +1,13 @@
 use ark_ff::PrimeField;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 use crate::utils::math::Math;
 
 pub struct EqPolynomial<F> {
     r: Vec<F>,
 }
+
+const PARALLEL_THRESHOLD: usize = 16;
 
 impl<F: PrimeField> EqPolynomial<F> {
     pub fn new(r: Vec<F>) -> Self {
@@ -18,10 +21,18 @@ impl<F: PrimeField> EqPolynomial<F> {
             .product()
     }
 
-    #[tracing::instrument(skip_all, name = "EqPolynomial.evals")]
+    #[tracing::instrument(skip_all, name = "EqPolynomial::evals")]
     pub fn evals(&self) -> Vec<F> {
         let ell = self.r.len();
 
+        match ell {
+            0..=PARALLEL_THRESHOLD => self.evals_serial(ell),
+            _ => self.evals_parallel(ell),
+        }
+    }
+
+    /// Computes evals serially. Uses less memory (and fewer allocations) than `evals_parallel`.
+    fn evals_serial(&self, ell: usize) -> Vec<F> {
         let mut evals: Vec<F> = vec![F::one(); ell.pow2()];
         let mut size = 1;
         for j in 0..ell {
@@ -33,6 +44,35 @@ impl<F: PrimeField> EqPolynomial<F> {
                 evals[i] = scalar * self.r[j];
                 evals[i - 1] = scalar - evals[i];
             }
+        }
+        evals
+    }
+
+    /// Computes evals in parallel. Uses more memory and allocations than `evals_serial`, but
+    /// evaluates biggest layers of the dynamic programming tree in parallel.
+    fn evals_parallel(&self, ell: usize) -> Vec<F> {
+        let mut previous_evals: Vec<F>;
+        let mut evals: Vec<F> = vec![F::one()];
+        for j in 1..=ell {
+            previous_evals = evals;
+            evals = match j {
+                0..=PARALLEL_THRESHOLD => previous_evals
+                    .iter()
+                    .flat_map(|&eval| {
+                        let x1 = eval * self.r[j - 1];
+                        let x0 = eval - x1;
+                        vec![x0, x1]
+                    })
+                    .collect(),
+                _ => previous_evals
+                    .par_iter()
+                    .flat_map_iter(|&eval| {
+                        let x1 = eval * self.r[j - 1];
+                        let x0 = eval - x1;
+                        vec![x0, x1]
+                    })
+                    .collect(),
+            };
         }
         evals
     }
