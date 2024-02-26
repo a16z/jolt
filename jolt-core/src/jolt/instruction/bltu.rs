1154b92c42bc54bcc2f74db050889e3a64f4f187
sragss <sragsdale@a16z.com>
2024-02-26 14:35:50 -0700

    bltu

--- jolt-core/src/jolt/instruction/bltu.rs
+++ jolt-core/src/jolt/instruction/bltu.rs
@@ -55,30 +55,47 @@ mod test {
     use rand_chacha::rand_core::RngCore;
 
     use crate::{jolt::instruction::JoltInstruction, jolt_instruction_test};
+    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
 
     use super::BLTUInstruction;
 
     #[test]
-    fn bltu_instruction_e2e() {
+    fn bltu_instruction_32_e2e() {
+        let mut rng = test_rng();
+        const C: usize = 4;
+        const M: usize = 1 << 16;
+
+        for _ in 0..256 {
+            let x = rng.next_u32() as u64;
+            let y = rng.next_u32() as u64;
+            let instruction = BLTUInstruction(x, y);
+            let expected = instruction.lookup_entry_u64();
+            jolt_instruction_test!(instruction, expected.into());
+            assert_eq!(
+                instruction.lookup_entry::<Fr>(C, M),
+                expected.into()
+            );
+        }
+    }
+
+    #[test]
+    fn bltu_instruction_64_e2e() {
         let mut rng = test_rng();
         const C: usize = 8;
         const M: usize = 1 << 16;
 
         for _ in 0..256 {
-            let (x, y) = (rng.next_u64(), rng.next_u64());
-            jolt_instruction_test!(BLTUInstruction(x, y), (x < y).into());
+            let x = rng.next_u64();
+            let y = rng.next_u64();
+            let instruction = BLTUInstruction(x, y);
+            let expected = instruction.lookup_entry_u64();
+            jolt_instruction_test!(instruction, expected.into());
             assert_eq!(
-                BLTUInstruction(x, y).lookup_entry::<Fr>(C, M),
-                (x < y).into()
+                instruction.lookup_entry::<Fr>(C, M),
+                expected.into()
             );
         }
-        for _ in 0..256 {
-            let x = rng.next_u64();
-            jolt_instruction_test!(BLTUInstruction(x, x), Fr::zero());
-        }
     }
-
-    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
 
     #[test]
     fn u64_parity() {
