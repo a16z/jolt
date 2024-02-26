9b9bb44730cf39501318125d65ca9221c8537cf9
sragss <sragsdale@a16z.com>
2024-02-26 14:24:27 -0700

    bge

--- jolt-core/src/jolt/instruction/bge.rs
+++ jolt-core/src/jolt/instruction/bge.rs
@@ -59,6 +59,7 @@ mod test {
     use rand_chacha::rand_core::RngCore;
 
     use crate::{jolt::instruction::JoltInstruction, jolt_instruction_test};
+    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
 
     use super::BGEInstruction;
 
@@ -72,10 +73,13 @@ mod test {
             let x = rng.next_u64() as i64;
             let y = rng.next_u64() as i64;
 
-            jolt_instruction_test!(BGEInstruction(x as u64, y as u64), (x >= y).into());
+            let instruction = BGEInstruction(x as u64, y as u64);
+            let expected = instruction.lookup_entry_u64();
+
+            jolt_instruction_test!(instruction, expected.into());
             assert_eq!(
-                BGEInstruction(x as u64, y as u64).lookup_entry::<Fr>(C, M),
-                (x >= y).into()
+                instruction.lookup_entry::<Fr>(C, M),
+                expected.into()
             );
         }
         for _ in 0..256 {
@@ -87,8 +91,6 @@ mod test {
             );
         }
     }
-
-    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
 
     #[test]
     fn u64_parity() {
