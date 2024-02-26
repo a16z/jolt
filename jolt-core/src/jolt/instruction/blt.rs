1888500ad8b8c5b227634867a0df9cece7d86f1c
sragss <sragsdale@a16z.com>
2024-02-26 14:34:46 -0700

    blt

--- jolt-core/src/jolt/instruction/blt.rs
+++ jolt-core/src/jolt/instruction/blt.rs
@@ -62,6 +62,8 @@ impl JoltInstruction for BLTInstruction {
     }
 
     fn lookup_entry_u64(&self) -> u64 {
+        assert!(((self.0 | self.1) >> 32) == 0, "Only 32-bit implemented");
+
         ((self.0 as i32) < (self.1 as i32)).into()
     }
 
@@ -78,26 +80,29 @@ mod test {
     use rand_chacha::rand_core::RngCore;
 
     use crate::{jolt::instruction::JoltInstruction, jolt_instruction_test};
+    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
 
     use super::BLTInstruction;
 
     #[test]
-    fn blt_instruction_e2e() {
+    fn blt_instruction_32_e2e() {
         let mut rng = test_rng();
-        const C: usize = 8;
+        const C: usize = 4;
         const M: usize = 1 << 16;
 
         for _ in 0..256 {
-            let x = rng.next_u64() as i64;
-            let y = rng.next_u64() as i64;
-            jolt_instruction_test!(BLTInstruction(x as u64, y as u64), (x < y).into());
+            let x = rng.next_u32();
+            let y = rng.next_u32();
+            let instruction = BLTInstruction(x as u64, y as u64);
+            let expected = instruction.lookup_entry_u64();
+            jolt_instruction_test!(instruction, expected.into());
             assert_eq!(
-                BLTInstruction(x as u64, y as u64).lookup_entry::<Fr>(C, M),
-                (x < y).into()
+                instruction.lookup_entry::<Fr>(C, M),
+                expected.into()
             );
         }
         for _ in 0..256 {
-            let x = rng.next_u64() as i64;
+            let x = rng.next_u32();
             jolt_instruction_test!(BLTInstruction(x as u64, x as u64), Fr::zero());
             assert_eq!(
                 BLTInstruction(x as u64, x as u64).lookup_entry::<Fr>(C, M),
@@ -105,8 +110,6 @@ mod test {
             );
         }
     }
-
-    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
 
     #[test]
     fn u64_parity() {
