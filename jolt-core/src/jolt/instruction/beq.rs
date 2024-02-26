6e1e129944ccc07e873ea8c15ab9bfc9f865782b
sragss <sragsdale@a16z.com>
2024-02-26 14:22:06 -0700

    beq

--- jolt-core/src/jolt/instruction/beq.rs
+++ jolt-core/src/jolt/instruction/beq.rs
@@ -48,32 +48,45 @@ mod test {
     use rand_chacha::rand_core::RngCore;
 
     use crate::{jolt::instruction::JoltInstruction, jolt_instruction_test};
+    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
 
     use super::BEQInstruction;
 
     #[test]
-    fn beq_instruction_e2e() {
+    fn beq_instruction_32_e2e() {
+        let mut rng = test_rng();
+        const C: usize = 4;
+        const M: usize = 1 << 16;
+
+        for _ in 0..256 {
+            let (x, y) = (rng.next_u32() as u64, rng.next_u32() as u64);
+            let instruction = BEQInstruction(x, y);
+            let expected = instruction.lookup_entry_u64();
+            jolt_instruction_test!(instruction, expected.into());
+            assert_eq!(
+                instruction.lookup_entry::<Fr>(C, M),
+                expected.into()
+            );
+        }
+    }
+
+    #[test]
+    fn beq_instruction_64_e2e() {
         let mut rng = test_rng();
         const C: usize = 8;
         const M: usize = 1 << 16;
 
         for _ in 0..256 {
             let (x, y) = (rng.next_u64(), rng.next_u64());
-            jolt_instruction_test!(BEQInstruction(x, y), (x == y).into());
+            let instruction = BEQInstruction(x, y);
+            let expected = instruction.lookup_entry_u64();
+            jolt_instruction_test!(instruction, expected.into());
             assert_eq!(
-                BEQInstruction(x, y).lookup_entry::<Fr>(C, M),
-                (x == y).into()
+                instruction.lookup_entry::<Fr>(C, M),
+                expected.into()
             );
         }
-        for _ in 0..256 {
-            let x = rng.next_u64();
-            jolt_instruction_test!(BEQInstruction(x, x), Fr::one());
-            assert_eq!(BEQInstruction(x, x).lookup_entry::<Fr>(C, M), Fr::one());
-        }
     }
-
-    use crate::jolt::instruction::test::{lookup_entry_u64_parity_random, lookup_entry_u64_parity};
-
     #[test]
     fn u64_parity() {
         let concrete_instruction = BEQInstruction(0, 0);
