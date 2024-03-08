d11877ff11f988be82530f69a68b922d0b55607f
sragss <sragsdale@a16z.com>
2024-03-08 15:32:39 -0800

    parallelize bytecode poly construction

--- common/src/parallel.rs
+++ common/src/parallel.rs
@@ -1,4 +1,16 @@
 #[macro_export]
+macro_rules! par_join_5 {
+    ($task1:expr, $task2:expr, $task3:expr, $task4:expr, $task5:expr) => {{
+        let ((result1, result2), ((result3, result4), result5)) =
+                rayon::join(
+                    || rayon::join($task1, $task2),
+                    || rayon::join(|| rayon::join($task3, $task4), $task5),
+                );
+        (result1, result2, result3, result4, result5)
+    }};
+}
+
+#[macro_export]
 macro_rules! par_join_8 {
     ($task1:expr, $task2:expr, $task3:expr, $task4:expr, $task5:expr, $task6:expr, $task7:expr, $task8:expr) => {{
         let (((result1, result2), (result3, result4)), ((result5, result6), (result7, result8))) =
