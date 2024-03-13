be25aab0a46d3d62afaee85eab451c3bad8d8513
sragss <sragsdale@a16z.com>
2024-03-13 16:57:19 -0700

    parallelize flattening

--- jolt-core/src/jolt/vm/mod.rs
+++ jolt-core/src/jolt/vm/mod.rs
@@ -447,7 +447,7 @@ where
         let (memreg_a_rw, memreg_v_reads, memreg_v_writes) = jolt_polynomials.read_write_memory.get_polys_r1cs();
         println!("length of memreg_a_rw and memreg_v_reads are: {} and {}", memreg_a_rw.len(), memreg_v_writes.len());
 
-        let chunks_query: Vec<F> = jolt_polynomials.instruction_lookups.dim.iter().take(C).flat_map(|poly| poly.evals()).collect();
+        let chunks_query: Vec<F> = jolt_polynomials.instruction_lookups.dim.par_iter().take(C).flat_map(|poly| poly.evals()).collect();
 
         // Flattening this out into a Vec<F> and chunking into PADDED_TRACE_LEN-sized chunks 
         // will be the exact witness vector to feed into the R1CS
