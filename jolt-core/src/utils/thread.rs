0acfdebbf74436cf590cf73a23a660ac6da41798
sragss <sragsdale@a16z.com>
2024-03-28 11:43:32 -0700

    accelerate poly_ABC calc via better alloc and chunked par mul

--- jolt-core/src/utils/thread.rs
+++ jolt-core/src/utils/thread.rs
@@ -1,5 +1,9 @@
-use std::thread;
+use std::thread::{self, JoinHandle};
 
 pub fn drop_in_background_thread<T>(data: T) where T: Send + 'static {
     thread::spawn(move || drop(data));
+}
+
+pub fn allocate_vec_in_background<T: Clone + Send + 'static>(value: T, size: usize) -> JoinHandle<Vec<T>> {
+    thread::spawn(move || vec![value; size])
 }
