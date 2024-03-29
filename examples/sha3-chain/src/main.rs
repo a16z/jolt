e908a2ab12d937feb77c9870439843e7e4e43bc8
sragss <sragsdale@a16z.com>
2024-03-29 09:22:41 -0400

    increase sha3-chain example size

--- examples/sha3-chain/src/main.rs
+++ examples/sha3-chain/src/main.rs
@@ -16,7 +16,7 @@ pub fn main() {
     let (prove_sha3_chain, verify_sha3_chain) = guest::build_sha3_chain();
 
     let input  = [5u8; 32];
-    let iters = 100;
+    let iters = 500;
     let (output, proof) = prove_sha3_chain(input, iters);
     let is_valid = verify_sha3_chain(proof);
 
