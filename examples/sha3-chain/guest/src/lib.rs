3847eaea8ebc0eb30e54a1c25335993c641c0ab1
Noah Citron <noah@jeff.org>
2024-03-29 14:14:49 +0000

    migrate hash chain examples to new sdk setup

--- examples/sha3-chain/guest/src/lib.rs
+++ examples/sha3-chain/guest/src/lib.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(feature = "guest", no_std)]
-#![cfg_attr(feature = "guest", no_main)]
+#![no_main]
 
-use sha2::{Sha256, Digest};
+use sha3::{Keccak256, Digest};
 
-#[jolt_sdk::main]
-fn sha2_chain(input: [u8; 32], num_iters: u32) -> [u8; 32] {
+#[jolt::provable]
+fn sha3_chain(input: [u8; 32], num_iters: u32) -> [u8; 32] {
     let mut hash = input;
     for _ in 0..num_iters {
-        let mut hasher = Sha256::new();
+        let mut hasher = Keccak256::new();
         hasher.update(input);
         let res = &hasher.finalize();
         hash = Into::<[u8; 32]>::into (*res);
