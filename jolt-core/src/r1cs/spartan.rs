2b2eb78d0c407f8c9f79a85988b3f6a14952acae
Arasu Arun <arasu.arun5@gmail.com>
2024-04-05 09:25:55 -0400

    restore io consistency constraints

--- jolt-core/src/r1cs/spartan.rs
+++ jolt-core/src/r1cs/spartan.rs
@@ -385,26 +385,26 @@ impl<F: PrimeField, G: CurveGroup<ScalarField = F>> UniformSpartanProof<F, G> {
                 + r_inner_sumcheck_RLC * r_inner_sumcheck_RLC * constant_term_C;
 
 
-            // /* 4. Add IO consistency constraints, which ensure that (output_pc[step i] - input_pc[step i+1]) * input_pc[step i+1] = 0
-            //     For each step i in (0..NUM_STEPS-1):
-            //     A, B: 0 
-            //     C: output of i - input of i+1 ==> (index i, value 1), (index NUM_STEPS+i+1, value -1)
-            // */            
-            // let row_io_consistency = key.shape_single_step.num_cons-1; // this is considered the last constraint
-            // let r_sq_eq_rx_con = r_sq * eq_rx_con[row_io_consistency];
-            // RLC_evals[0..key.true_num_steps-1]
-            //     .par_iter_mut()
-            //     .enumerate()
-            //     .for_each(|(i, rlc)| {
-            //         *rlc += r_sq_eq_rx_con * eq_rx_ts[i];
-            //     });
+            /* 4. Add IO consistency constraints, which ensure that (output_pc[step i] - input_pc[step i+1]) * input_pc[step i+1] = 0
+                For each step i in (0..NUM_STEPS-1):
+                A, B: 0 
+                C: output of i - input of i+1 ==> (index i, value 1), (index NUM_STEPS+i+1, value -1)
+            */            
+            let row_io_consistency = key.shape_single_step.num_cons-1; // this is considered the last constraint
+            let r_sq_eq_rx_con = r_sq * eq_rx_con[row_io_consistency];
+            RLC_evals[0..key.true_num_steps-1]
+                .par_iter_mut()
+                .enumerate()
+                .for_each(|(i, rlc)| {
+                    *rlc += r_sq_eq_rx_con * eq_rx_ts[i];
+                });
 
-            // RLC_evals[key.num_steps+1..key.num_steps+key.true_num_steps]
-            //     .par_iter_mut()
-            //     .enumerate()
-            //     .for_each(|(i, rlc)| {
-            //         *rlc -= r_sq_eq_rx_con * eq_rx_ts[i];
-            //     });
+            RLC_evals[key.num_steps+1..key.num_steps+key.true_num_steps]
+                .par_iter_mut()
+                .enumerate()
+                .for_each(|(i, rlc)| {
+                    *rlc -= r_sq_eq_rx_con * eq_rx_ts[i];
+                });
 
             RLC_evals
         };
@@ -573,10 +573,7 @@ impl<F: PrimeField, G: CurveGroup<ScalarField = F>> UniformSpartanProof<F, G> {
             (F::one() - inner_sumcheck_r[0]) * eval_W + inner_sumcheck_r[0] * eval_X
         };
 
-        // let (T_x, T_y) = rayon::join(
-        //         || EqPolynomial::new(r_x.to_vec()).evals(),
-        //         || EqPolynomial::new(inner_sumcheck_r.to_vec()).evals(),
-        //     );
+
 
         let num_steps_bits = key.num_steps.trailing_zeros();
         let (rx_con, rx_ts) =
@@ -619,7 +616,7 @@ impl<F: PrimeField, G: CurveGroup<ScalarField = F>> UniformSpartanProof<F, G> {
                     .collect()
             };
 
-        let evals = multi_evaluate_uniform(
+        let mut evals = multi_evaluate_uniform(
             &[
                 &key.shape_single_step.A,
                 &key.shape_single_step.B,
@@ -627,12 +624,16 @@ impl<F: PrimeField, G: CurveGroup<ScalarField = F>> UniformSpartanProof<F, G> {
             ]
         );
 
-        // // Handle IO consistency  
-        // let start_row_io_consistency = (key.shape_single_step.num_cons-1) * key.num_steps; 
-        // evals[2] += (0..key.true_num_steps-1)
-        //     .into_par_iter()
-        //     .map(|i| T_x[start_row_io_consistency + i] * (T_y[i] - T_y[key.num_steps+i+1]))
-        //     .sum::<F>();
+        // Handle IO consistency  
+        let (T_x, T_y) = rayon::join(
+                || EqPolynomial::new(r_x.to_vec()).evals(),
+                || EqPolynomial::new(inner_sumcheck_r.to_vec()).evals(),
+            );
+        let start_row_io_consistency = (key.shape_single_step.num_cons-1) * key.num_steps; 
+        evals[2] += (0..key.true_num_steps-1)
+            .into_par_iter()
+            .map(|i| T_x[start_row_io_consistency + i] * (T_y[i] - T_y[key.num_steps+i+1]))
+            .sum::<F>();
 
         let left_expected = evals[0]
             + r_inner_sumcheck_RLC * evals[1]
