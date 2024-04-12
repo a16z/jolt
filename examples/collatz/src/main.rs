d164b2b4e21e0e6f2a4c4de27cdc9457bdc6d95f
Michael Zhu <mchl.zhu.96@gmail.com>
2024-04-12 09:19:24 -0400

    Update Collatz host

--- examples/collatz/src/main.rs
+++ examples/collatz/src/main.rs
@@ -1,4 +1,14 @@
 pub fn main() {
+    // Prove/verify convergence for a single number:
+    let (prove_collatz_single, verify_collatz_single) = guest::build_collatz_convergence();
+
+    let (output, proof) = prove_collatz_single(19);
+    let is_valid = verify_collatz_single(proof);
+
+    println!("output: {}", output);
+    println!("valid: {}", is_valid);
+
+    // Prove/verify convergence for a range of numbers:
     let (prove_collatz_convergence, verify_collatz_convergence) =
         guest::build_collatz_convergence_range();
 
