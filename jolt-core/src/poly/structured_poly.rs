ec65c323accf414becc2c023264c774434054d93
sragss <sragsdale@a16z.com>
2024-04-17 15:02:43 -0700

    CommitmentScheme generics

--- jolt-core/src/poly/structured_poly.rs
+++ jolt-core/src/poly/structured_poly.rs
@@ -1,34 +1,61 @@
 use ark_ec::CurveGroup;
 use ark_ff::PrimeField;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use common::constants::NUM_R1CS_POLYS;
 
-use super::pedersen::PedersenGenerators;
+use super::{hyrax::{BatchedHyraxOpeningProof, HyraxCommitment, HyraxOpeningProof}, pedersen::PedersenGenerators};
 use crate::{
     lasso::memory_checking::NoPreprocessing,
     utils::{errors::ProofVerifyError, transcript::ProofTranscript},
 };
 
+pub trait CommitmentScheme {
+    type Field: PrimeField;
+    type Generators;
+    type Commitment;
+    type Proof;
+    type BatchedProof;
+
+    fn commit(&self, gens: Self::Generators) -> Self::Commitment;
+}
+
+pub struct HyraxConfig<G: CurveGroup> {
+    marker: std::marker::PhantomData<G>
+}
+
+impl<G: CurveGroup> CommitmentScheme for HyraxConfig<G> {
+    type Field = G::ScalarField; // TODO(sragss): include? or seperate field config?
+    type Generators = PedersenGenerators<G>;
+    type Commitment = HyraxCommitment<NUM_R1CS_POLYS, G>;
+    type Proof = HyraxOpeningProof<NUM_R1CS_POLYS, G>;
+    type BatchedProof = BatchedHyraxOpeningProof<NUM_R1CS_POLYS, G>;
+
+    fn commit(&self, gens: Self::Generators) -> Self::Commitment {
+        todo!("actually implement for hyrax")
+    }
+}
+
 /// Encapsulates the pattern of a collection of related polynomials (e.g. those used to
 /// prove instruction lookups in Jolt) that can be "batched" for more efficient
 /// commitments/openings.
-pub trait StructuredCommitment<G: CurveGroup>: Send + Sync + Sized {
+pub trait StructuredCommitment<C: CommitmentScheme>: Send + Sync + Sized {
     /// The batched commitment to these polynomials.
     type Commitment;
 
     /// Commits to batched polynomials, typically using `HyraxCommitment::batch_commit_polys`.
-    fn commit(&self, generators: &PedersenGenerators<G>) -> Self::Commitment;
+    fn commit(&self, generators: &C::Generators) -> Self::Commitment;
 }
 
 /// Encapsulates the pattern of opening a batched polynomial commitment at a single point.
 /// Note that there may be a one-to-many mapping from `StructuredCommitment` to `StructuredOpeningProof`:
 /// different subset of the same polynomials may be opened at different points, resulting in
 /// different opening proofs.
-pub trait StructuredOpeningProof<F, G, Polynomials>:
+pub trait StructuredOpeningProof<F, C, Polynomials>:
     Sync + CanonicalSerialize + CanonicalDeserialize
 where
     F: PrimeField,
-    G: CurveGroup<ScalarField = F>,
-    Polynomials: StructuredCommitment<G> + ?Sized,
+    C: CommitmentScheme,
+    Polynomials: StructuredCommitment<C> + ?Sized,
 {
     type Preprocessing = NoPreprocessing;
     type Proof: Sync + CanonicalSerialize + CanonicalDeserialize;
@@ -58,7 +85,7 @@ where
     /// Verifies an opening proof, given the associated polynomial `commitment` and `opening_point`.
     fn verify_openings(
         &self,
-        generators: &PedersenGenerators<G>,
+        generators: &C::Generators,
         opening_proof: &Self::Proof,
         commitment: &Polynomials::Commitment,
         opening_point: &[F],
