665cd192c3158070660f4871de4657838311ddd4
shreyas-londhe <shreyas_londhe@outlook.com>
2024-04-27 06:48:11 +0530

    fix: removed compute_native fn

--- examples/sha2-chain/src/main.rs
+++ examples/sha2-chain/src/main.rs
@@ -1,23 +1,9 @@
-use sha2::{Digest, Sha256};
-
-fn compute_native(input: [u8; 32], num_iters: u32) -> [u8; 32] {
-    let mut hash = input;
-    for _ in 0..num_iters {
-        let mut hasher = Sha256::new();
-        hasher.update(&hash);
-        let res = &hasher.finalize();
-        hash = Into::<[u8; 32]>::into(*res);
-    }
-
-    hash
-}
-
 pub fn main() {
     let (prove_sha2_chain, verify_sha2_chain) = guest::build_sha2_chain();
 
     let input = [5u8; 32];
     let iters = 100;
-    let native_output = compute_native(input, iters);
+    let native_output = guest::sha2_chain(input, iters);
     let (output, proof) = prove_sha2_chain(input, iters);
     let is_valid = verify_sha2_chain(proof);
 
