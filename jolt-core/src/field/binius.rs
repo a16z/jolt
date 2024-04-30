26f035457bc0ebe5bddd60fcdabe6bcfc598a52a
sragss <sragsdale@a16z.com>
2024-04-29 18:06:50 -0700

    obvious impls

--- jolt-core/src/field/binius.rs
+++ jolt-core/src/field/binius.rs
@@ -43,23 +43,23 @@ impl<F: binius_field::TowerField> JoltField for BiniusField<F> {
     const NUM_BYTES: usize = 16;
 
     fn random<R: rand_core::RngCore>(rng: &mut R) -> Self {
-        todo!()
+        Self(F::random(rng))
     }
 
     fn is_zero(&self) -> bool {
-        todo!()
+        self.0.is_zero().into()
     }
 
     fn is_one(&self) -> bool {
-        todo!()
+        self.0 == Self::one().0
     }
 
     fn zero() -> Self {
-        todo!()
+        Self(F::ZERO)
     }
 
     fn one() -> Self {
-        todo!()
+        Self(F::ONE)
     }
 
     fn from_u64(n: u64) -> Option<Self> {
@@ -67,7 +67,7 @@ impl<F: binius_field::TowerField> JoltField for BiniusField<F> {
     }
 
     fn square(&self) -> Self {
-        todo!()
+        Self(self.0.square())
     }
 
     fn from_bytes(bytes: &[u8]) -> Self {
@@ -79,8 +79,7 @@ impl<F: binius_field::TowerField> Neg for BiniusField<F> {
     type Output = Self;
 
     fn neg(self) -> Self {
-        // Implement the negation operation here
-        todo!()
+        BiniusField(self.0.neg())
     }
 }
 
@@ -88,8 +87,7 @@ impl<F: binius_field::TowerField> Add for BiniusField<F> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        // Implement the addition operation here
-        todo!()
+        BiniusField(self.0.add(other.0))
     }
 }
 
@@ -97,8 +95,7 @@ impl<F: binius_field::TowerField> Sub for BiniusField<F> {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
-        // Implement the subtraction operation here
-        todo!()
+        BiniusField(self.0.sub(other.0))
     }
 }
 
@@ -106,8 +103,7 @@ impl<F: binius_field::TowerField> Mul for BiniusField<F> {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
-        // Implement the multiplication operation here
-        todo!()
+        BiniusField(self.0.mul(other.0))
     }
 }
 
@@ -115,56 +111,49 @@ impl<F: binius_field::TowerField> Div for BiniusField<F> {
     type Output = Self;
 
     fn div(self, other: Self) -> Self {
-        // Implement the division operation here
         todo!()
+        // BiniusField(self.0.div(other.0))
     }
 }
 
 impl<F: binius_field::TowerField> AddAssign for BiniusField<F> {
     fn add_assign(&mut self, other: Self) {
-        // Implement the addition assignment operation here
-        todo!()
+        self.0.add_assign(other.0);
     }
 }
 
 impl<F: binius_field::TowerField> SubAssign for BiniusField<F> {
     fn sub_assign(&mut self, other: Self) {
-        // Implement the subtraction assignment operation here
-        todo!()
+        self.0.sub_assign(other.0);
     }
 }
 
 impl<F: binius_field::TowerField> MulAssign for BiniusField<F> {
     fn mul_assign(&mut self, other: Self) {
-        // Implement the multiplication assignment operation here
-        todo!()
+        self.0.mul_assign(other.0);
     }
 }
 
 impl<F: binius_field::TowerField> core::iter::Sum for BiniusField<F> {
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        // Implement the sum operation here
-        todo!()
+        iter.fold(Self::zero(), |acc, x| BiniusField(acc.0.add(x.0)))
     }
 }
 
 impl<'a, F: binius_field::TowerField> core::iter::Sum<&'a Self> for BiniusField<F> {
     fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
-        // Implement the sum operation here
-        todo!()
+        iter.fold(Self::zero(), |acc, x| BiniusField(acc.0.add(x.0)))
     }
 }
 
 impl<F: binius_field::TowerField> core::iter::Product for BiniusField<F> {
     fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
-        // Implement the product operation here
-        todo!()
+        iter.fold(Self::one(), |acc, x| BiniusField(acc.0.mul(x.0)))
     }
 }
 
 impl<'a, F: binius_field::TowerField> core::iter::Product<&'a Self> for BiniusField<F> {
     fn product<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
-        // Implement the product operation here
-        todo!()
+        iter.fold(Self::one(), |acc, x| BiniusField(acc.0.mul(x.0)))
     }
 }
