e3c83965b3fc6ee198f037d3a1453ef19df31377
sragss <sragsdale@a16z.com>
2024-05-06 13:09:14 -0700

    BiniusField: add u64 construction

--- jolt-core/src/field/binius.rs
+++ jolt-core/src/field/binius.rs
@@ -1,14 +1,38 @@
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use binius_field::{BinaryField128b, BinaryField128bPolyval};
 use std::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign};
 
 use super::JoltField;
 
+// TODO(sragss): Likely don't need both Pod and BiniusConstructable. Most of the binius types
+// already implement pod: https://gitlab.com/search?search=pod&nav_source=navbar&project_id=52331778&group_id=78402358&search_code=true&repository_ref=main
+
+
+impl BiniusConstructable for BinaryField128b {
+    fn new(n: u64) -> Self {
+        Self::new(n as u128)
+    }
+}
+
+impl BiniusConstructable for BinaryField128bPolyval {
+    fn new(n: u64) -> Self {
+        Self::new(n as u128)
+    }
+}
+
+impl BiniusGeneric for BinaryField128b {}
+impl BiniusGeneric for BinaryField128bPolyval {}
+
+pub trait BiniusGeneric: binius_field::TowerField + BiniusConstructable + bytemuck::Pod {}
+
+pub trait BiniusConstructable {
+    fn new(n: u64) -> Self;
+}
+
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq)]
-pub struct BiniusField<F: binius_field::TowerField>(F);
+pub struct BiniusField<F: BiniusGeneric>(F);
 
-impl<F: binius_field::TowerField> BiniusField<F> {}
-
-impl<F: binius_field::TowerField> JoltField for BiniusField<F> {
+impl<F: BiniusGeneric> JoltField for BiniusField<F> {
     const NUM_BYTES: usize = 16;
 
     fn random<R: rand_core::RngCore>(rng: &mut R) -> Self {
@@ -32,7 +56,9 @@ impl<F: binius_field::TowerField> JoltField for BiniusField<F> {
     }
 
     fn from_u64(n: u64) -> Option<Self> {
-        todo!()
+        // let field_element = bytemuck::cast::<u64, F>(n);
+        // Some(Self(field_element))
+        Some(Self(F::new(n)))
     }
 
     fn square(&self) -> Self {
@@ -40,11 +66,14 @@ impl<F: binius_field::TowerField> JoltField for BiniusField<F> {
     }
 
     fn from_bytes(bytes: &[u8]) -> Self {
-        todo!()
+        assert_eq!(bytes.len(), Self::NUM_BYTES);
+
+        let field_element = bytemuck::try_from_bytes::<F>(bytes).unwrap();
+        Self(field_element.to_owned())
     }
 }
 
-impl<F: binius_field::TowerField> Neg for BiniusField<F> {
+impl<F: BiniusGeneric> Neg for BiniusField<F> {
     type Output = Self;
 
     fn neg(self) -> Self {
@@ -52,7 +81,7 @@ impl<F: binius_field::TowerField> Neg for BiniusField<F> {
     }
 }
 
-impl<F: binius_field::TowerField> Add for BiniusField<F> {
+impl<F: BiniusGeneric> Add for BiniusField<F> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
@@ -60,7 +89,7 @@ impl<F: binius_field::TowerField> Add for BiniusField<F> {
     }
 }
 
-impl<F: binius_field::TowerField> Sub for BiniusField<F> {
+impl<F: BiniusGeneric> Sub for BiniusField<F> {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
@@ -68,7 +97,7 @@ impl<F: binius_field::TowerField> Sub for BiniusField<F> {
     }
 }
 
-impl<F: binius_field::TowerField> Mul for BiniusField<F> {
+impl<F: BiniusGeneric> Mul for BiniusField<F> {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
@@ -76,7 +105,7 @@ impl<F: binius_field::TowerField> Mul for BiniusField<F> {
     }
 }
 
-impl<F: binius_field::TowerField> Div for BiniusField<F> {
+impl<F: BiniusGeneric> Div for BiniusField<F> {
     type Output = Self;
 
     fn div(self, other: Self) -> Self {
@@ -85,63 +114,65 @@ impl<F: binius_field::TowerField> Div for BiniusField<F> {
     }
 }
 
-impl<F: binius_field::TowerField> AddAssign for BiniusField<F> {
+impl<F: BiniusGeneric> AddAssign for BiniusField<F> {
     fn add_assign(&mut self, other: Self) {
         self.0.add_assign(other.0);
     }
 }
 
-impl<F: binius_field::TowerField> SubAssign for BiniusField<F> {
+impl<F: BiniusGeneric> SubAssign for BiniusField<F> {
     fn sub_assign(&mut self, other: Self) {
         self.0.sub_assign(other.0);
     }
 }
 
-impl<F: binius_field::TowerField> MulAssign for BiniusField<F> {
+impl<F: BiniusGeneric> MulAssign for BiniusField<F> {
     fn mul_assign(&mut self, other: Self) {
         self.0.mul_assign(other.0);
     }
 }
 
-impl<F: binius_field::TowerField> core::iter::Sum for BiniusField<F> {
+impl<F: BiniusGeneric> core::iter::Sum for BiniusField<F> {
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
         iter.fold(Self::zero(), |acc, x| BiniusField(acc.0.add(x.0)))
     }
 }
 
-impl<'a, F: binius_field::TowerField> core::iter::Sum<&'a Self> for BiniusField<F> {
+impl<'a, F: BiniusGeneric> core::iter::Sum<&'a Self> for BiniusField<F> {
     fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
         iter.fold(Self::zero(), |acc, x| BiniusField(acc.0.add(x.0)))
     }
 }
 
-impl<F: binius_field::TowerField> core::iter::Product for BiniusField<F> {
+impl<F: BiniusGeneric> core::iter::Product for BiniusField<F> {
     fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
         iter.fold(Self::one(), |acc, x| BiniusField(acc.0.mul(x.0)))
     }
 }
 
-impl<'a, F: binius_field::TowerField> core::iter::Product<&'a Self> for BiniusField<F> {
+impl<'a, F: BiniusGeneric> core::iter::Product<&'a Self> for BiniusField<F> {
     fn product<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
         iter.fold(Self::one(), |acc, x| BiniusField(acc.0.mul(x.0)))
     }
 }
 
-impl<F: binius_field::TowerField> CanonicalSerialize for BiniusField<F> {
-    fn serialize_with_mode<W: std::io::prelude::Write>(
+impl<F: BiniusGeneric> CanonicalSerialize for BiniusField<F> {
+    fn serialize_with_mode<W: ark_std::io::Write>(
         &self,
-        writer: W,
-        compress: ark_serialize::Compress,
+        mut writer: W,
+        _compress: ark_serialize::Compress,
     ) -> Result<(), ark_serialize::SerializationError> {
-        todo!()
+            let bytes = bytemuck::bytes_of(&self.0);
+            writer.write_all(&bytes)?;
+            Ok(())
     }
 
-    fn serialized_size(&self, compress: ark_serialize::Compress) -> usize {
-        todo!()
+    fn serialized_size(&self, _compress: ark_serialize::Compress) -> usize {
+        Self::NUM_BYTES
     }
 }
 
-impl<F: binius_field::TowerField> CanonicalDeserialize for BiniusField<F> {
+impl<F: BiniusGeneric> CanonicalDeserialize for BiniusField<F> {
     // Required method
     fn deserialize_with_mode<R: std::io::prelude::Read>(
         reader: R,
@@ -152,7 +183,7 @@ impl<F: binius_field::TowerField> CanonicalDeserialize for BiniusField<F> {
     }
 }
 
-impl<F: binius_field::TowerField> ark_serialize::Valid for BiniusField<F> {
+impl<F: BiniusGeneric> ark_serialize::Valid for BiniusField<F> {
     fn check(&self) -> Result<(), ark_serialize::SerializationError> {
         todo!()
     }
