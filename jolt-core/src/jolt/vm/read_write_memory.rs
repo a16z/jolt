9fea23cb974b521961001a6413801a9b1443279e
aleph_v <20227705+aleph-v@users.noreply.github.com>
2024-05-10 19:24:17 +0200

    trailing zeros -> log_2

--- jolt-core/src/jolt/vm/read_write_memory.rs
+++ jolt-core/src/jolt/vm/read_write_memory.rs
@@ -1622,7 +1622,7 @@ where
         let memory_layout = &preprocessing.program_io.as_ref().unwrap().memory_layout;
 
         let nonzero_memory_size = memory_layout.ram_witness_offset as usize;
-        let log_nonzero_memory_size = nonzero_memory_size.trailing_zeros() as usize;
+        let log_nonzero_memory_size = nonzero_memory_size.log_2() as usize;
         assert!(
             nonzero_memory_size.is_power_of_two(),
             "Ram witness offset must be a power of two"
