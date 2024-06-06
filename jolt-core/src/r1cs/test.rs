96107cf9a34ca1af4bd4abec43cc79a2d4bfd4ef
sragss <sragsdale@a16z.com>
2024-06-06 11:02:41 -0400

    fix tests

--- jolt-core/src/r1cs/test.rs
+++ jolt-core/src/r1cs/test.rs
@@ -1,6 +1,6 @@
 use crate::{
     impl_r1cs_input_lc_conversions,
-    poly::field::JoltField,
+    field::JoltField,
     r1cs::builder::{OffsetEqConstraint, R1CSBuilder, R1CSConstraintBuilder},
 };
 
