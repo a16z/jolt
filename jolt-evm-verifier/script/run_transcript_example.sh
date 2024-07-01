4107af6751c930c8462b6eba74a4e32151ef8c08
aleph_v <20227705+aleph-v@users.noreply.github.com>
2024-07-01 12:51:42 +0200

    dev to nul

--- jolt-evm-verifier/script/run_transcript_example.sh
+++ jolt-evm-verifier/script/run_transcript_example.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cargo build --manifest-path script/Cargo.toml --release --quiet
+cargo build --manifest-path script/Cargo.toml --release --quiet 2> /dev/null 
 
 ./script/target/release/transcript_example ${1:-proofs}
