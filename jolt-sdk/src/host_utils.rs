967f795f673c1fef9461a42b4bdf4386521230b0
PatStiles <pdstiles78@gmail.com>
2024-07-02 20:16:38 -0700

    mv'd proof back to vm

--- jolt-sdk/src/host_utils.rs
+++ jolt-sdk/src/host_utils.rs
@@ -2,11 +2,6 @@ pub use ark_bn254::{Fr as F, G1Projective as G};
 pub use ark_ec::CurveGroup;
 pub use jolt_core::{poly::commitment::hyrax::HyraxScheme, field::JoltField};
 
-use eyre::Result;
-use std::fs::File;
-use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
-use std::path::PathBuf;
-
 pub use common::{
     constants::MEMORY_OPS_PER_INSTRUCTION,
     rv_trace::{MemoryOp, RV32IM},
@@ -19,33 +14,3 @@ pub use jolt_core::jolt::vm::{
     Jolt, JoltCommitments, JoltPreprocessing, JoltProof,
 };
 pub use tracer;
-
-pub type ProofPCS = HyraxScheme<G>;
-
-#[derive(CanonicalSerialize, CanonicalDeserialize)]
-pub struct Proof {
-    pub proof: RV32IJoltProof<F, ProofPCS>,
-    pub commitments: JoltCommitments<ProofPCS>,
-}
-
-impl Proof {
-    /// Gets the byte size of the full proof
-    pub fn size(&self) -> Result<usize> {
-        let mut buffer = Vec::new();
-        self.serialize_compressed(&mut buffer)?;
-        Ok(buffer.len())
-    }
-
-    /// Saves the proof to a file
-    pub fn save_to_file<P: Into<PathBuf>>(&self, path: P) -> Result<()> {
-        let file = File::create(path.into())?;
-        self.serialize_compressed(file)?;
-        Ok(())
-    }
-
-    /// Reads a proof from a file
-    pub fn from_file<P: Into<PathBuf>>(path: P) -> Result<Self> {
-        let file = File::open(path.into())?;
-        Ok(Proof::deserialize_compressed(file)?)
-    }
-}
