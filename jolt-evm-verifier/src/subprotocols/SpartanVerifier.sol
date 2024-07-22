0669d29c3edeae154b913198e72c9d7cc8aea267
aleph_v <20227705+aleph-v@users.noreply.github.com>
2024-07-22 16:26:37 +0200

    sumcheck refactor and start of spartan verifier

---
+++ jolt-evm-verifier/src/subprotocols/SpartanVerifier.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {Transcript, FiatShamirTranscript} from "./FiatShamirTranscript.sol";
+import {MODULUS, Fr, FrLib} from "../reference/Fr.sol";
+import {SumcheckVerifier, SumcheckInstanceProof} from "./SumcheckVerifier.sol";
+import {HyperKZG, HyperKZGProof} from "./HyperKZG.sol";
+
+struct SpartanProof {
+    SumcheckInstanceProof outer;
+    uint256 outerClaim0;
+    uint256 outerClaim1;
+    uint256 outerClaim2;
+    SumcheckInstanceProof inner;
+    uint256[] claimedEvals;
+    HyperKZGProof proof;
+}
+
+contract SpartanVerifier is HyperKZG {
+    using FiatShamirTranscript for Transcript;
+    using FrLib for Fr;
+    using SumcheckVerifier for SumcheckInstanceProof;
+
+
+    function verifySpartanR1CS(SpartanProof memory proof) internal pure {
+
+    }
+}
