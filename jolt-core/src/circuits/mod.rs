f1e7f393aa16b3ff285ba09b7ceabc4fb773e821
Ivan Mikushin <imikushin@users.noreply.github.com>
2024-08-03 18:58:48 -0700

    WIP: LoadedSNARK trait

--- jolt-core/src/circuits/mod.rs
+++ jolt-core/src/circuits/mod.rs
@@ -10,11 +10,12 @@ use ark_r1cs_std::{
     fields::nonnative::AllocatedNonNativeFieldVar,
 };
 use ark_relations::r1cs::ConstraintSynthesizer;
-use ark_serialize::CanonicalSerialize;
+use ark_serialize::{CanonicalSerialize, SerializationError, Valid};
 use ark_std::{
     iterable::Iterable,
     ops::Neg,
     rand::{CryptoRng, RngCore},
+    result::Result,
     One, Zero,
 };
 
@@ -62,6 +63,20 @@ where
     pub delayed_msms: Vec<DelayedMSMDef>,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum LoadedSNARKError<E, S>
+where
+    E: Pairing,
+    S: SNARK<E::ScalarField>,
+{
+    /// Wraps `S::Error`.
+    #[error(transparent)]
+    SNARKError(S::Error),
+    /// Wraps `SerializationError`.
+    #[error(transparent)]
+    SerializationError(#[from] SerializationError),
+}
+
 pub trait LoadedSNARK<E, P, S>
 where
     E: Pairing<G1Affine = Affine<P>, BaseField = P::BaseField, ScalarField = P::ScalarField>,
@@ -74,19 +89,23 @@ where
         circuit_pk: &S::ProvingKey,
         circuit: Self::Circuit,
         rng: &mut R,
-    ) -> Result<S::Proof, S::Error>;
+    ) -> Result<S::Proof, LoadedSNARKError<E, S>>;
 
     fn msm_inputs(
         msm_defs: &[DelayedMSMDef],
         public_input: &[E::ScalarField],
-    ) -> Result<Vec<(Vec<E::G1Affine>, Vec<E::ScalarField>)>, S::Error> {
+    ) -> Result<Vec<(Vec<E::G1Affine>, Vec<E::ScalarField>)>, SerializationError> {
         msm_defs
             .iter()
             .map(|msm_def| {
                 let g1_offset = msm_def.g1_offset;
                 let msm_length = msm_def.length;
-                assert!(msm_length > 1);
-                let g1s = Self::g1_elements(public_input, g1_offset, msm_length);
+                assert!(msm_length > 1); // TODO make it a verifier key validity error
+                let g1s = Self::g1_elements(public_input, g1_offset, msm_length)?;
+
+                if public_input.len() < msm_def.scalar_offset + msm_length - 1 {
+                    return Err(SerializationError::InvalidData);
+                };
                 let scalars = [
                     &public_input[msm_def.scalar_offset..msm_def.scalar_offset + msm_length - 1],
                     &[-E::ScalarField::one()],
@@ -101,8 +120,12 @@ where
         public_input: &[E::ScalarField],
         g1_offset: usize,
         length: usize,
-    ) -> Vec<E::G1Affine> {
+    ) -> Result<Vec<E::G1Affine>, SerializationError> {
         let g1_element_size = g1_affine_size_in_scalar_field_elements::<E>();
+        if public_input.len() < g1_offset + length * g1_element_size {
+            return Err(SerializationError::InvalidData);
+        };
+
         public_input[g1_offset..g1_offset + length * g1_element_size]
             .chunks(g1_element_size)
             .map(|chunk| g1_affine_from_scalar_field::<E, P>(chunk))
@@ -113,20 +136,20 @@ where
         vk: &LoadedSNARKVerifyingKey<E, S>,
         public_input: &[E::ScalarField],
         proof: &S::Proof,
-    ) -> Result<Vec<(Vec<E::G1>, Vec<E::G2>)>, S::Error> {
+    ) -> Result<Vec<(Vec<E::G1>, Vec<E::G2>)>, SerializationError> {
         let g1_vectors = vk
             .delayed_pairings
             .iter()
             .map(|pairing_def| {
-                let l_g1 = Self::g1_elements(public_input, pairing_def.l_g1_offset, 1)[0];
-                let r_g1 = Self::g1_elements(public_input, pairing_def.r_g1_offset, 1)[0];
+                let l_g1 = Self::g1_elements(public_input, pairing_def.l_g1_offset, 1)?[0];
+                let r_g1 = Self::g1_elements(public_input, pairing_def.r_g1_offset, 1)?[0];
 
-                vec![l_g1.into(), (-r_g1).into()]
+                Ok(vec![l_g1.into(), (-r_g1).into()])
             })
-            .collect::<Vec<Vec<E::G1>>>();
-        Ok(g1_vectors
+            .collect::<Result<Vec<Vec<E::G1>>, SerializationError>>();
+        Ok(g1_vectors?
             .into_iter()
-            .zip(Self::g2_elements(vk, public_input, proof))
+            .zip(Self::g2_elements(vk, public_input, proof)?)
             .collect())
     }
 
@@ -134,14 +157,15 @@ where
         vk: &LoadedSNARKVerifyingKey<E, S>,
         public_input: &[<E as Pairing>::ScalarField],
         proof: &S::Proof,
-    ) -> Vec<Vec<E::G2>>;
+    ) -> Result<Vec<Vec<E::G2>>, SerializationError>;
 
     fn verify(
         vk: &LoadedSNARKVerifyingKey<E, S>,
         public_input: &[E::ScalarField],
         proof: &S::Proof,
-    ) -> Result<bool, S::Error> {
-        let r = S::verify_with_processed_vk(&vk.snark_pvk, public_input, proof)?;
+    ) -> Result<bool, LoadedSNARKError<E, S>> {
+        let r = S::verify_with_processed_vk(&vk.snark_pvk, public_input, proof)
+            .map_err(|e| LoadedSNARKError::SNARKError(e))?;
         if !r {
             return Ok(false);
         }
@@ -177,11 +201,18 @@ fn g1_affine_size_in_scalar_field_elements<E: Pairing>() -> usize {
     params.num_limbs * 2 + 1
 }
 
-fn g1_affine_from_scalar_field<E, P>(s: &[E::ScalarField]) -> E::G1Affine
+fn g1_affine_from_scalar_field<E, P>(
+    s: &[E::ScalarField],
+) -> Result<E::G1Affine, SerializationError>
 where
     E: Pairing<G1Affine = Affine<P>, BaseField = P::BaseField, ScalarField = P::ScalarField>,
     P: SWCurveConfig<BaseField: PrimeField>,
 {
+    let infinity = !s[s.len() - 1].is_zero();
+    if infinity {
+        return Ok(E::G1Affine::zero());
+    }
+
     let base_field_size_in_limbs = (s.len() - 1) / 2;
     let x = AllocatedNonNativeFieldVar::<E::BaseField, E::ScalarField>::limbs_to_value(
         s[..base_field_size_in_limbs].to_vec(),
@@ -191,7 +222,12 @@ where
         s[base_field_size_in_limbs..s.len() - 1].to_vec(),
         OptimizationType::Weight,
     );
-    let infinity = !s[s.len() - 1].is_zero();
 
-    Affine { x, y, infinity }
+    let affine = Affine {
+        x,
+        y,
+        infinity: false,
+    };
+    affine.check()?;
+    Ok(affine)
 }
