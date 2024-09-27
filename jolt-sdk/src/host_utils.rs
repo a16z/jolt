7a466f960b378c1bcd2ebd04f93735df34c9a379
mycelia <5660109+myceliaspy@users.noreply.github.com>
2024-09-28 00:00:32 +0200

    never heard of fmt in my life

--- jolt-sdk/src/host_utils.rs
+++ jolt-sdk/src/host_utils.rs
@@ -10,7 +10,7 @@ pub use jolt_core::host;
 pub use jolt_core::jolt::instruction;
 pub use jolt_core::jolt::vm::{
     bytecode::BytecodeRow,
-    rv32i_vm::{JoltHyperKZGProof, RV32IJoltProof, RV32IJoltVM, PCS, RV32I, Serializable},
+    rv32i_vm::{JoltHyperKZGProof, RV32IJoltProof, RV32IJoltVM, Serializable, PCS, RV32I},
     Jolt, JoltCommitments, JoltPreprocessing, JoltProof,
 };
 pub use tracer;
