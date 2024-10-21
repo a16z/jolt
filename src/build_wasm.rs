f29cf748014bc004546b1b769c91b3caf1bf682d
Sagar Dhawan <sdhawan@a16z.com>
2024-10-21 13:59:59 -0500

    fix: WASM build failures because of binius

--- src/build_wasm.rs
+++ src/build_wasm.rs
@@ -53,8 +53,9 @@ fn preprocess_and_save(func_name: &str, attributes: &Attributes, is_std: bool) -
 }
 
 fn extract_provable_functions() -> Vec<FunctionAttributes> {
-    let content = fs::read_to_string("guest/src/lib.rs").expect("Unable to read file");
-    let syntax: syn::File = syn::parse_file(&content).expect("Unable to parse file");
+    let guest_path = Path::new("guest/src/lib.rs");
+    let content = fs::read_to_string(guest_path).expect(&format!("Unable to read file: {:?}", guest_path));
+    let syntax: syn::File = syn::parse_file(&content).expect(&format!("Unable to parse file: {:?}", guest_path));
 
     syntax
         .items
