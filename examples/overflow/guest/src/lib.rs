58cabcccd19d664f7df5d920af8fdbff13f35c3f
4rgon4ut <egorlaptev98@gmail.com>
2024-10-23 22:42:09 +0400

    feat(example): add heap example and valid case

--- examples/overflow/guest/src/lib.rs
+++ examples/overflow/guest/src/lib.rs
@@ -1,12 +1,27 @@
 #![cfg_attr(feature = "guest", no_std)]
 #![no_main]
 
-const ARRAY_SIZE: usize = 2048;
+extern crate alloc;
+use alloc::vec;
+use alloc::vec::Vec;
 
 #[jolt::provable(stack_size = 1024)]
-fn overflow_stack() -> u32 {
-    let arr = [1; ARRAY_SIZE];
+fn allocate_stack() -> u32 {
+    let arr = [1u32; 1024];
+    arr.iter().sum()
+}
 
-    let sum = arr.iter().fold(0u32, |acc, &x| acc.wrapping_add(x));
-    sum
+#[jolt::provable(stack_size = 8192)]
+fn allocate_stack_with_increased_size() -> u32 {
+    allocate_stack()
+}
+
+#[jolt::provable(memory_size = 4096)]
+fn allocate_heap() -> u32 {
+    let mut vectors = Vec::new();
+
+    loop {
+        let v = vec![1u32; 1024];
+        vectors.extend(v);
+    }
 }
