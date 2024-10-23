45b5246299337f81f99180321685723d70db0f4b
4rgon4ut <egorlaptev98@gmail.com>
2024-10-23 22:49:49 +0400

    chore(examples): fix naming

--- examples/overflow/src/main.rs
+++ examples/overflow/src/main.rs
@@ -2,24 +2,24 @@ use std::any::Any;
 use std::panic;
 
 pub fn main() {
-    let (prove_allocate_stack, _) = guest::build_allocate_stack();
+    let (prove_overflow_stack, _) = guest::build_overflow_stack();
 
     let res = panic::catch_unwind(|| {
         // trying to allocate 1024 elems array and sum it up
         // with stack_size=1024, should panic
-        let (_, _) = prove_allocate_stack();
+        let (_, _) = prove_overflow_stack();
     });
     handle_result(res);
 
     // now lets try to overflow the heap, should also panic
-    let (prove_allocate_heap, _) = guest::build_allocate_heap();
+    let (prove_overflow_heap, _) = guest::build_overflow_heap();
 
     let res = panic::catch_unwind(|| {
-        let (_, _) = prove_allocate_heap();
+        let (_, _) = prove_overflow_heap();
     });
     handle_result(res);
 
-    // valid case for stack allocation, calls allocate_stack() under the hood
+    // valid case for stack allocation, calls overflow_stack() under the hood
     // but with stack_size=8192
     let (prove_allocate_stack_with_increased_size, verfiy_allocate_stack_with_increased_size) =
         guest::build_allocate_stack_with_increased_size();
@@ -34,7 +34,7 @@ pub fn main() {
 fn handle_result(res: Result<(), Box<dyn Any + Send>>) {
     match &res {
         Err(e) => match e.downcast_ref::<String>() {
-            Some(msg) => println!("Panic occurred with message: {}\n", msg),
+            Some(msg) => println!("> Panic occurred with message: {}\n", msg),
             _ => (),
         },
         _ => (),
