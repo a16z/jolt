e7e56aad1e4e51a9880f9282b8b1ec2b588b81d6
Roy Rotstein <roy.rotstein87@gmail.com>
2025-01-13 23:28:15 -0800

    added comments for jolt-core/jolt/instruction/ecall.rs

---
+++ jolt-core/src/jolt/instruction/ecall.rs
@@ -0,0 +1,3 @@
+// Implementation of the ECALL instruction.
+// Reuse existing advice instruction (16x for each precompile word in the precompile output).
+// Last instruction is a call to the precompile based on t0 register. 
