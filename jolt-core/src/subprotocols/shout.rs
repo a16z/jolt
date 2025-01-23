cc7db748daa8833dc257fe8c40c9163909df947d
Michael Zhu <mchl.zhu.96@gmail.com>
2025-01-23 16:22:46 -0500

    Hamming weight 1 sumcheck

--- jolt-core/src/subprotocols/shout.rs
+++ jolt-core/src/subprotocols/shout.rs
@@ -55,7 +55,7 @@ pub fn prove_shout<F: JoltField, ProofTranscript: Transcript>(
 
     const DEGREE: usize = 2;
     let mut compressed_polys: Vec<CompressedUniPoly<F>> = Vec::with_capacity(num_rounds);
-    for _round in 0..num_rounds {
+    for _ in 0..num_rounds {
         let univariate_poly_evals: [F; 2] = (0..ra.len() / 2)
             .into_par_iter()
             .map(|i| {
@@ -100,11 +100,72 @@ pub fn prove_shout<F: JoltField, ProofTranscript: Transcript>(
     )
 }
 
+/// Implements the sumcheck prover for the Hamming weight 1 check in step 5 of
+/// Figure 6 in the Twist+Shout paper.
+pub fn prove_hamming_weight<F: JoltField, ProofTranscript: Transcript>(
+    lookup_table: Vec<F>,
+    read_addresses: Vec<usize>,
+    r_cycle_prime: Vec<F>,
+    transcript: &mut ProofTranscript,
+) -> (SumcheckInstanceProof<F, ProofTranscript>, Vec<F>, F) {
+    let K = lookup_table.len();
+    let T = read_addresses.len();
+    debug_assert_eq!(T.log_2(), r_cycle_prime.len());
+
+    let num_rounds = K.log_2();
+    let mut r_address_double_prime: Vec<F> = Vec::with_capacity(num_rounds);
+
+    let E: Vec<F> = EqPolynomial::evals(&r_cycle_prime);
+    let F: Vec<_> = (0..K)
+        .into_par_iter()
+        .map(|k| {
+            read_addresses
+                .iter()
+                .enumerate()
+                .filter_map(|(cycle, address)| if *address == k { Some(E[cycle]) } else { None })
+                .sum::<F>()
+        })
+        .collect();
+
+    let mut ra = MultilinearPolynomial::from(F);
+    let mut previous_claim = F::one();
+
+    let mut compressed_polys: Vec<CompressedUniPoly<F>> = Vec::with_capacity(num_rounds);
+    for _ in 0..num_rounds {
+        let univariate_poly_eval: F = (0..ra.len() / 2)
+            .into_par_iter()
+            .map(|i| ra.get_bound_coeff(2 * i))
+            .sum();
+
+        let univariate_poly =
+            UniPoly::from_evals(&[univariate_poly_eval, previous_claim - univariate_poly_eval]);
+
+        let compressed_poly = univariate_poly.compress();
+        compressed_poly.append_to_transcript(transcript);
+        compressed_polys.push(compressed_poly);
+
+        let r_j = transcript.challenge_scalar::<F>();
+        r_address_double_prime.push(r_j);
+
+        previous_claim = univariate_poly.evaluate(&r_j);
+
+        ra.bind(r_j, BindingOrder::LowToHigh);
+    }
+
+    let ra_claim = ra.final_sumcheck_claim();
+    (
+        SumcheckInstanceProof::new(compressed_polys),
+        r_address_double_prime,
+        ra_claim,
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::utils::transcript::KeccakTranscript;
     use ark_bn254::Fr;
+    use ark_ff::One;
     use ark_std::test_rng;
     use rand_core::RngCore;
 
@@ -134,6 +195,40 @@ mod tests {
             2,
             &mut verifier_transcript,
         );
+        assert!(
+            verification_result.is_ok(),
+            "Verification failed with error: {:?}",
+            verification_result.err()
+        );
+    }
+
+    #[test]
+    fn hamming_weight_sumcheck() {
+        const TABLE_SIZE: usize = 64;
+        const NUM_LOOKUPS: usize = 1 << 10;
+
+        let mut rng = test_rng();
+
+        let lookup_table: Vec<Fr> = (0..TABLE_SIZE).map(|_| Fr::random(&mut rng)).collect();
+        let read_addresses: Vec<usize> = (0..NUM_LOOKUPS)
+            .map(|_| rng.next_u32() as usize % TABLE_SIZE)
+            .collect();
+
+        let mut prover_transcript = KeccakTranscript::new(b"test_transcript");
+        let r_cycle_prime: Vec<Fr> = prover_transcript.challenge_vector(NUM_LOOKUPS.log_2());
+        let (sumcheck_proof, _, _) = prove_hamming_weight(
+            lookup_table,
+            read_addresses,
+            r_cycle_prime,
+            &mut prover_transcript,
+        );
+
+        let mut verifier_transcript = KeccakTranscript::new(b"test_transcript");
+        verifier_transcript.compare_to(prover_transcript);
+        let _: Vec<Fr> = verifier_transcript.challenge_vector(NUM_LOOKUPS.log_2());
+
+        let verification_result =
+            sumcheck_proof.verify(Fr::one(), TABLE_SIZE.log_2(), 1, &mut verifier_transcript);
         assert!(
             verification_result.is_ok(),
             "Verification failed with error: {:?}",
