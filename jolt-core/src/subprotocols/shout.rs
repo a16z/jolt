d03b8ca316850fa6facbb2a86f7ae876224cad40
Michael Zhu <mchl.zhu.96@gmail.com>
2025-01-23 12:30:22 -0500

    core PIOP sumcheck for Shout d=1

---
+++ jolt-core/src/subprotocols/shout.rs
@@ -0,0 +1,143 @@
+use crate::{
+    field::JoltField,
+    poly::{
+        eq_poly::EqPolynomial,
+        multilinear_polynomial::{
+            BindingOrder, MultilinearPolynomial, PolynomialBinding, PolynomialEvaluation,
+        },
+        unipoly::{CompressedUniPoly, UniPoly},
+    },
+    utils::{
+        math::Math,
+        transcript::{AppendToTranscript, Transcript},
+    },
+};
+use rayon::prelude::*;
+
+use super::sumcheck::SumcheckInstanceProof;
+
+/// Implements the sumcheck prover for the core Shout PIOP when d = 1. See
+/// Figure 5 from the Twist+Shout paper.
+pub fn prove_shout<F: JoltField, ProofTranscript: Transcript>(
+    lookup_table: Vec<F>,
+    read_addresses: Vec<usize>,
+    transcript: &mut ProofTranscript,
+) -> (SumcheckInstanceProof<F, ProofTranscript>, Vec<F>, F, F) {
+    let K = lookup_table.len();
+    let T = read_addresses.len();
+    let r_cycle: Vec<F> = transcript.challenge_vector(T.log_2());
+
+    // Sumcheck for the core Shout PIOP (Figure 5)
+    let num_rounds = K.log_2();
+    let mut r_address: Vec<F> = Vec::with_capacity(num_rounds);
+
+    let E: Vec<F> = EqPolynomial::evals(&r_cycle);
+    let F: Vec<_> = (0..K)
+        .into_par_iter()
+        .map(|k| {
+            read_addresses
+                .iter()
+                .enumerate()
+                .filter_map(|(cycle, address)| if *address == k { Some(E[cycle]) } else { None })
+                .sum::<F>()
+        })
+        .collect();
+
+    let sumcheck_claim: F = F
+        .par_iter()
+        .zip(lookup_table.par_iter())
+        .map(|(&ra, &val)| ra * val)
+        .sum();
+    let mut previous_claim = sumcheck_claim;
+
+    let mut ra = MultilinearPolynomial::from(F);
+    let mut val = MultilinearPolynomial::from(lookup_table);
+
+    const DEGREE: usize = 2;
+    let mut compressed_polys: Vec<CompressedUniPoly<F>> = Vec::with_capacity(num_rounds);
+    for _round in 0..num_rounds {
+        let univariate_poly_evals: [F; 2] = (0..ra.len() / 2)
+            .into_par_iter()
+            .map(|i| {
+                let ra_evals = ra.sumcheck_evals(i, DEGREE, BindingOrder::LowToHigh);
+                let val_evals = val.sumcheck_evals(i, DEGREE, BindingOrder::LowToHigh);
+
+                [ra_evals[0] * val_evals[0], ra_evals[1] * val_evals[1]]
+            })
+            .reduce(
+                || [F::zero(); 2],
+                |running, new| [running[0] + new[0], running[1] + new[1]],
+            );
+
+        let univariate_poly = UniPoly::from_evals(&[
+            univariate_poly_evals[0],
+            previous_claim - univariate_poly_evals[0],
+            univariate_poly_evals[1],
+        ]);
+
+        let compressed_poly = univariate_poly.compress();
+        compressed_poly.append_to_transcript(transcript);
+        compressed_polys.push(compressed_poly);
+
+        let r_j = transcript.challenge_scalar::<F>();
+        r_address.push(r_j);
+
+        previous_claim = univariate_poly.evaluate(&r_j);
+
+        // Bind polynomials
+        rayon::join(
+            || ra.bind(r_j, BindingOrder::LowToHigh),
+            || val.bind(r_j, BindingOrder::LowToHigh),
+        );
+    }
+
+    let ra_claim = ra.final_sumcheck_claim();
+    (
+        SumcheckInstanceProof::new(compressed_polys),
+        r_address,
+        sumcheck_claim,
+        ra_claim,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::transcript::KeccakTranscript;
+    use ark_bn254::Fr;
+    use ark_std::test_rng;
+    use rand_core::RngCore;
+
+    #[test]
+    fn core_shout_sumcheck() {
+        const TABLE_SIZE: usize = 64;
+        const NUM_LOOKUPS: usize = 1 << 10;
+
+        let mut rng = test_rng();
+
+        let lookup_table: Vec<Fr> = (0..TABLE_SIZE).map(|_| Fr::random(&mut rng)).collect();
+        let read_addresses: Vec<usize> = (0..NUM_LOOKUPS)
+            .map(|_| rng.next_u32() as usize % TABLE_SIZE)
+            .collect();
+
+        let mut prover_transcript = KeccakTranscript::new(b"test_transcript");
+        let (sumcheck_proof, _, sumcheck_claim, _) =
+            prove_shout(lookup_table, read_addresses, &mut prover_transcript);
+
+        let mut verifier_transcript = KeccakTranscript::new(b"test_transcript");
+        verifier_transcript.compare_to(prover_transcript);
+
+        let _r_cycle: Vec<Fr> = verifier_transcript.challenge_vector(NUM_LOOKUPS.log_2());
+        let verification_result = sumcheck_proof.verify(
+            sumcheck_claim,
+            TABLE_SIZE.log_2(),
+            2,
+            &mut verifier_transcript,
+        );
+        assert!(
+            verification_result.is_ok(),
+            "Verification failed with error: {:?}",
+            verification_result.err()
+        );
+    }
+}
