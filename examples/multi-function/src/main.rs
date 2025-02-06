cfbaee33e13788eaadf3c71d6907cc909c724c09
Aditya Zutshi <zutshi@galois.com>
2025-02-06 13:26:08 -0800

    Feature/ci plot benchmarks (#572)
    * added ci and dummy results
    
    * removed dead code and added initial benchmarking script
    
    * turned off cancel-in-progress to debug abdrupt terminiation
    
    * excluded memory intensive examples and added back benchmark-ci
    
    * added back cancel-in-progress
    
    * added cahcing
    
    * added result generation
    
    * minor: changed name
    
    * enabled more tests
    
    * Excluded build time. Included memory plotting.
    
    * bugfix: terminated json properly.
    
    * debug: time is 0
    
    * bugfix: time is 0
    
    * bugfix: time is 0
    
    * run all benchmarks
    
    * pinned ubuntu 24.04
    
    * renamed memory-ops -> memory-ops-ex. The dir name is now consistent with the package name.
    
    * renamed memory-ops-ex -> memory-ops. The dir name is now consistent with the package name.
    
    * exit the CI if build fails.
    
    * add error handling to run_benchmarks.sh to exit on error
    
    * update CI workflow to trigger on feature/CI-plot-benchmarks branch
    
    * print output of time for each benchmark for debug purposes
    
    * remove feature/CI-plot-benchmarks branch from CI workflow triggers
    
    * Benchmark runtime for prover only
    
    * Trigger benchmark CI on updates to main
    
    ---------
    
    Co-authored-by: James Parker <james@galois.com>

--- examples/multi-function/src/main.rs
+++ examples/multi-function/src/main.rs
@@ -1,8 +1,12 @@
+use std::time::Instant;
+
 pub fn main() {
     let (prove_add, verify_add) = guest::build_add();
     let (prove_mul, verify_mul) = guest::build_mul();
 
+    let now = Instant::now();
     let (output, proof) = prove_add(5, 10);
+    println!("Prover runtime: {} s", now.elapsed().as_secs_f64());
     let is_valid = verify_add(proof);
 
     println!("add output: {}", output);
