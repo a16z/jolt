d449f1cb1c8c5e118f036f60f9960f4bfbf3fac7
github-action-benchmark <github@users.noreply.github.com>
2025-02-11 22:54:59 +0000

    add Benchmarks (customSmallerIsBetter) benchmark result for 783da5d32010e707f85085d59ae0451f6d8a6b25

---
+++ dev/bench/data.js
@@ -0,0 +1,140 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1739314499666,
+  "repoUrl": "https://github.com/a16z/jolt",
+  "entries": {
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "33655900+cre-mer@users.noreply.github.com",
+            "name": "Jonas Merhej",
+            "username": "cre-mer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "783da5d32010e707f85085d59ae0451f6d8a6b25",
+          "message": "fix dependencies in example Cargo.toml (#587)",
+          "timestamp": "2025-02-11T14:27:48-08:00",
+          "tree_id": "a63a65ad01534ccac2e1972689e6e1796f89462f",
+          "url": "https://github.com/a16z/jolt/commit/783da5d32010e707f85085d59ae0451f6d8a6b25"
+        },
+        "date": 1739314498742,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "alloc-time",
+            "value": 1.9042,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "alloc-mem",
+            "value": 3400024,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "fibonacci-time",
+            "value": 1.7855,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "fibonacci-mem",
+            "value": 3394324,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "memory-ops-time",
+            "value": 1.719,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "memory-ops-mem",
+            "value": 3394188,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "muldiv-time",
+            "value": 1.6964,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "muldiv-mem",
+            "value": 3391632,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "multi-function-time",
+            "value": 1.6481,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "multi-function-mem",
+            "value": 4610940,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "sha2-chain-time",
+            "value": 63.0816,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "sha2-chain-mem",
+            "value": 11033940,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "sha2-ex-time",
+            "value": 2.55,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "sha2-ex-mem",
+            "value": 3392852,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "sha3-ex-time",
+            "value": 3.4869,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "sha3-ex-mem",
+            "value": 3392300,
+            "unit": "KB",
+            "extra": ""
+          },
+          {
+            "name": "stdlib-time",
+            "value": 2.4405,
+            "unit": "s",
+            "extra": ""
+          },
+          {
+            "name": "stdlib-mem",
+            "value": 4659872,
+            "unit": "KB",
+            "extra": ""
+          }
+        ]
+      }
+    ]
+  }
+}
