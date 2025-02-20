80301543dc1c00fb9e2d72fb4af55d153dcdfb02
Donny <130464015+defitricks@users.noreply.github.com>
2025-02-20 18:59:10 +0200

    refactor: Refactor for Safer and More Readable Memory Management

--- jolt-sdk/src/alloc.rs
+++ jolt-sdk/src/alloc.rs
@@ -1,4 +1,5 @@
 use core::alloc::{GlobalAlloc, Layout};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub struct BumpAllocator;
 
@@ -14,15 +15,15 @@ extern "C" {
     static _HEAP_PTR: u8;
 }
 
-static mut ALLOC_NEXT: usize = 0;
+static ALLOC_NEXT: AtomicUsize = AtomicUsize::new(0);
 
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub unsafe extern "C" fn sys_alloc(size: usize, align: usize) -> *mut u8 {
-    let mut next = unsafe { ALLOC_NEXT };
+    let mut next = get_next_alloc();
 
     if next == 0 {
-        next = unsafe { (&_HEAP_PTR) as *const u8 as usize };
+        next = get_heap_ptr();
     }
 
     next = align_up(next, align);
@@ -30,8 +31,20 @@ pub unsafe extern "C" fn sys_alloc(size: usize, align: usize) -> *mut u8 {
     let ptr = next as *mut u8;
     next += size;
 
-    unsafe { ALLOC_NEXT = next };
+    set_next_alloc(next);
     ptr
+}
+
+fn get_heap_ptr() -> usize {
+    unsafe { (&_HEAP_PTR) as *const u8 as usize }
+}
+
+fn get_next_alloc() -> usize {
+    ALLOC_NEXT.load(Ordering::Relaxed)
+}
+
+fn set_next_alloc(next: usize) {
+    ALLOC_NEXT.store(next, Ordering::Relaxed)
 }
 
 fn align_up(addr: usize, align: usize) -> usize {
