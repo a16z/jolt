023eec0b389430cd1baf2357f558f48e0f8c15c8
arithmic-ASHISH <110672988+arithmic-ASHISH@users.noreply.github.com>
2025-02-26 10:41:36 +0530

    uncomment reseeding of program_io i/o

--- jolt-core/src/jolt/vm/mod.rs
+++ jolt-core/src/jolt/vm/mod.rs
@@ -790,8 +790,8 @@ where
         transcript.append_u64(Self::Subtables::COUNT as u64);
         transcript.append_u64(memory_layout.max_input_size);
         transcript.append_u64(memory_layout.max_output_size);
-        // transcript.append_bytes(&program_io.inputs);
-        // transcript.append_bytes(&program_io.outputs);
+        transcript.append_bytes(&program_io.inputs);
+        transcript.append_bytes(&program_io.outputs);
         transcript.append_u64(program_io.panic as u64);
     }
 }
