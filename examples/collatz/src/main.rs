bbd91b01b2badb6fe71da7ff52d26c42a2dfeacd
Marcella Hastings <marcella@galois.com>
2025-03-28 15:25:44 -0400

    update examples with new build pattern

--- examples/collatz/src/main.rs
+++ examples/collatz/src/main.rs
@@ -2,26 +2,42 @@ use std::time::Instant;
 
 pub fn main() {
     // Prove/verify convergence for a single number:
-    let (prove_collatz_single, verify_collatz_single) = guest::build_collatz_convergence();
+    let target_dir = "/tmp/jolt-guest-targets";
+    let program = guest::compile_collatz_convergence(target_dir);
+
+    let prover_preprocessing = guest::preprocess_prover_collatz_convergence(&program);
+    let verifier_preprocessing = guest::preprocess_verifier_collatz_convergence(&program);
+
+    let prove_collatz_single =
+        guest::build_prover_collatz_convergence(program, prover_preprocessing);
+    let verify_collatz_single = guest::build_verifier_collatz_convergence(verifier_preprocessing);
 
     let now = Instant::now();
-    let (output, proof) = prove_collatz_single(19);
+    let input = 19;
+    let (output, proof) = prove_collatz_single(input);
     println!("Prover runtime: {} s", now.elapsed().as_secs_f64());
-    let is_valid = verify_collatz_single(proof);
+    let is_valid = verify_collatz_single(input, output, proof);
 
     println!("output: {}", output);
     println!("valid: {}", is_valid);
 
     // Prove/verify convergence for a range of numbers:
-    let (prove_collatz_convergence, verify_collatz_convergence) =
-        guest::build_collatz_convergence_range();
+    let program = guest::compile_collatz_convergence_range(target_dir);
+
+    let prover_preprocessing = guest::preprocess_prover_collatz_convergence_range(&program);
+    let verifier_preprocessing = guest::preprocess_verifier_collatz_convergence_range(&program);
+
+    let prove_collatz_convergence =
+        guest::build_prover_collatz_convergence_range(program, prover_preprocessing);
+    let verify_collatz_convergence =
+        guest::build_verifier_collatz_convergence_range(verifier_preprocessing);
 
     // https://www.reddit.com/r/compsci/comments/gk9x6g/collatz_conjecture_news_recently_i_managed_to/
     let start: u128 = 1 << 68;
     let now = Instant::now();
     let (output, proof) = prove_collatz_convergence(start, start + 100);
     println!("Prover runtime: {} s", now.elapsed().as_secs_f64());
-    let is_valid = verify_collatz_convergence(proof);
+    let is_valid = verify_collatz_convergence(start, start + 100, output, proof);
 
     println!("output: {}", output);
     println!("valid: {}", is_valid);
--- examples/multi-function/src/main.rs
+++ examples/multi-function/src/main.rs
@@ -1,20 +1,54 @@
 use std::time::Instant;
 
+use jolt_sdk::JoltHyperKZGProof;
+
 pub fn main() {
-    let (prove_add, verify_add) = guest::build_add();
-    let (prove_mul, verify_mul) = guest::build_mul();
+    let (prove_add, verify_add) = build_add();
+    let (prove_mul, verify_mul) = build_mul();
 
     let now = Instant::now();
     let (output, proof) = prove_add(5, 10);
     println!("Prover runtime: {} s", now.elapsed().as_secs_f64());
-    let is_valid = verify_add(proof);
+    let is_valid = verify_add(5, 10, output, proof);
 
     println!("add output: {}", output);
     println!("add valid: {}", is_valid);
 
     let (output, proof) = prove_mul(5, 10);
-    let is_valid = verify_mul(proof);
+    let is_valid = verify_mul(5, 10, output, proof);
 
     println!("mul output: {}", output);
     println!("mul valid: {}", is_valid);
+}
+
+fn build_add() -> (
+    impl Fn(u32, u32) -> (u32, JoltHyperKZGProof) + Sync + Send,
+    impl Fn(u32, u32, u32, JoltHyperKZGProof) -> bool + Sync + Send,
+) {
+    let target_dir = "/tmp/jolt-guest-targets";
+    let program = guest::compile_add(target_dir);
+
+    let prover_preprocessing = guest::preprocess_prover_add(&program);
+    let verifier_preprocessing = guest::preprocess_verifier_add(&program);
+
+    let prove = guest::build_prover_add(program, prover_preprocessing);
+    let verify = guest::build_verifier_add(verifier_preprocessing);
+
+    (prove, verify)
+}
+
+fn build_mul() -> (
+    impl Fn(u32, u32) -> (u32, JoltHyperKZGProof) + Sync + Send,
+    impl Fn(u32, u32, u32, JoltHyperKZGProof) -> bool + Sync + Send,
+) {
+    let target_dir = "/tmp/jolt-guest-targets";
+    let program = guest::compile_mul(target_dir);
+
+    let prover_preprocessing = guest::preprocess_prover_mul(&program);
+    let verifier_preprocessing = guest::preprocess_verifier_mul(&program);
+
+    let prove = guest::build_prover_mul(program, prover_preprocessing);
+    let verify = guest::build_verifier_mul(verifier_preprocessing);
+
+    (prove, verify)
 }
