0c23b10d557aaae20215ea14ca390c480ebf6998
markosg04 <mgeorghiades@a16z.com>
2025-05-08 15:47:45 -0400

    style: comments

--- examples/fibonacci/guest/src/lib.rs
+++ examples/fibonacci/guest/src/lib.rs
@@ -1,28 +1,19 @@
 #![cfg_attr(feature = "guest", no_std)]
 use jolt::{end_cycle_tracking, start_cycle_tracking};
 
-
-// come back to this at the end
 #[jolt::provable]
 fn fib(n: u32) -> u128 {
     let mut a: u128 = 0;
-    start_cycle_tracking("empty");
     let mut b: u128 = 1;
-    end_cycle_tracking("empty");
     let mut sum: u128;
 
-    start_cycle_tracking("fib_loop");
+    start_cycle_tracking("fib_loop"); // Use `start_cycle_tracking("{name}")` to start a cycle span
 
     for _ in 1..n {
         sum = a + b;
-        // ----- end measured span ------------
         a = b;
         b = sum;
     }
-    end_cycle_tracking("fib_loop");
-
-    // end_cycle_tracking("fib_loop");
-    // // ----- end measured span ------------------------------------------
-
+    end_cycle_tracking("fib_loop"); // Use `end_cycle_tracking("{name}")` to end a cycle span
     b
 }
