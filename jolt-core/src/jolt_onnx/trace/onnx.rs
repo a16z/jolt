bcd6ff1fc37bc976b978bd8d5868856bee883f7f
Forpee <khalilhassam3@gmail.com>
2025-05-30 21:57:56 +0200

    chore: update comment for clarity in to_precompile function

--- jolt-core/src/jolt_onnx/trace/onnx.rs
+++ jolt-core/src/jolt_onnx/trace/onnx.rs
@@ -58,7 +58,8 @@ impl ONNXTraceRow {
     pub fn to_precompile(&self) -> Option<PrecompileOperators> {
         match self.instruction.opcode {
             Operator::MatMul => {
-                // # Note: We pad the tensor dimensions to the next power of two
+                // --- # Note ---
+                // We pad the tensor dimensions to the next power of two
                 let inputs = self
                     .layer_state
                     .input_vals
