4c53321ff66dc715c29ffde7ad378284f2734c66
Michael Zhu <mchl.zhu.96@gmail.com>
2025-06-10 17:33:25 -0400

    Dory sha3 test

--- jolt-core/src/jolt/vm/rv32i_vm.rs
+++ jolt-core/src/jolt/vm/rv32i_vm.rs
@@ -85,6 +85,7 @@ mod tests {
     use crate::host;
     use crate::jolt::vm::rv32i_vm::{Jolt, RV32IJoltVM};
     use crate::poly::commitment::commitment_scheme::CommitmentScheme;
+    use crate::poly::commitment::dory::DoryCommitmentScheme;
     use crate::poly::commitment::hyperkzg::HyperKZG;
     use crate::poly::commitment::mock::MockCommitScheme;
     use crate::poly::commitment::zeromorph::Zeromorph;
@@ -149,7 +150,7 @@ mod tests {
     }
 
     #[test]
-    fn sha3_e2e_zeromorph() {
+    fn sha3_e2e_dory() {
         let guard = SHA3_FILE_LOCK.lock().unwrap();
         let mut program = host::Program::new("sha3-guest");
         let (bytecode, memory_init) = program.decode();
@@ -165,14 +166,13 @@ mod tests {
             1 << 20,
             1 << 20,
         );
-        let (jolt_proof, jolt_commitments, debug_info) = <RV32IJoltVM as Jolt<
-            32,
-            Fr,
-            HyperKZG<Bn254, KeccakTranscript>,
-            KeccakTranscript,
-        >>::prove(
-            io_device, trace, preprocessing.clone()
-        );
+        let (jolt_proof, jolt_commitments, debug_info) =
+            <RV32IJoltVM as Jolt<
+                32,
+                Fr,
+                DoryCommitmentScheme<KeccakTranscript>,
+                KeccakTranscript,
+            >>::prove(io_device, trace, preprocessing.clone());
 
         let verification_result = RV32IJoltVM::verify(
             preprocessing.shared,
