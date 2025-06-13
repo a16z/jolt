c27624041ac72b455ef3c5de88ca5c6efee85e0a
Forpee <khalilhassam3@gmail.com>
2025-06-13 20:49:37 +0200

    feat: init maxpool

--- jolt-core/src/jolt_onnx/subtable/mod.rs
+++ jolt-core/src/jolt_onnx/subtable/mod.rs
@@ -1,6 +1,7 @@
 //! This module provides the subtables used by the ONNX runtime.
 
-pub mod is_pos;
-pub mod sigmoid;
-pub mod is_zero;
 pub mod is_max;
+pub mod is_pos;
+pub mod is_zero;
+pub mod max;
+pub mod sigmoid;
