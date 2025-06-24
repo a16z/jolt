3baf6c2003c805ce8e5d420ba2aeb5cdec71bff1
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2025-06-24 14:52:01 -0400

    prefix suffix for identity poly
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- jolt-core/src/poly/identity_poly.rs
+++ jolt-core/src/poly/identity_poly.rs
@@ -1,21 +1,36 @@
 use crate::field::JoltField;
-
+use crate::poly::multilinear_polynomial::{
+    MultilinearPolynomial, PrefixPolynomial, SuffixPolynomial,
+};
 use crate::utils::math::Math;
+use crate::utils::uninterleave_bits;
 
 use super::multilinear_polynomial::{BindingOrder, PolynomialBinding, PolynomialEvaluation};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Endianness {
+    Little,
+    Big,
+}
 
 pub struct IdentityPolynomial<F: JoltField> {
     num_vars: usize,
     num_bound_vars: usize,
     bound_value: F,
+    endianness: Endianness,
 }
 
 impl<F: JoltField> IdentityPolynomial<F> {
     pub fn new(num_vars: usize) -> Self {
+        Self::new_with_endianness(num_vars, Endianness::Little)
+    }
+
+    pub fn new_with_endianness(num_vars: usize, endianness: Endianness) -> Self {
         IdentityPolynomial {
             num_vars,
             num_bound_vars: 0,
             bound_value: F::zero(),
+            endianness,
         }
     }
 }
@@ -33,7 +48,11 @@ impl<F: JoltField> PolynomialBinding<F> for IdentityPolynomial<F> {
             "IdentityPolynomial only supports low-to-high binding"
         );
 
-        self.bound_value += F::from_u32(1u32 << self.num_bound_vars) * r;
+        let pow = match self.endianness {
+            Endianness::Little => self.num_bound_vars,
+            Endianness::Big => self.num_vars - 1 - self.num_bound_vars,
+        };
+        self.bound_value += F::from_u64(1u64 << pow) * r;
         self.num_bound_vars += 1;
     }
 
@@ -52,7 +71,12 @@ impl<F: JoltField> PolynomialEvaluation<F> for IdentityPolynomial<F> {
     fn evaluate(&self, r: &[F]) -> F {
         let len = r.len();
         assert_eq!(len, self.num_vars);
-        (0..len).map(|i| F::from_u64(i.pow2() as u64) * r[i]).sum()
+        match self.endianness {
+            Endianness::Little => (0..len).map(|i| F::from_u64(i.pow2() as u64) * r[i]).sum(),
+            Endianness::Big => (0..len)
+                .map(|i| F::from_u64((len - 1 - i).pow2() as u64) * r[i])
+                .sum(),
+        }
     }
 
     fn batch_evaluate(_polys: &[&Self], _r: &[F]) -> (Vec<F>, Vec<F>) {
@@ -69,14 +93,215 @@ impl<F: JoltField> PolynomialEvaluation<F> for IdentityPolynomial<F> {
         );
 
         let mut evals = vec![F::zero(); degree];
-        evals[0] = self.bound_value + F::from_u64((index as u64) << (1 + self.num_bound_vars));
-        let m = F::from_u32(1 << self.num_bound_vars);
+        let m = match self.endianness {
+            Endianness::Little => F::from_u32(1 << self.num_bound_vars),
+            Endianness::Big => F::from_u32(1 << (self.num_vars - 1 - self.num_bound_vars)),
+        };
+        evals[0] = self.bound_value + (m + m).mul_u64(index as u64);
         let mut eval = evals[0] + m;
         for i in 1..degree {
             eval += m;
             evals[i] = eval;
         }
         evals
+    }
+}
+
+impl<F: JoltField> PrefixPolynomial<F> for IdentityPolynomial<F> {
+    type PrefixCheckpoints = F;
+
+    fn prefix_polynomial(&self, prefix_len: usize) -> MultilinearPolynomial<F> {
+        assert!(prefix_len % 2 == 0);
+        assert_eq!(self.endianness, Endianness::Big);
+        let bound_value = self.bound_value.mul_u64(1 << prefix_len);
+        MultilinearPolynomial::from(
+            (0..prefix_len.pow2())
+                .map(|i| bound_value + F::from_u64(i as u64))
+                .collect::<Vec<F>>(),
+        )
+    }
+
+    fn update_checkpoints(&mut self, checkpoints: F) {
+        self.bound_value = checkpoints;
+    }
+}
+
+impl<F: JoltField> SuffixPolynomial<F> for IdentityPolynomial<F> {
+    fn suffix_mle(&self, index: u64, suffix_len: usize) -> F {
+        assert!(suffix_len % 2 == 0);
+        assert_eq!(self.endianness, Endianness::Big);
+        F::from_u64(index)
+    }
+}
+
+pub struct ShiftSuffixPolynomial;
+impl<F: JoltField> SuffixPolynomial<F> for ShiftSuffixPolynomial {
+    fn suffix_mle(&self, _index: u64, suffix_len: usize) -> F {
+        assert!(suffix_len % 2 == 0);
+        F::from_u64(1 << suffix_len)
+    }
+}
+
+/// BatchedUninterleavePolynomial evaluates
+/// sum_{i=0}^{num_vars/2-1} (r[2i]  + z * r[2i + 1]) * 2^(num_vars/2 - 1 - i)
+pub struct BatchedUninterleavePolynomial<F: JoltField> {
+    num_vars: usize,
+    num_bound_vars: usize,
+    z: F,
+    bound_value: F,
+    /// Stores the odd variable that's been bound but hasn't updated bound_value yet
+    pending_odd_bind: Option<F>,
+}
+
+impl<F: JoltField> BatchedUninterleavePolynomial<F> {
+    pub fn new(num_vars: usize, z: F) -> Self {
+        assert_eq!(num_vars % 2, 0, "num_vars must be divisible by 2");
+        BatchedUninterleavePolynomial {
+            num_vars,
+            num_bound_vars: 0,
+            z,
+            bound_value: F::zero(),
+            pending_odd_bind: None,
+        }
+    }
+}
+
+impl<F: JoltField> PolynomialBinding<F> for BatchedUninterleavePolynomial<F> {
+    fn is_bound(&self) -> bool {
+        self.num_bound_vars != 0
+    }
+
+    fn bind(&mut self, r: F, order: BindingOrder) {
+        debug_assert!(self.num_bound_vars < self.num_vars);
+        debug_assert_eq!(
+            order,
+            BindingOrder::HighToLow,
+            "BatchedUninterleavePolynomial only supports high-to-low binding"
+        );
+
+        if self.num_bound_vars % 2 == 0 {
+            // Binding an even variable (r[2i])
+            self.pending_odd_bind = Some(r);
+        } else {
+            // Binding an odd variable (r[2i+1])
+            let odd_r = self.pending_odd_bind.take().unwrap();
+            self.bound_value += self.bound_value; // multiply by 2
+            self.bound_value += odd_r + self.z * r;
+        }
+        self.num_bound_vars += 1;
+    }
+
+    fn bind_parallel(&mut self, r: F, order: BindingOrder) {
+        // Binding is constant time, no parallelism necessary
+        self.bind(r, order);
+    }
+
+    fn final_sumcheck_claim(&self) -> F {
+        debug_assert_eq!(self.num_vars, self.num_bound_vars);
+        debug_assert!(self.pending_odd_bind.is_none());
+        self.bound_value
+    }
+}
+
+impl<F: JoltField> PolynomialEvaluation<F> for BatchedUninterleavePolynomial<F> {
+    fn evaluate(&self, r: &[F]) -> F {
+        let len = r.len();
+        assert_eq!(len, self.num_vars);
+        assert_eq!(len % 2, 0);
+
+        (0..len / 2)
+            .map(|i| {
+                F::from_u64(1u64 << (self.num_vars / 2 - 1 - i))
+                    * (r[2 * i] + self.z * r[2 * i + 1])
+            })
+            .sum()
+    }
+
+    fn batch_evaluate(_polys: &[&Self], _r: &[F]) -> (Vec<F>, Vec<F>) {
+        unimplemented!("Currently unused")
+    }
+
+    fn sumcheck_evals(&self, index: usize, degree: usize, order: BindingOrder) -> Vec<F> {
+        debug_assert!(degree > 0);
+        debug_assert!(index < self.num_vars.pow2() / 2);
+        debug_assert_eq!(
+            order,
+            BindingOrder::HighToLow,
+            "BatchedUninterleavePolynomial only supports high-to-low binding"
+        );
+
+        let mut evals = vec![F::zero(); degree];
+
+        let (right, left) = uninterleave_bits(index as u64);
+        let comb = F::from_u32(right) + self.z.mul_u64(left as u64);
+
+        if self.num_bound_vars % 2 == 0 {
+            assert!(self.pending_odd_bind.is_none());
+            let unbound_pairs = (self.num_vars - self.num_bound_vars) / 2;
+            assert!(unbound_pairs > 0);
+            evals[0] = self.bound_value.mul_u64(1 << unbound_pairs) + comb;
+            let m = F::from_u32(1 << (unbound_pairs - 1));
+            let mut eval = evals[0] + m;
+            for i in 1..degree {
+                eval += m;
+                evals[i] = eval;
+            }
+            evals
+        } else {
+            assert!(self.pending_odd_bind.is_some());
+            let odd_r = self.pending_odd_bind.unwrap();
+            let unbound_pairs = (self.num_vars - self.num_bound_vars).div_ceil(2);
+            evals[0] = self.bound_value.mul_u64(1 << unbound_pairs)
+                + comb
+                + odd_r.mul_u64(1 << (unbound_pairs - 1));
+            let m = self.z.mul_u64(1 << (unbound_pairs - 1));
+            let mut eval = evals[0] + m;
+            for i in 1..degree {
+                eval += m;
+                evals[i] = eval;
+            }
+            evals
+        }
+    }
+}
+
+pub struct ShiftHalfSuffixPolynomial;
+impl<F: JoltField> SuffixPolynomial<F> for ShiftHalfSuffixPolynomial {
+    fn suffix_mle(&self, _index: u64, suffix_len: usize) -> F {
+        assert!(suffix_len % 2 == 0);
+        F::from_u64(1 << (suffix_len / 2))
+    }
+}
+
+impl<F: JoltField> SuffixPolynomial<F> for BatchedUninterleavePolynomial<F> {
+    fn suffix_mle(&self, index: u64, suffix_len: usize) -> F {
+        assert!(suffix_len % 2 == 0);
+        assert!(self.num_bound_vars % 2 == 0);
+        let (right, left) = uninterleave_bits(index);
+        F::from_u32(right) + self.z.mul_u64(left as u64)
+    }
+}
+
+impl<F: JoltField> PrefixPolynomial<F> for BatchedUninterleavePolynomial<F> {
+    type PrefixCheckpoints = F;
+
+    fn prefix_polynomial(&self, prefix_len: usize) -> MultilinearPolynomial<F> {
+        assert!(prefix_len % 2 == 0);
+        assert!(self.num_bound_vars % 2 == 0);
+        let bound_value = self.bound_value.mul_u64(1 << (prefix_len / 2));
+        MultilinearPolynomial::from(
+            (0..prefix_len.pow2())
+                .map(|i| {
+                    let (right, left) = uninterleave_bits(i as u64);
+                    let comb = F::from_u32(right) + self.z.mul_u64(left as u64);
+                    bound_value + comb
+                })
+                .collect::<Vec<F>>(),
+        )
+    }
+
+    fn update_checkpoints(&mut self, checkpoints: F) {
+        self.bound_value = checkpoints;
     }
 }
 
@@ -252,6 +477,55 @@ mod tests {
     }
 
     #[test]
+    fn identity_poly_prefix_suffix_decomposition() {
+        const NUM_VARS: usize = 8;
+        const PREFIX_LEN: usize = 2;
+        const SUFFIX_LEN: usize = NUM_VARS - PREFIX_LEN;
+
+        let identity_poly: IdentityPolynomial<Fr> =
+            IdentityPolynomial::new_with_endianness(NUM_VARS, Endianness::Big);
+        let prefix_poly = identity_poly.prefix_polynomial(PREFIX_LEN);
+        let shift_suffix = ShiftSuffixPolynomial;
+
+        // Test over the entire boolean hypercube that:
+        // IdentityPolynomial::evaluate(x_0, ..., x_7) =
+        //     IdentityPolynomial::prefix_polynomial().evaluate(x_0, x_1) * shift_suffix_mle(x_2, .., x_7) +
+        //     IdentityPolynomial::suffix_mle(x_2, ..., x_7)
+
+        for i in 0..(1 << NUM_VARS) {
+            let mut eval_point = vec![Fr::ZERO; NUM_VARS];
+            for j in 0..NUM_VARS {
+                if (i >> j) & 1 == 1 {
+                    eval_point[j] = Fr::ONE;
+                }
+            }
+
+            let direct_eval = identity_poly.evaluate(&eval_point);
+
+            let prefix_eval_point = &eval_point[0..PREFIX_LEN];
+            let suffix_eval_point = &eval_point[PREFIX_LEN..];
+
+            let mut suffix_index = 0u64;
+            for (j, &bit) in suffix_eval_point.iter().rev().enumerate() {
+                if bit == Fr::ONE {
+                    suffix_index |= 1 << j;
+                }
+            }
+
+            let prefix_eval = prefix_poly.evaluate(prefix_eval_point);
+            let shift_suffix_eval: Fr = shift_suffix.suffix_mle(suffix_index, SUFFIX_LEN);
+            let suffix_eval = identity_poly.suffix_mle(suffix_index, SUFFIX_LEN);
+
+            let decomposed_eval = prefix_eval * shift_suffix_eval + suffix_eval;
+
+            assert_eq!(
+                direct_eval, decomposed_eval,
+                "IdentityPolynomial decomposition failed at index {i}: direct={direct_eval}, decomposed={decomposed_eval}"
+            );
+        }
+    }
+
+    #[test]
     fn unmap_address_poly_evaluate() {
         const NUM_VARS: usize = 10;
         const INPUT_START: u64 = 0x80000000;
@@ -279,6 +553,145 @@ mod tests {
         point_3[0] = Fr::ONE;
         point_3[1] = Fr::ONE;
         assert_eq!(unmap_poly.evaluate(&point_3), Fr::from(INPUT_START + 8));
+    }
+
+    #[test]
+    fn batched_uninterleave_poly_boolean_hypercube() {
+        const NUM_VARS: usize = 8;
+
+        let mut rng = test_rng();
+        let z = Fr::random(&mut rng);
+        let batched_poly = BatchedUninterleavePolynomial::<Fr>::new(NUM_VARS, z);
+
+        // Test evaluation on boolean hypercube
+        for i in 0..(1 << NUM_VARS) {
+            let mut eval_point = vec![Fr::ZERO; NUM_VARS];
+            for j in 0..NUM_VARS {
+                if (i >> j) & 1 == 1 {
+                    eval_point[j] = Fr::ONE;
+                }
+            }
+            eval_point.reverse();
+
+            // Use uninterleave_bits to match the polynomial's implementation
+            let (right, left) = uninterleave_bits(i as u64);
+            let expected = Fr::from_u32(right) + z * Fr::from_u32(left);
+
+            assert_eq!(
+                batched_poly.evaluate(&eval_point),
+                expected,
+                "Boolean hypercube evaluation failed at index {i}"
+            );
+        }
+    }
+
+    #[test]
+    fn batched_uninterleave_poly_prefix_suffix_decomposition() {
+        const NUM_VARS: usize = 8;
+        const PREFIX_LEN: usize = 2;
+        const SUFFIX_LEN: usize = NUM_VARS - PREFIX_LEN;
+
+        let mut rng = test_rng();
+        let z = Fr::random(&mut rng);
+        let batched_poly = BatchedUninterleavePolynomial::<Fr>::new(NUM_VARS, z);
+        let prefix_poly = batched_poly.prefix_polynomial(PREFIX_LEN);
+        let shift_suffix = ShiftHalfSuffixPolynomial;
+
+        // Test over the entire boolean hypercube that:
+        // BatchedUninterleavePolynomial::evaluate(x_0, ..., x_7) =
+        //     BatchedUninterleavePolynomial::prefix_polynomial().evaluate(x_0, x_1) * shift_suffix_mle(x_2, .., x_7) +
+        //     BatchedUninterleavePolynomial::suffix_mle(x_2, ..., x_7)
+
+        for i in 0..(1 << NUM_VARS) {
+            let mut eval_point = vec![Fr::ZERO; NUM_VARS];
+            for j in 0..NUM_VARS {
+                if (i >> j) & 1 == 1 {
+                    eval_point[j] = Fr::ONE;
+                }
+            }
+
+            let direct_eval = batched_poly.evaluate(&eval_point);
+
+            let prefix_eval_point = &eval_point[0..PREFIX_LEN];
+            let suffix_eval_point = &eval_point[PREFIX_LEN..];
+
+            let mut suffix_index = 0u64;
+            for (j, &bit) in suffix_eval_point.iter().rev().enumerate() {
+                if bit == Fr::ONE {
+                    suffix_index |= 1 << j;
+                }
+            }
+
+            let prefix_eval = prefix_poly.evaluate(prefix_eval_point);
+            let shift_suffix_eval: Fr = shift_suffix.suffix_mle(suffix_index, SUFFIX_LEN);
+            let suffix_eval = batched_poly.suffix_mle(suffix_index, SUFFIX_LEN);
+
+            let decomposed_eval = prefix_eval * shift_suffix_eval + suffix_eval;
+
+            assert_eq!(
+                direct_eval, decomposed_eval,
+                "BatchedUninterleavePolynomial decomposition failed at index {i}: direct={direct_eval}, decomposed={decomposed_eval}"
+            );
+        }
+    }
+
+    #[test]
+    fn batched_uninterleave_poly() {
+        const NUM_VARS: usize = 8;
+
+        let mut rng = test_rng();
+        let z = Fr::random(&mut rng);
+        println!("z = {z}");
+        let mut batched_poly = BatchedUninterleavePolynomial::<Fr>::new(NUM_VARS, z);
+
+        // Create reference polynomial with evaluations
+        let reference_evals: Vec<Fr> = (0u64..(1 << NUM_VARS))
+            .map(|i| {
+                let (right, left) = uninterleave_bits(i);
+                Fr::from_u32(right) + z * Fr::from_u32(left)
+            })
+            .collect();
+        let mut reference_poly = MultilinearPolynomial::from(reference_evals);
+
+        // Verify that both polynomials agree on the entire boolean hypercube
+        for i in 0..(1 << NUM_VARS) {
+            let mut eval_point = vec![Fr::ZERO; NUM_VARS];
+            for j in 0..NUM_VARS {
+                if (i >> j) & 1 == 1 {
+                    eval_point[j] = Fr::ONE;
+                }
+            }
+            let batched_eval = batched_poly.evaluate(&eval_point);
+            let reference_eval = reference_poly.evaluate(&eval_point);
+            assert_eq!(
+                batched_eval, reference_eval,
+                "Evaluation mismatch at index {i}: batched={batched_eval}, reference={reference_eval}"
+            );
+        }
+
+        for round in 0..NUM_VARS {
+            let num_evals = 1 << (NUM_VARS - round - 1);
+
+            for i in 0..num_evals {
+                let batched_evals = batched_poly.sumcheck_evals(i, 3, BindingOrder::HighToLow);
+                let reference_evals = reference_poly.sumcheck_evals(i, 3, BindingOrder::HighToLow);
+
+                assert_eq!(
+                    batched_evals, reference_evals,
+                    "Round {round}, index {i}: sumcheck_evals mismatch"
+                );
+            }
+
+            let r = Fr::random(&mut rng);
+            batched_poly.bind(r, BindingOrder::HighToLow);
+            reference_poly.bind(r, BindingOrder::HighToLow);
+        }
+
+        assert_eq!(
+            batched_poly.final_sumcheck_claim(),
+            reference_poly.final_sumcheck_claim(),
+            "Final sumcheck claims don't match"
+        );
     }
 
     #[test]
