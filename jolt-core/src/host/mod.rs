05d61136098d51309297fe9264beca5ecc00d7b6
danielwlz <183745461+danielwlz@users.noreply.github.com>
2025-07-10 02:07:19 +0800

    Add missing reference to KECCAK256 in jolt-core

--- jolt-core/src/host/mod.rs
+++ jolt-core/src/host/mod.rs
@@ -187,6 +187,7 @@ impl Program {
                 RV32IMInstruction::SRLI(srli) => srli.virtual_sequence(),
                 RV32IMInstruction::SHA256(sha256) => sha256.virtual_sequence(),
                 RV32IMInstruction::SHA256INIT(sha256init) => sha256init.virtual_sequence(),
+                RV32IMInstruction::KECCAK256(keccak256) => keccak256.virtual_sequence(),
                 _ => vec![instr],
             })
             .collect();
