a750e8b2ed4d5f8edc81ab11ab65800b8af96c4a
Forpee <khalilhassam3@gmail.com>
2025-07-10 17:42:07 +0200

    feat: init zkml-jolt-core crate

--- onnx-tracer/src/lib.rs
+++ onnx-tracer/src/lib.rs
@@ -24,81 +24,83 @@ pub mod tensor;
 /// The denominator in the fixed point representation used when quantizing inputs
 pub type Scale = i32;
 
+pub fn decode() {}
+
 /// Parameters specific to a proving run
 #[derive(Debug, Args, Deserialize, Serialize, Clone, PartialEq, PartialOrd)]
 pub struct RunArgs {
-  /// Hand-written parser for graph variables, eg. batch_size=1
-  #[arg(short = 'V', long, value_parser = parse_key_val::<String, usize>, default_value =
+    /// Hand-written parser for graph variables, eg. batch_size=1
+    #[arg(short = 'V', long, value_parser = parse_key_val::<String, usize>, default_value =
   "batch_size=1", value_delimiter = ',')]
-  pub variables: Vec<(String, usize)>,
-  /// if the scale is ever > scale_rebase_multiplier * input_scale then the scale is
-  /// rebased
-  // to input_scale (this a more advanced parameter, use with caution)
-  #[arg(long, default_value = "1")]
-  pub scale_rebase_multiplier: u32,
-  /// The denominator in the fixed point representation used when quantizing inputs
-  #[arg(short = 'S', long, default_value = "7", allow_hyphen_values = true)]
-  pub input_scale: Scale,
-  /// The denominator in the fixed point representation used when quantizing
-  /// parameters
-  #[arg(long, default_value = "7", allow_hyphen_values = true)]
-  pub param_scale: Scale,
-  //     // /// The tolerance for error on model outputs
-  //     // #[arg(short = 'T', long, default_value = "0")]
-  //     // pub tolerance: Tolerance,
+    pub variables: Vec<(String, usize)>,
+    /// if the scale is ever > scale_rebase_multiplier * input_scale then the scale is
+    /// rebased
+    // to input_scale (this a more advanced parameter, use with caution)
+    #[arg(long, default_value = "1")]
+    pub scale_rebase_multiplier: u32,
+    /// The denominator in the fixed point representation used when quantizing inputs
+    #[arg(short = 'S', long, default_value = "7", allow_hyphen_values = true)]
+    pub input_scale: Scale,
+    /// The denominator in the fixed point representation used when quantizing
+    /// parameters
+    #[arg(long, default_value = "7", allow_hyphen_values = true)]
+    pub param_scale: Scale,
+    //     // /// The tolerance for error on model outputs
+    //     // #[arg(short = 'T', long, default_value = "0")]
+    //     // pub tolerance: Tolerance,
 
-  //     /// The min and max elements in the lookup table input column
-  //     #[arg(short = 'B', long, value_parser = parse_tuple::<i128>, default_value =
-  // "(-32768,32768)")]     pub lookup_range: (i128, i128),
-  //     /// The log_2 number of rows
-  //     #[arg(short = 'K', long, default_value = "17")]
-  //     pub logrows: u32,
-  //     /// The log_2 number of rows
-  //     #[arg(short = 'N', long, default_value = "2")]
-  //     pub num_inner_cols: usize,
-  //     // /// Flags whether inputs are public, private, hashed
-  //     // #[arg(long, default_value = "private")]
-  //     // pub input_visibility: Visibility,
-  //     // /// Flags whether outputs are public, private, hashed
-  //     // #[arg(long, default_value = "public")]
-  //     // pub output_visibility: Visibility,
-  //     // /// Flags whether params are public, private, hashed
-  //     // #[arg(long, default_value = "private")]
-  //     // pub param_visibility: Visibility,
+    //     /// The min and max elements in the lookup table input column
+    //     #[arg(short = 'B', long, value_parser = parse_tuple::<i128>, default_value =
+    // "(-32768,32768)")]     pub lookup_range: (i128, i128),
+    //     /// The log_2 number of rows
+    //     #[arg(short = 'K', long, default_value = "17")]
+    //     pub logrows: u32,
+    //     /// The log_2 number of rows
+    //     #[arg(short = 'N', long, default_value = "2")]
+    //     pub num_inner_cols: usize,
+    //     // /// Flags whether inputs are public, private, hashed
+    //     // #[arg(long, default_value = "private")]
+    //     // pub input_visibility: Visibility,
+    //     // /// Flags whether outputs are public, private, hashed
+    //     // #[arg(long, default_value = "public")]
+    //     // pub output_visibility: Visibility,
+    //     // /// Flags whether params are public, private, hashed
+    //     // #[arg(long, default_value = "private")]
+    //     // pub param_visibility: Visibility,
 }
 
 impl Default for RunArgs {
-  fn default() -> Self {
-    Self {
-      // tolerance: Tolerance::default(),
-      input_scale: 7,
-      param_scale: 7,
-      scale_rebase_multiplier: 1,
-      // lookup_range: (-32768, 32768),
-      // logrows: 17,
-      // num_inner_cols: 2,
-      variables: vec![("batch_size".to_string(), 1)],
-      // input_visibility: Visibility::Private,
-      // output_visibility: Visibility::Public,
-      // param_visibility: Visibility::Private,
+    fn default() -> Self {
+        Self {
+            // tolerance: Tolerance::default(),
+            input_scale: 7,
+            param_scale: 7,
+            scale_rebase_multiplier: 1,
+            // lookup_range: (-32768, 32768),
+            // logrows: 17,
+            // num_inner_cols: 2,
+            variables: vec![("batch_size".to_string(), 1)],
+            // input_visibility: Visibility::Private,
+            // output_visibility: Visibility::Public,
+            // param_visibility: Visibility::Private,
+        }
     }
-  }
 }
 
 /// Parse a single key-value pair
 fn parse_key_val<T, U>(
-  s: &str,
+    s: &str,
 ) -> Result<(T, U), Box<dyn std::error::Error + Send + Sync + 'static>>
 where
-  T: std::str::FromStr,
-  T::Err: std::error::Error + Send + Sync + 'static,
-  U: std::str::FromStr,
-  U::Err: std::error::Error + Send + Sync + 'static,
+    T: std::str::FromStr,
+    T::Err: std::error::Error + Send + Sync + 'static,
+    U: std::str::FromStr,
+    U::Err: std::error::Error + Send + Sync + 'static,
 {
-  let pos = s
-    .find('=')
-    .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{s}`"))?;
-  Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
+    let pos = s
+        .find('=')
+        .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{s}`"))?;
+    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
 }
 
 // impl RunArgs {
