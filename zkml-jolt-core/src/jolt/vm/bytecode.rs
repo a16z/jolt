78253f2d8258ee5324545d3b2ca3e116c1da92b3
Forpee <khalilhassam3@gmail.com>
2025-07-13 21:39:54 +0200

    feat: init bytecode shout proof

---
+++ zkml-jolt-core/src/jolt/vm/bytecode.rs
@@ -0,0 +1,15 @@
+use onnx_tracer::trace_types::{ONNXCycle, ONNXInstr};
+
+pub struct BytecodePreprocessing {
+    code_size: usize,
+    bytecode: Vec<ONNXInstr>,
+}
+
+pub struct BytecodeProof {}
+
+impl BytecodeProof {
+    pub fn prove(preprocessing: &BytecodePreprocessing, trace: &[ONNXCycle]) {
+        let K = preprocessing.code_size;
+        let T = trace.len();
+    }
+}
