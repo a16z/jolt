e2ec55a4a913b7a6e5041379ac809078e8f58810
Forpee <khalilhassam3@gmail.com>
2025-07-14 13:58:15 +0200

    wip: bytecode proof

--- zkml-jolt-core/src/jolt/vm/bytecode.rs
+++ zkml-jolt-core/src/jolt/vm/bytecode.rs
@@ -11,5 +11,10 @@ impl BytecodeProof {
     pub fn prove(preprocessing: &BytecodePreprocessing, trace: &[ONNXCycle]) {
         let K = preprocessing.code_size;
         let T = trace.len();
+
+        // --- Shout PIOP ---
+        // --- Hamming weight check ---
+        // --- Booleanity check ---
+        // --- raf evaluation ---
     }
 }
