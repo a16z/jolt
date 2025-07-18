35e2358b40e0a5283a37373edd54fb0be45c1271
Forpee <khalilhassam3@gmail.com>
2025-07-18 11:17:11 +0200

    feat: raf eval

--- zkml-jolt-core/src/jolt/vm/bytecode.rs
+++ zkml-jolt-core/src/jolt/vm/bytecode.rs
@@ -4,6 +4,7 @@ use jolt_core::{
     poly::{
         compact_polynomial::SmallScalar,
         eq_poly::EqPolynomial,
+        identity_poly::IdentityPolynomial,
         multilinear_polynomial::{
             BindingOrder, MultilinearPolynomial, PolynomialBinding, PolynomialEvaluation,
         },
@@ -12,7 +13,7 @@ use jolt_core::{
     subprotocols::sumcheck::SumcheckInstanceProof,
     utils::{
         math::Math,
-        transcript::{self, AppendToTranscript, Transcript},
+        transcript::{AppendToTranscript, Transcript},
     },
 };
 use onnx_tracer::trace_types::{ONNXCycle, ONNXInstr};
@@ -142,6 +143,7 @@ where
 }
 
 pub fn prove_booleanity<F, ProofTranscript>(
+    trace: &[ONNXCycle],
     r: &[F],
     D: Vec<F>,
     G: Vec<F>,
@@ -150,5 +152,227 @@ pub fn prove_booleanity<F, ProofTranscript>(
     ProofTranscript: Transcript,
     F: JoltField,
 {
-    let B = EqPolynomial::evals(r);
+    const DEGREE: usize = 3;
+    let K = r.len().pow2();
+    let T = trace.len();
+    let mut B = MultilinearPolynomial::from(EqPolynomial::evals(r));
+
+    let num_rounds = K.log_2() + T.log_2();
+    let mut compressed_polys: Vec<CompressedUniPoly<F>> = Vec::with_capacity(num_rounds);
+
+    // --- First log(K) rounds of sumcheck ---
+    let mut previous_claim = F::zero();
+    // EQ(k_m, c) for k_m \in {0, 1} and c \in {0, 2, 3}
+    let eq_km_c: [[F; DEGREE]; 2] = [
+        [
+            F::one(),        // eq(0, 0) = 0 * 0 + (1 - 0) * (1 - 0)
+            F::from_i64(-1), // eq(0, 2) = 0 * 2 + (1 - 0) * (1 - 2)
+            F::from_i64(-2), // eq(0, 3) = 0 * 3 + (1 - 0) * (1 - 3)
+        ],
+        [
+            F::zero(),     // eq(1, 0) = 1 * 0 + (1 - 1) * (1 - 0)
+            F::from_u8(2), // eq(1, 2) = 1 * 2 + (1 - 1) * (1 - 2)
+            F::from_u8(3), // eq(1, 3) = 1 * 3 + (1 - 1) * (1 - 3)
+        ],
+    ];
+    // EQ(k_m, c)^2 for k_m \in {0, 1} and c \in {0, 2, 3}
+    let eq_km_c_squared: [[F; DEGREE]; 2] = [
+        [F::one(), F::one(), F::from_u8(4)],
+        [F::zero(), F::from_u8(4), F::from_u8(9)],
+    ];
+    let mut F = vec![F::zero(); K];
+    F[0] = F::one();
+    let mut r_address_prime: Vec<F> = Vec::with_capacity(K.log_2());
+    for round in 0..K.log_2() {
+        let m = round + 1;
+        let univariate_poly_evals: [F; 3] = (0..B.len() / 2)
+            .into_par_iter()
+            .map(|k_prime| {
+                let B_evals = B.sumcheck_evals(k_prime, DEGREE, BindingOrder::LowToHigh);
+                let inner_sum = G[k_prime << m..(k_prime + 1) << m]
+                    .par_iter()
+                    .enumerate()
+                    .map(|(k, &G_k)| {
+                        // Since we're binding variables from low to high, k_m is the high bit
+                        let k_m = k >> (m - 1);
+                        // We then index into F using (k_{m-1}, ..., k_1)
+                        let F_k = F[k % (1 << (m - 1))];
+                        // G_times_F := G[k] * F[k_1, ...., k_{m-1}]
+                        let G_times_F = G_k * F_k;
+                        // For c \in {0, 2, 3} compute:
+                        //    G[k] * (F[k_1, ...., k_{m-1}, c]^2 - F[k_1, ...., k_{m-1}, c])
+                        //    = G_times_F * (eq(k_m, c)^2 * F[k_1, ...., k_{m-1}] - eq(k_m, c))
+                        [
+                            G_times_F * (eq_km_c_squared[k_m][0] * F_k - eq_km_c[k_m][0]),
+                            G_times_F * (eq_km_c_squared[k_m][1] * F_k - eq_km_c[k_m][1]),
+                            G_times_F * (eq_km_c_squared[k_m][2] * F_k - eq_km_c[k_m][2]),
+                        ]
+                    })
+                    .reduce(
+                        || [F::zero(); 3],
+                        |running, new| {
+                            [
+                                running[0] + new[0],
+                                running[1] + new[1],
+                                running[2] + new[2],
+                            ]
+                        },
+                    );
+                [
+                    B_evals[0] * inner_sum[0],
+                    B_evals[1] * inner_sum[1],
+                    B_evals[2] * inner_sum[2],
+                ]
+            })
+            .reduce(
+                || [F::zero(); 3],
+                |running, new| {
+                    [
+                        running[0] + new[0],
+                        running[1] + new[1],
+                        running[2] + new[2],
+                    ]
+                },
+            );
+        let univariate_poly = UniPoly::from_evals(&[
+            univariate_poly_evals[0],
+            previous_claim - univariate_poly_evals[0],
+            univariate_poly_evals[1],
+            univariate_poly_evals[2],
+        ]);
+        let compressed_poly = univariate_poly.compress();
+        compressed_poly.append_to_transcript(transcript);
+        compressed_polys.push(compressed_poly);
+        let r_j = transcript.challenge_scalar::<F>();
+        r_address_prime.push(r_j);
+        previous_claim = univariate_poly.evaluate(&r_j);
+        B.bind_parallel(r_j, BindingOrder::LowToHigh);
+
+        // Update F for this round (see Equation 55)
+        let (F_left, F_right) = F.split_at_mut(1 << round);
+        F_left
+            .par_iter_mut()
+            .zip(F_right.par_iter_mut())
+            .for_each(|(x, y)| {
+                *y = *x * r_j;
+                *x -= *y;
+            });
+    }
+
+    // Last log(T) rounds of sumcheck
+    let eq_r_r = B.final_sumcheck_claim();
+    let H: Vec<F> = trace.iter().map(|cycle| F[cycle.instr.address]).collect();
+    let mut H = MultilinearPolynomial::from(H);
+    let mut D = MultilinearPolynomial::from(D);
+    let mut r_cycle_prime: Vec<F> = Vec::with_capacity(T.log_2());
+    for _round in 0..T.log_2() {
+        #[cfg(test)]
+        {
+            let expected: F = eq_r_r
+                * (0..H.len())
+                    .map(|j| {
+                        let D_j = D.get_bound_coeff(j);
+                        let H_j = H.get_bound_coeff(j);
+                        D_j * (H_j.square() - H_j)
+                    })
+                    .sum::<F>();
+            assert_eq!(
+                expected, previous_claim,
+                "Sumcheck sanity check failed in round {_round}"
+            );
+        }
+        let mut univariate_poly_evals: [F; 3] = (0..D.len() / 2)
+            .into_par_iter()
+            .map(|i| {
+                let D_evals = D.sumcheck_evals(i, DEGREE, BindingOrder::LowToHigh);
+                let H_evals = H.sumcheck_evals(i, DEGREE, BindingOrder::LowToHigh);
+
+                [
+                    D_evals[0] * (H_evals[0] * H_evals[0] - H_evals[0]),
+                    D_evals[1] * (H_evals[1] * H_evals[1] - H_evals[1]),
+                    D_evals[2] * (H_evals[2] * H_evals[2] - H_evals[2]),
+                ]
+            })
+            .reduce(
+                || [F::zero(); 3],
+                |running, new| {
+                    [
+                        running[0] + new[0],
+                        running[1] + new[1],
+                        running[2] + new[2],
+                    ]
+                },
+            );
+        univariate_poly_evals = [
+            eq_r_r * univariate_poly_evals[0],
+            eq_r_r * univariate_poly_evals[1],
+            eq_r_r * univariate_poly_evals[2],
+        ];
+        let univariate_poly = UniPoly::from_evals(&[
+            univariate_poly_evals[0],
+            previous_claim - univariate_poly_evals[0],
+            univariate_poly_evals[1],
+            univariate_poly_evals[2],
+        ]);
+        let compressed_poly = univariate_poly.compress();
+        compressed_poly.append_to_transcript(transcript);
+        compressed_polys.push(compressed_poly);
+        let r_j = transcript.challenge_scalar::<F>();
+        r_cycle_prime.push(r_j);
+        previous_claim = univariate_poly.evaluate(&r_j);
+
+        // Bind polynomials
+        rayon::join(
+            || D.bind_parallel(r_j, BindingOrder::LowToHigh),
+            || H.bind_parallel(r_j, BindingOrder::LowToHigh),
+        );
+    }
+}
+
+pub fn prove_raf_eval<F, ProofTranscript>(y: F, F: Vec<F>, transcript: &mut ProofTranscript)
+where
+    ProofTranscript: Transcript,
+    F: JoltField,
+{
+    const DEGREE: usize = 2;
+    let K = F.len();
+    let num_rounds = K.log_2();
+    let mut ra = MultilinearPolynomial::from(F);
+    let mut int = IdentityPolynomial::<F>::new(num_rounds);
+    let mut prev_claim = y;
+    let mut compressed_polys: Vec<CompressedUniPoly<F>> = Vec::with_capacity(num_rounds);
+    let mut r_address_double_prime: Vec<F> = Vec::with_capacity(num_rounds);
+    for _ in 0..num_rounds {
+        let uni_poly_evals: [F; 2] = (0..ra.len() / 2)
+            .into_par_iter()
+            .map(|index| {
+                let ra_evals = ra.sumcheck_evals(index, DEGREE, BindingOrder::HighToLow);
+                let int_evals = int.sumcheck_evals(index, DEGREE, BindingOrder::HighToLow);
+                [ra_evals[0] * int_evals[0], ra_evals[1] * int_evals[1]]
+            })
+            .reduce(
+                || [F::zero(); 2],
+                |running, new| [running[0] + new[0], running[1] + new[1]],
+            );
+        let uni_poly = UniPoly::from_evals(&[
+            uni_poly_evals[0],
+            prev_claim - uni_poly_evals[0],
+            uni_poly_evals[1],
+        ]);
+        let compressed_poly = uni_poly.compress();
+        compressed_poly.append_to_transcript(transcript);
+        compressed_polys.push(compressed_poly);
+
+        let r_j = transcript.challenge_scalar::<F>();
+        r_address_double_prime.push(r_j);
+        prev_claim = uni_poly.evaluate(&r_j);
+
+        // Bind polynomials
+        rayon::join(
+            || ra.bind_parallel(r_j, BindingOrder::HighToLow),
+            || int.bind_parallel(r_j, BindingOrder::HighToLow),
+        );
+    }
+    let sc: SumcheckInstanceProof<F, ProofTranscript> =
+        SumcheckInstanceProof::new(compressed_polys);
 }
