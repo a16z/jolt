b46e5e2983346cf6dbcd40042b7fe6baa0d1b6d7
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2025-08-01 17:50:32 -0400

    Revert "temp: modify muldiv example"
    This reverts commit 6738606d1f53a89863557186925e0011f7a40cc6.

--- examples/muldiv/src/main.rs
+++ examples/muldiv/src/main.rs
@@ -1,16 +1,20 @@
+use std::time::Instant;
+
 pub fn main() {
     let target_dir = "/tmp/jolt-guest-targets";
-    let mut program = guest::compile_test(target_dir);
+    let mut program = guest::compile_muldiv(target_dir);
 
-    let prover_preprocessing = guest::preprocess_prover_test(&mut program);
+    let prover_preprocessing = guest::preprocess_prover_muldiv(&mut program);
     let verifier_preprocessing =
-        guest::verifier_preprocessing_from_prover_test(&prover_preprocessing);
+        guest::verifier_preprocessing_from_prover_muldiv(&prover_preprocessing);
 
-    let prove_test = guest::build_prover_test(program, prover_preprocessing);
-    let verify_test = guest::build_verifier_test(verifier_preprocessing);
+    let prove = guest::build_prover_muldiv(program, prover_preprocessing);
+    let verify = guest::build_verifier_muldiv(verifier_preprocessing);
 
-    let (output, proof, program_io) = prove_test(50);
-    let is_valid = verify_test(50, output, program_io.panic, proof);
+    let now = Instant::now();
+    let (output, proof, program_io) = prove(12031293, 17, 92);
+    println!("Prover runtime: {} s", now.elapsed().as_secs_f64());
+    let is_valid = verify(12031293, 17, 92, output, program_io.panic, proof);
 
     println!("output: {output}");
     println!("valid: {is_valid}");
