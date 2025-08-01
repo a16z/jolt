6738606d1f53a89863557186925e0011f7a40cc6
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2025-08-01 16:53:33 -0400

    temp: modify muldiv example
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- examples/muldiv/guest/src/lib.rs
+++ examples/muldiv/guest/src/lib.rs
@@ -1,12 +1,7 @@
 #![cfg_attr(feature = "guest", no_std)]
-use core::hint::black_box;
+#![allow(arithmetic_overflow)]
 
 #[jolt::provable(memory_size = 10240, max_trace_length = 65536)]
-fn muldiv(a: u32, b: u32, c: u32) -> u32 {
-    use jolt::{end_cycle_tracking, start_cycle_tracking};
-
-    start_cycle_tracking("muldiv");
-    let result = black_box(a * b / c); // use black_box to keep code in place
-    end_cycle_tracking("muldiv");
-    result
+fn test(x: u32) -> u32 {
+    3 % x.pow(x)
 }
