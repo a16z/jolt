b30575b4015563146c24ca5947fdf413b4b9f08c
danielwlz <183745461+danielwlz@users.noreply.github.com>
2025-08-11 18:13:35 +0800

    Patch sha3-ex and sha3-chain to use keccak inline

--- examples/sha3-chain/guest/src/lib.rs
+++ examples/sha3-chain/guest/src/lib.rs
@@ -2,10 +2,9 @@
 
 #[jolt::provable(memory_size = 10240, max_trace_length = 65536)]
 fn sha3_chain(input: [u8; 32], num_iters: u32) -> [u8; 32] {
-    let hash = input;
+    let mut hash = input;
     for _ in 0..num_iters {
-        todo!()
-        // hash = jolt::keccak256::Keccak256::digest(&hash);
+        hash = inlines::Keccak256::digest(&hash);
     }
     hash
 }
