58974758d5a5f508b9c39545df9cbac42465ccfd
Sagar Dhawan <107961892+sagar-a16z@users.noreply.github.com>
2025-08-15 18:17:34 -0400

    add a verifier example (#869)
    * add a verifier example
    
    * fix tests
    
    * update test fixtures to fix verifier testss
    
    * fmt

---
+++ examples/verifier/guest/src/lib.rs
@@ -0,0 +1,38 @@
+use jolt::{end_cycle_tracking, start_cycle_tracking};
+use jolt_verifier::zkvm::{JoltRV32IM, JoltVerifierPreprocessing, RV32IMJoltProof, Serializable};
+use jolt_verifier::{common, DoryCommitmentScheme, Jolt};
+
+mod fib_io_device_bytes;
+mod fib_proof_bytes;
+mod jolt_verifier_preprocessing_bytes;
+
+#[jolt::provable(
+    memory_size = 33554432,
+    max_trace_length = 67108864,
+    stack_size = 1048576
+)]
+fn verifier() {
+    start_cycle_tracking("preprocessing");
+    let preprocessing: JoltVerifierPreprocessing<ark_bn254::Fr, DoryCommitmentScheme> =
+        JoltVerifierPreprocessing::deserialize_from_bytes(
+            jolt_verifier_preprocessing_bytes::VERIFIER_PREPROCESSING_BYTES,
+        )
+        .unwrap();
+    end_cycle_tracking("preprocessing");
+    start_cycle_tracking("proof");
+    let proof = RV32IMJoltProof::deserialize_from_bytes(fib_proof_bytes::FIB_PROOF_BYTES).unwrap();
+    end_cycle_tracking("proof");
+    start_cycle_tracking("device");
+    let device = common::jolt_device::JoltDevice::deserialize_from_bytes(
+        fib_io_device_bytes::FIB_IO_DEVICE_BYTES,
+    )
+    .unwrap();
+    end_cycle_tracking("device");
+    // assert!(device.memory_layout.stack_size > 1024);
+    // assert!(proof.trace_length > 0);
+    start_cycle_tracking("verification");
+    let verifier = JoltRV32IM::verify(&preprocessing, proof, device, None);
+    end_cycle_tracking("verification");
+
+    assert!(verifier.is_ok(), "Verifier failed: {:?}", verifier.err());
+}
