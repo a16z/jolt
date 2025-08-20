319c6865d59895937b837818725aa7f19e751f2d
MozirDmitriy <dmitriymozir@gmail.com>
2025-08-20 18:17:06 +0300

    fix: prevent overflow in negative i64/i128 field conversions

--- jolt-core/src/field/ark.rs
+++ jolt-core/src/field/ark.rs
@@ -70,7 +70,7 @@ impl JoltField for ark_bn254::Fr {
 
     fn from_i64(val: i64) -> Self {
         if val.is_negative() {
-            let val = (-val) as u64;
+            let val = val.unsigned_abs();
             if val <= u16::MAX as u64 {
                 -<Self as JoltField>::from_u16(val as u16)
             } else if val <= u32::MAX as u64 {
@@ -92,7 +92,7 @@ impl JoltField for ark_bn254::Fr {
 
     fn from_i128(val: i128) -> Self {
         if val.is_negative() {
-            let val = (-val) as u128;
+            let val = val.unsigned_abs();
             if val <= u16::MAX as u128 {
                 -<Self as JoltField>::from_u16(val as u16)
             } else if val <= u32::MAX as u128 {
