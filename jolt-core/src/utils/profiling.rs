598ef7d341ee0f18a138221ea96a0d8a291e2815
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2025-09-30 12:36:37 -0400

    bring back print_current_memory_usage
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- jolt-core/src/utils/profiling.rs
+++ jolt-core/src/utils/profiling.rs
@@ -54,35 +54,40 @@ pub fn report_memory_usage() {
     tracing::info!("=====================================================");
 }
 
-// #[cfg(not(target_arch = "wasm32"))]
-// pub fn print_current_memory_usage(label: &str) {
-//     if let Some(usage) = memory_stats() {
-//         let memory_usage_gb = usage.physical_mem as f64 / 1_000_000_000.0;
-//         if memory_usage_gb >= 1.0 {
-//             tracing::debug!("\"{label}\" current memory usage: {memory_usage_gb:.2} GB");
-//         } else {
-//             tracing::debug!(
-//                 "\"{}\" current memory usage: {:.2} MB",
-//                 label,
-//                 memory_usage_gb * 1000.0
-//             );
-//         }
-//     } else {
-//         tracing::debug!("Failed to get current memory usage (\"{label}\")");
-//     }
-// }
+#[cfg(not(target_arch = "wasm32"))]
+pub fn print_current_memory_usage(label: &str) {
+    if tracing::enabled!(tracing::Level::DEBUG) {
+        if let Some(usage) = memory_stats() {
+            let memory_usage_gb = usage.physical_mem as f64 / 1_000_000_000.0;
+            if memory_usage_gb >= 1.0 {
+                tracing::debug!("\"{label}\" current memory usage: {memory_usage_gb:.2} GB");
+            } else {
+                tracing::debug!(
+                    "\"{}\" current memory usage: {:.2} MB",
+                    label,
+                    memory_usage_gb * 1000.0
+                );
+            }
+        } else {
+            tracing::debug!("Failed to get current memory usage (\"{label}\")");
+        }
+    }
+}
 
 #[cfg(feature = "allocative")]
 pub fn print_data_structure_heap_usage<T: Allocative>(label: &str, data: &T) {
-    let memory_usage_gb = allocative::size_of_unique_allocated_data(data) as f64 / 1_000_000_000.0;
-    if memory_usage_gb >= 1.0 {
-        tracing::debug!("\"{label}\" memory usage: {memory_usage_gb:.2} GB");
-    } else {
-        tracing::debug!(
-            "\"{}\" memory usage: {:.2} MB",
-            label,
-            memory_usage_gb * 1000.0
-        );
+    if tracing::enabled!(tracing::Level::DEBUG) {
+        let memory_usage_gb =
+            allocative::size_of_unique_allocated_data(data) as f64 / 1_000_000_000.0;
+        if memory_usage_gb >= 1.0 {
+            tracing::debug!("\"{label}\" memory usage: {memory_usage_gb:.2} GB");
+        } else {
+            tracing::debug!(
+                "\"{}\" memory usage: {:.2} MB",
+                label,
+                memory_usage_gb * 1000.0
+            );
+        }
     }
 }
 
