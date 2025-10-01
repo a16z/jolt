3aa111c877f8ba9453a4df0095e084c60f7d9df4
Andrew Milson <andrewmilson@users.noreply.github.com>
2025-10-01 16:56:13 -0400

    Only materialize ra_i polynomials after 2nd sumcheck round (#968)

--- jolt-core/src/zkvm/instruction_lookups/ra_virtual.rs
+++ jolt-core/src/zkvm/instruction_lookups/ra_virtual.rs
@@ -1,35 +1,31 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::Rc, sync::Arc};
 
 use allocative::Allocative;
 use common::constants::XLEN;
 use itertools::chain;
-use rayon::{
-    iter::{
-        IndexedParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator,
-        ParallelIterator,
-    },
-    slice::ParallelSlice,
+use rayon::iter::{
+    IntoParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator,
 };
-use tracer::instruction::Cycle;
 
 use crate::{
     field::JoltField,
     poly::{
         commitment::commitment_scheme::CommitmentScheme,
         eq_poly::EqPolynomial,
-        multilinear_polynomial::{BindingOrder, MultilinearPolynomial, PolynomialBinding},
+        multilinear_polynomial::{BindingOrder, PolynomialBinding},
         opening_proof::{
             OpeningPoint, ProverOpeningAccumulator, SumcheckId, VerifierOpeningAccumulator,
             BIG_ENDIAN,
         },
+        ra_poly::RaPolynomial,
     },
     subprotocols::{mles_product_sum::compute_mles_product_sum, sumcheck::SumcheckInstance},
     transcripts::Transcript,
-    utils::{lookup_bits::LookupBits, math::Math, thread::drop_in_background_thread},
+    utils::lookup_bits::LookupBits,
     zkvm::{
         dag::state_manager::StateManager,
         instruction::LookupQuery,
-        instruction_lookups::{D, K_CHUNK, LOG_K, LOG_K_CHUNK},
+        instruction_lookups::{D, LOG_K, LOG_K_CHUNK},
         witness::{CommittedPolynomial, VirtualPolynomial},
     },
 };
@@ -38,70 +34,39 @@ use crate::{
 pub struct RaSumcheck<F: JoltField> {
     r_cycle: Vec<F>,
     input_claim: F,
-    T: usize,
     prover_state: Option<RaProverState<F>>,
 }
 
 #[derive(Allocative)]
 pub struct RaProverState<F: JoltField> {
-    ra_i_polys: Vec<MultilinearPolynomial<F>>,
+    ra_i_polys: Vec<RaPolynomial<F>>,
     /// Challenges drawn throughout  the sumcheck.
     r_sumcheck: Vec<F>,
 }
 
 impl<F: JoltField> RaSumcheck<F> {
-    #[tracing::instrument(skip_all, name = "InstructionRaSumcheck::compute_ra_i_polys")]
-    fn compute_ra_i_polys(
-        trace: &[Cycle],
-        state_manager: &StateManager<'_, F, impl Transcript, impl CommitmentScheme<Field = F>>,
-    ) -> Vec<MultilinearPolynomial<F>> {
-        let lookup_indices: Vec<_> = trace
-            .par_iter()
-            .map(|cycle| LookupBits::new(LookupQuery::<XLEN>::to_lookup_index(cycle), LOG_K))
-            .collect();
-
-        // Retrieve the random address variables generated in ReadRafSumcheck.
-        let (r, _claim) = state_manager.get_virtual_polynomial_opening(
-            VirtualPolynomial::InstructionRa,
-            SumcheckId::InstructionReadRaf,
-        );
-        let (r_address, _r_cycle) = r.split_at_r(LOG_K);
-
-        let ra_i_ploys = r_address
-            .par_chunks(LOG_K_CHUNK)
-            .enumerate()
-            .map(|(i, chunk)| {
-                let eq = EqPolynomial::evals(chunk);
-                let ra = lookup_indices
-                    .par_iter()
-                    .map(|k| {
-                        let k_bound =
-                            (u128::from(k) >> (LOG_K_CHUNK * (D - i - 1))) % K_CHUNK as u128;
-                        eq[k_bound as usize]
-                    })
-                    .collect::<Vec<F>>();
-                drop_in_background_thread(eq);
-                MultilinearPolynomial::from(ra)
-            })
-            .collect();
-        drop_in_background_thread(lookup_indices);
-        ra_i_ploys
-    }
-
     pub fn new_prover<ProofTranscript: Transcript, PCS: CommitmentScheme<Field = F>>(
         state_manager: &mut StateManager<'_, F, ProofTranscript, PCS>,
     ) -> Self {
         let (_preprocessing, trace, _, _) = state_manager.get_prover_data();
-        let T = trace.len();
 
         let (r, ra_claim) = state_manager.get_virtual_polynomial_opening(
             VirtualPolynomial::InstructionRa,
             SumcheckId::InstructionReadRaf,
         );
 
-        let (_, r_cycle) = r.split_at_r(LOG_K);
+        let (r_address, r_cycle) = r.split_at_r(LOG_K);
 
-        let ra_i_polys = Self::compute_ra_i_polys(trace, state_manager);
+        let lookup_indices: Arc<[LookupBits]> = trace
+            .par_iter()
+            .map(|cycle| LookupBits::new(LookupQuery::<XLEN>::to_lookup_index(cycle), LOG_K))
+            .collect::<Vec<_>>()
+            .into();
+
+        let ra_i_polys = (0..D)
+            .into_par_iter()
+            .map(|i| RaPolynomial::new(lookup_indices.clone(), D, LOG_K_CHUNK, r_address, i))
+            .collect();
 
         let prover_state = RaProverState {
             ra_i_polys,
@@ -111,7 +76,6 @@ impl<F: JoltField> RaSumcheck<F> {
         Self {
             r_cycle: r_cycle.to_vec(),
             input_claim: ra_claim,
-            T,
             prover_state: Some(prover_state),
         }
     }
@@ -119,7 +83,6 @@ impl<F: JoltField> RaSumcheck<F> {
     pub fn new_verifier<ProofTranscript: Transcript, PCS: CommitmentScheme<Field = F>>(
         state_manager: &mut StateManager<'_, F, ProofTranscript, PCS>,
     ) -> Self {
-        let (_, _, T) = state_manager.get_verifier_data();
         let (r, ra_claim) = state_manager.get_virtual_polynomial_opening(
             VirtualPolynomial::InstructionRa,
             SumcheckId::InstructionReadRaf,
@@ -128,7 +91,6 @@ impl<F: JoltField> RaSumcheck<F> {
         Self {
             r_cycle: r_cycle.to_vec(),
             input_claim: ra_claim,
-            T,
             prover_state: None,
         }
     }
@@ -140,7 +102,7 @@ impl<F: JoltField> SumcheckInstance<F> for RaSumcheck<F> {
     }
 
     fn num_rounds(&self) -> usize {
-        self.T.log_2()
+        self.r_cycle.len()
     }
 
     fn input_claim(&self) -> F {
@@ -149,18 +111,12 @@ impl<F: JoltField> SumcheckInstance<F> for RaSumcheck<F> {
 
     #[tracing::instrument(skip_all, name = "InstructionRaSumcheck::compute_prover_message")]
     fn compute_prover_message(&mut self, _round: usize, previous_claim: F) -> Vec<F> {
-        let prover_state = self
-            .prover_state
-            .as_ref()
-            .expect("Prover state not initialized");
+        let prover_state = self.prover_state.as_ref().unwrap();
         let ra_i_polys = &prover_state.ra_i_polys;
+        let r_cycle = &self.r_cycle;
+        let r_sumcheck = &prover_state.r_sumcheck;
 
-        let poly = compute_mles_product_sum(
-            ra_i_polys,
-            previous_claim,
-            &self.r_cycle,
-            &prover_state.r_sumcheck,
-        );
+        let poly = compute_mles_product_sum(ra_i_polys, previous_claim, r_cycle, r_sumcheck);
 
         // Evaluate the poly at 0, 2, 3, ..., degree.
         let degree = self.degree();
@@ -179,7 +135,7 @@ impl<F: JoltField> SumcheckInstance<F> for RaSumcheck<F> {
         prover_state
             .ra_i_polys
             .par_iter_mut()
-            .for_each(|poly| poly.bind_parallel(r_j, BindingOrder::HighToLow));
+            .for_each(|p| p.bind_parallel(r_j, BindingOrder::HighToLow));
 
         prover_state.r_sumcheck.push(r_j);
     }
