448384700b3431e3149d28c42dd5a31b4e13a162
Andrew Tretyakov <atretyakov@a16z.com>
2025-10-21 16:31:49 -0400

    ref: remove virtual move (#1033)
    * remove virtual move
    
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
    
    * fix arch tests
    
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- jolt-core/src/zkvm/instruction/mod.rs
+++ jolt-core/src/zkvm/instruction/mod.rs
@@ -253,7 +253,7 @@ define_rv32im_trait_impls! {
         VirtualAssertWordAlignment, VirtualAssertLTE,
         VirtualAssertValidDiv0, VirtualAssertValidUnsignedRemainder,
         VirtualChangeDivisor, VirtualChangeDivisorW, VirtualAssertMulUNoOverflow,
-        VirtualZeroExtendWord, VirtualSignExtendWord, VirtualMove, VirtualMovsign, VirtualMULI, VirtualPow2,
+        VirtualZeroExtendWord, VirtualSignExtendWord, VirtualMovsign, VirtualMULI, VirtualPow2,
         VirtualPow2I, VirtualPow2W, VirtualPow2IW, VirtualRev8W, VirtualShiftRightBitmask, VirtualShiftRightBitmaskI,
         VirtualROTRI, VirtualROTRIW,
         VirtualSRA, VirtualSRAI, VirtualSRL, VirtualSRLI,
@@ -300,7 +300,6 @@ pub mod virtual_assert_valid_unsigned_remainder;
 pub mod virtual_assert_word_alignment;
 pub mod virtual_change_divisor;
 pub mod virtual_change_divisor_w;
-pub mod virtual_move;
 pub mod virtual_movsign;
 pub mod virtual_muli;
 pub mod virtual_pow2;
