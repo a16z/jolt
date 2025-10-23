d43bf88c94225613f5cfe6af7f2e55a92da4524f
Omid Bodaghi <42227752+omibo@users.noreply.github.com>
2025-10-23 12:46:03 -0700

    Revert JALR back to its original implementation

--- tracer/src/instruction/jalr.rs
+++ tracer/src/instruction/jalr.rs
@@ -15,9 +15,9 @@ declare_riscv_instr!(
 impl JALR {
     fn exec(&self, cpu: &mut Cpu, _: &mut <JALR as RISCVInstruction>::RAMAccess) {
         let tmp = cpu.sign_extend(cpu.pc as i64);
-        cpu.pc = ((cpu.x[self.operands.rs1 as usize]
-            .wrapping_add(normalize_imm(self.operands.imm, &cpu.xlen)))
-            & !1) as u64;
+        cpu.pc = ((cpu.x[self.operands.rs1 as usize] as u64)
+            .wrapping_add(self.operands.imm as i32 as u64))
+            & !1;
         if self.operands.rd != 0 {
             // Skip returns (rd=0) and non-standard link registers
             if self.operands.rd == 1 {
