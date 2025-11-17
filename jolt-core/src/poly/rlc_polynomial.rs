cb52cef1ddb64a8d9ffaa42b90a029ff38445561
Ben Hamlin <hamlinb@galois.com>
2025-11-17 09:19:29 -0800

    Implement a streaming version of RLCPolynomial
    This commit implements streaming for the `linear_combination` and
    `vector_matrix_product` methods of `RLCPolynomial`. The dense and one-hot
    implementations are separate for now. Both implementations lazily iterate
    over R entries of the polynomial at a time, where R is the size of a row.
    Parallelizing the iteration is still todo.

--- jolt-core/src/poly/rlc_polynomial.rs
+++ jolt-core/src/poly/rlc_polynomial.rs
@@ -224,3 +224,291 @@ impl<F: JoltField> RLCPolynomial<F> {
         result
     }
 }
+
+type OneHotRow = Vec<Option<u8>>;
+
+#[derive(Debug, Clone)]
+pub struct StreamingOneHotRLCPolynomial<F, I> {
+    polynomials: Vec<I>,
+    coefficients: Vec<F>,
+}
+
+impl<F: JoltField, I: Iterator<Item = OneHotRow>> Iterator for StreamingOneHotRLCPolynomial<F, I> {
+    type Item = Vec<(OneHotRow, F)>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.polynomials
+            .iter_mut()
+            .map(Iterator::next)
+            .zip(&self.coefficients)
+            .map(|(opt_row, coeff)| opt_row.map(|row| (row, *coeff)))
+            .collect()
+    }
+}
+
+impl<F: JoltField, I: Iterator<Item = OneHotRow>> StreamingOneHotRLCPolynomial<F, I> {
+    pub fn linear_combination(polynomials: Vec<I>, coefficients: Vec<F>) -> Self {
+        debug_assert_eq!(polynomials.len(), coefficients.len());
+
+        Self {
+            polynomials,
+            coefficients,
+        }
+    }
+
+    pub fn vector_matrix_product(self, left_vec: &[JoltFieldWrapper<F>]) -> Vec<JoltFieldWrapper<F>> {
+        debug_assert_eq!(left_vec.len(), DoryGlobals::get_max_num_rows());
+        let left_vec: &[F] =
+            unsafe { std::slice::from_raw_parts(left_vec.as_ptr() as *const F, left_vec.len()) };
+
+        let T = DoryGlobals::get_T();
+        let num_cols = DoryGlobals::get_num_columns();
+        // XXX Is this always true?
+        let chunk_len = num_cols;
+
+        // TODO: Use par_iter; think about better ways to organize iteration
+        let mut result = vec![F::zero(); num_cols];
+        if T >= num_cols {
+            let rows_per_k = T / num_cols;
+
+            self.into_iter()
+                .enumerate()
+                .for_each(|(chunk_index, poly_chunks)| {
+                    for (chunk, coeff) in poly_chunks {
+                        for (col_index, dest) in result.iter_mut().enumerate() {
+                            if let Some(k) = chunk[col_index] {
+                                let row_index = k as usize * rows_per_k + chunk_index;
+                                *dest += coeff * left_vec[row_index];
+                            }
+                        }
+                    }
+                });
+        } else {
+            self.into_iter()
+                .enumerate()
+                .for_each(|(chunk_index, poly_chunks)| {
+                    for (chunk, coeff) in poly_chunks {
+                        for (t, k) in chunk.into_iter().enumerate() {
+                            let t = chunk_index * chunk_len + t;
+                            if let Some(k) = k {
+                                let global_index = k as u128 * T as u128 + t as u128;
+                                let col_index = (global_index % num_cols as u128) as usize;
+                                let row_index = (global_index / num_cols as u128) as usize;
+                                result[col_index] += coeff * left_vec[row_index];
+                            }
+                        }
+                    }
+                })
+        }
+
+        result.into_iter().map(JoltFieldWrapper).collect()
+    }
+}
+
+type DenseRow<F> = Vec<F>;
+
+#[derive(Debug, Clone)]
+pub struct StreamingDenseRLCPolynomial<F, I> {
+    polynomials: Vec<I>,
+    coefficients: Vec<F>,
+}
+
+impl<F: JoltField, I: Iterator<Item = DenseRow<F>>> Iterator for StreamingDenseRLCPolynomial<F, I> {
+    type Item = DenseRow<F>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let num_cols = DoryGlobals::get_num_columns();
+        self.polynomials
+            .iter_mut()
+            .map(Iterator::next)
+            .zip(&self.coefficients)
+            .fold(Some(vec![F::zero(); num_cols]), |opt_acc, (opt_row, coefficient)| {
+                match (opt_acc, opt_row) {
+                    (Some(mut acc), Some(row)) => {
+                        for col in 0..row.len() {
+                            acc[col] += *coefficient * row[col];
+                        }
+
+                        Some(acc)
+                    }
+                    _ => None,
+                }
+            })
+    }
+}
+
+impl<F: JoltField, I: Iterator<Item = DenseRow<F>>> StreamingDenseRLCPolynomial<F, I> {
+    pub fn linear_combination(polynomials: Vec<I>, coefficients: Vec<F>) -> Self {
+        debug_assert_eq!(polynomials.len(), coefficients.len());
+
+        Self {
+            polynomials,
+            coefficients,
+        }
+    }
+
+    pub fn vector_matrix_product(self, left_vec: &[JoltFieldWrapper<F>]) -> Vec<JoltFieldWrapper<F>> {
+        debug_assert_eq!(left_vec.len(), DoryGlobals::get_max_num_rows());
+        let left_vec: &[F] =
+            unsafe { std::slice::from_raw_parts(left_vec.as_ptr() as *const F, left_vec.len()) };
+
+        let num_cols = DoryGlobals::get_num_columns();
+        self.into_iter()
+            .zip(left_vec)
+            .fold(vec![F::zero(); num_cols], |mut acc, (row, coefficient)| {
+                for col in 0..num_cols {
+                    acc[col] += *coefficient * row[col];
+                }
+
+                acc
+            })
+            .into_iter()
+            .map(JoltFieldWrapper)
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::poly::one_hot_polynomial::OneHotPolynomial;
+
+    use super::*;
+
+    enum StreamingMultilinearPolynomial<F> {
+        Dense(Vec<DenseRow<F>>),
+        OneHot(Vec<OneHotRow>),
+    }
+
+    impl<F: JoltField> From<&MultilinearPolynomial<F>> for StreamingMultilinearPolynomial<F> {
+        fn from(value: &MultilinearPolynomial<F>) -> Self {
+            let chunk_size = DoryGlobals::get_num_columns();
+
+            match value {
+                MultilinearPolynomial::LargeScalars(poly) => {
+                    let chunks = poly
+                        .Z
+                        .chunks(chunk_size)
+                        .map(|chunk| chunk.to_owned())
+                        .collect::<Vec<_>>();
+                    Self::Dense(chunks)
+                }
+                MultilinearPolynomial::OneHot(poly) => {
+                    let chunks = poly
+                        .nonzero_indices
+                        .chunks(chunk_size)
+                        .map(|chunk| chunk.to_owned())
+                        .collect::<Vec<_>>();
+                    Self::OneHot(chunks)
+                }
+                _ => unimplemented!(),
+            }
+        }
+    }
+
+    impl<F: JoltField> From<StreamingMultilinearPolynomial<F>> for Vec<DenseRow<F>> {
+        fn from(value: StreamingMultilinearPolynomial<F>) -> Self {
+            match value {
+                StreamingMultilinearPolynomial::Dense(poly) => poly,
+                _ => panic!("not a dense polynomial"),
+            }
+        }
+    }
+
+    impl<F: JoltField> From<StreamingMultilinearPolynomial<F>> for Vec<OneHotRow> {
+        fn from(value: StreamingMultilinearPolynomial<F>) -> Self {
+            match value {
+                StreamingMultilinearPolynomial::OneHot(poly) => poly,
+                _ => panic!("not a one-hot polynomial"),
+            }
+        }
+    }
+
+    fn random_multilinear_polynomial<F: JoltField>(rng: &mut impl rand_core::RngCore) -> MultilinearPolynomial<F> {
+        let T = DoryGlobals::get_T();
+        let K = (DoryGlobals::get_num_columns() * DoryGlobals::get_max_num_rows()) / T;
+
+        (rng.next_u64() % 2 == 1)
+            .then(|| {
+                let indices: Vec<Option<u8>> = (0..T)
+                    .map(|_| (rng.next_u64() % 2 == 1).then(|| (rng.next_u64() % (K as u64)) as u8))
+                    .collect();
+                MultilinearPolynomial::OneHot(OneHotPolynomial::from_indices(indices, K))
+            })
+            .unwrap_or_else(|| {
+                let coeffs: Vec<F> = (0..T)
+                    .map(|_| F::random(rng))
+                    .collect();
+                MultilinearPolynomial::from(coeffs)
+            })
+    }
+
+    fn streaming_vector_matrix_product_k_t(K: usize, T: usize) {
+        type F = ark_bn254::Fr;
+
+        let _guard = DoryGlobals::initialize(K, T);
+        let mut rng = rand_core::OsRng;
+
+        let num_polys = 22;
+        let num_rows = DoryGlobals::get_max_num_rows();
+
+        let polynomials: Vec<MultilinearPolynomial<F>> = (0..num_polys)
+            .map(|_| random_multilinear_polynomial(&mut rng))
+            .collect();
+        let coefficients: Vec<F> = (0..num_polys).map(|_| F::random(&mut rng)).collect();
+        let left_vector: Vec<_> = (0..num_rows).map(|_| JoltFieldWrapper(F::random(&mut rng))).collect();
+
+        let streaming_product: Vec<JoltFieldWrapper<F>> = {
+            // Separate out dense and one-hot polynomials with their respective coefficients
+            let (dense_polys_and_coeffs, one_hot_polys_and_coeffs) = polynomials
+                .iter()
+                .map(StreamingMultilinearPolynomial::from)
+                .zip(coefficients.clone())
+                .partition::<Vec<_>, _>(|(poly, _)| match poly {
+                    StreamingMultilinearPolynomial::Dense(_) => true,
+                    StreamingMultilinearPolynomial::OneHot(_) => false,
+                });
+            let (dense_polys, dense_coeffs): (Vec<_>, Vec<F>) = dense_polys_and_coeffs
+                .into_iter()
+                .map(|(poly, coeff)| (<Vec<DenseRow<F>>>::from(poly).into_iter(), coeff))
+                .unzip();
+            let (one_hot_polys, one_hot_coeffs): (Vec<_>, Vec<F>) = one_hot_polys_and_coeffs
+                .into_iter()
+                .map(|(poly, coeff)| (<Vec<OneHotRow>>::from(poly).into_iter(), coeff))
+                .unzip();
+
+            let dense_prod = StreamingDenseRLCPolynomial::linear_combination(dense_polys, dense_coeffs)
+                .vector_matrix_product(&left_vector);
+            let dense_prod: &[F] =
+                unsafe { std::slice::from_raw_parts(dense_prod.as_ptr() as *const F, dense_prod.len()) };
+            let one_hot_prod = StreamingOneHotRLCPolynomial::linear_combination(one_hot_polys, one_hot_coeffs)
+                .vector_matrix_product(&left_vector);
+            let one_hot_prod: &[F] =
+                unsafe { std::slice::from_raw_parts(one_hot_prod.as_ptr() as *const F, one_hot_prod.len()) };
+            dense_prod.into_iter()
+                .zip(one_hot_prod)
+                .map(|(a, b)| JoltFieldWrapper(a + b))
+                .collect()
+        };
+
+        let non_streaming_product: Vec<JoltFieldWrapper<F>> = {
+            let polynomials: Vec<Arc<MultilinearPolynomial<F>>> = polynomials
+                .into_iter()
+                .map(|poly| Arc::new(MultilinearPolynomial::from(poly)))
+                .collect();
+            RLCPolynomial::linear_combination(polynomials, &coefficients)
+                .vector_matrix_product(&left_vector)
+        };
+
+        assert_eq!(
+            streaming_product,
+            non_streaming_product,
+        )
+    }
+
+    #[test]
+    fn streaming_vector_matrix_product() {
+        streaming_vector_matrix_product_k_t(1, 256);
+        streaming_vector_matrix_product_k_t(256, 64);
+        streaming_vector_matrix_product_k_t(256, 4096);
+    }
+}
