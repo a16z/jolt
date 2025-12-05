cfaebdab75379265a6dc285715286536c9567dda
Ben Hamlin <hamlinb@galois.com>
2025-12-04 16:45:26 -0800

    Make extracted sumchecks into a a working module

--- zklean-extractor/src/sumchecks.rs
+++ zklean-extractor/src/sumchecks.rs
@@ -3,15 +3,17 @@ use std::collections::HashMap;
 use jolt_core::{
     field::JoltField,
     poly::opening_proof::SumcheckId,
-    subprotocols::sumcheck_claim::{ClaimExpr, InputOutputClaims, SumcheckFrontend},
+    subprotocols::sumcheck_claim::{Claim, ClaimExpr, InputOutputClaims, SumcheckFrontend},
     zkvm::{
         ram::read_write_checking::RamReadWriteCheckingVerifier,
         registers::read_write_checking::RegistersReadWriteCheckingVerifier,
         spartan::{
             instruction_input::InstructionInputSumcheckVerifier, shift::ShiftSumcheckVerifier,
         },
+        witness::{CommittedPolynomial, VirtualPolynomial},
     },
 };
+use regex::{NoExpand, Regex};
 
 use crate::{
     modules::{AsModule, Module},
@@ -62,7 +64,7 @@ impl<F: JoltField> ZkLeanSumchecks<F> {
                 self.insert_vars_from_claim_expr(sumcheck_id, &e2);
             }
             ClaimExpr::CommittedVar(committed_polynomial) => {
-                let var_name = std::format!("{committed_polynomial:?}");
+                let var_name = committed_var_ident(committed_polynomial);
                 self.sumchecks
                     .entry(sumcheck_id)
                     .and_modify(|s| s.insert_var(&var_name))
@@ -72,7 +74,7 @@ impl<F: JoltField> ZkLeanSumchecks<F> {
                     });
             }
             ClaimExpr::VirtualVar(virtual_polynomial) => {
-                let var_name = std::format!("{virtual_polynomial:?}");
+                let var_name = virtual_var_ident(virtual_polynomial);
                 self.sumchecks
                     .entry(sumcheck_id)
                     .and_modify(|s| s.insert_var(&var_name))
@@ -114,85 +116,230 @@ impl<F: JoltField> ZkLeanSumchecks<F> {
         f: &mut impl std::io::Write,
         mut indent_level: usize,
     ) -> std::io::Result<()> {
-        writeln!(f, "{}/-", indent(indent_level))?;
+        let sumcheck_vars_typ = "SumcheckVars";
+        let single_step_claims_fun_name = "uniform_claims";
+        let cross_step_claims_fun_name = "non_uniform_claims";
 
+        let mut vars_types: Vec<String> = vec![];
         for (id, sumcheck) in &self.sumchecks {
             if sumcheck.vars.len() > 0 {
-                writeln!(f, "{}struct {id:?} [f: field]", indent(indent_level))?;
+                let typename = sumcheck_ident(id);
+                vars_types.push(typename.clone());
+                writeln!(f, "{}structure {typename} (f : Type) : Type where", indent(indent_level))?;
+
                 indent_level += 1;
                 for var in &sumcheck.vars {
-                    writeln!(f, "{}{var} : f", indent(indent_level))?;
+                    writeln!(f, "{}{var} : ZKExpr f", indent(indent_level))?;
                 }
                 indent_level -= 1;
             }
-            writeln!(f, "")?;
+            writeln!(f)?;
         }
 
-        writeln!(f, "{}def claims [f : field] : bool", indent(indent_level))?;
+        writeln!(f, "{}structure {sumcheck_vars_typ} (f : Type) : Type where", indent(indent_level))?;
         indent_level += 1;
+        for typ in vars_types {
+            writeln!(f, "{}{typ} : {typ} f", indent(indent_level))?;
+        }
+        indent_level -= 1;
+        writeln!(f)?;
+
+        let mut single_step_claims_funs: Vec<String> = vec![];
+        let mut cross_step_claims_funs: Vec<String> = vec![];
         for (_, sumcheck) in &self.sumchecks {
             match &sumcheck.claims {
                 Some(claims) => {
-                    for claim in &claims.claims {
-                        write!(f, "{}", indent(indent_level))?;
-                        pretty_print_claim_expr(
+                    let (cross_step_claims, single_step_claims): (Vec<&Claim<F>>, Vec<&Claim<F>>) =
+                        claims.claims.iter().partition(|c| c.is_offset);
+                    if single_step_claims.len() > 0 {
+                        let fun_name = format!(
+                            "{}.{single_step_claims_fun_name}",
+                            sumcheck_ident(&claims.output_sumcheck_id)
+                        );
+                        single_step_claims_funs.push(fun_name.clone());
+                        pretty_print_claims_fun(
                             f,
-                            claim.input_sumcheck_id,
-                            &claim.input_claim_expr,
+                            &fun_name,
+                            &sumcheck_vars_typ,
+                            &single_step_claims,
+                            &claims.output_sumcheck_id,
+                            false,
+                            indent_level,
                         )?;
-                        write!(f, " == ")?;
-                        pretty_print_claim_expr(
+                    }
+                    if cross_step_claims.len() > 0 {
+                        let fun_name = format!(
+                            "{}.{cross_step_claims_fun_name}",
+                            sumcheck_ident(&claims.output_sumcheck_id)
+                        );
+                        cross_step_claims_funs.push(fun_name.clone());
+                        pretty_print_claims_fun(
                             f,
-                            claims.output_sumcheck_id,
-                            &claim.expected_output_claim_expr,
+                            &fun_name,
+                            &sumcheck_vars_typ,
+                            &cross_step_claims,
+                            &claims.output_sumcheck_id,
+                            true,
+                            indent_level,
                         )?;
-                        writeln!(f, "")?;
                     }
                 }
                 None => (),
             }
         }
+
+        writeln!(
+            f,
+            "{}def {single_step_claims_fun_name} [Field f] (cycle : {sumcheck_vars_typ} f) : ZKBuilder f PUnit := do",
+            indent(indent_level)
+        )?;
+        indent_level += 1;
+        for fun_name in &single_step_claims_funs {
+            writeln!(f, "{}{fun_name} cycle", indent(indent_level))?;
+        }
         indent_level -= 1;
 
-        writeln!(f, "{}-/", indent(indent_level))
+        writeln!(
+            f,
+            "{}def {cross_step_claims_fun_name} [Field f] (cycle next_cycle : {sumcheck_vars_typ} f) : ZKBuilder f PUnit := do",
+            indent(indent_level)
+        )?;
+        indent_level += 1;
+        for fun_name in &cross_step_claims_funs {
+            writeln!(f, "{}{fun_name} cycle next_cycle", indent(indent_level))?;
+        }
+
+        Ok(())
     }
+}
+
+fn remove_parens(mut string: String) -> String {
+    let open_paren = Regex::new(r"\(").unwrap();
+    let close_paren = Regex::new(r"\)").unwrap();
+
+    string = open_paren
+        .replace_all(string.as_str(), NoExpand("_"))
+        .to_string();
+    string = close_paren
+        .replace_all(string.as_str(), NoExpand(""))
+        .to_string();
+
+    string
+}
+
+fn sumcheck_ident(sumcheck_id: &SumcheckId) -> String {
+    remove_parens(format!("{sumcheck_id:?}_Vars"))
+}
+
+fn committed_var_ident(var: &CommittedPolynomial) -> String {
+    remove_parens(format!("{var:?}"))
+}
+
+fn virtual_var_ident(var: &VirtualPolynomial) -> String {
+    remove_parens(format!("{var:?}"))
+}
+
+fn pretty_print_claims_fun<F: JoltField>(
+    f: &mut impl std::io::Write,
+    fun_name: &str,
+    sumcheck_vars_typ: &str,
+    claims: &[&Claim<F>],
+    output_sumcheck_id: &SumcheckId,
+    is_offset: bool,
+    mut indent_level: usize,
+) -> std::io::Result<()> {
+    let cycle_vars = if is_offset {
+        "cycle next_cycle"
+    } else {
+        "cycle"
+    };
+    writeln!(
+        f,
+        "{}def {fun_name} [Field f] ({cycle_vars} : {sumcheck_vars_typ} f) : ZKBuilder f PUnit := do",
+        indent(indent_level)
+    )?;
+    indent_level += 1;
+    for claim in claims {
+        writeln!(f, "{}ZKBuilder.constrainEq ", indent(indent_level))?;
+        indent_level += 1;
+        write!(f, "{}", indent(indent_level))?;
+        pretty_print_claim_expr(
+            f,
+            "cycle",
+            &claim.input_sumcheck_id,
+            &claim.input_claim_expr,
+            true,
+        )?;
+        writeln!(f)?;
+        write!(f, "{}", indent(indent_level))?;
+        pretty_print_claim_expr(
+            f,
+            if is_offset { "next_cycle" } else { "cycle" },
+            output_sumcheck_id,
+            &claim.expected_output_claim_expr,
+            true,
+        )?;
+        writeln!(f)?;
+        writeln!(f)?;
+        indent_level -= 1;
+    }
+
+    Ok(())
 }
 
 fn pretty_print_claim_expr<F: JoltField>(
     f: &mut impl std::io::Write,
-    sumcheck_id: SumcheckId,
+    vars_ident: &str,
+    sumcheck_id: &SumcheckId,
     expr: &ClaimExpr<F>,
+    group: bool,
 ) -> std::io::Result<()> {
     match expr {
         ClaimExpr::Val(val) => {
             write!(f, "{val}")?;
         }
         ClaimExpr::Add(e1, e2) => {
-            write!(f, "(")?;
-            pretty_print_claim_expr(f, sumcheck_id, e1)?;
+            if group {
+                write!(f, "(")?;
+            }
+            pretty_print_claim_expr(f, vars_ident, sumcheck_id, e1, false)?;
             write!(f, " + ")?;
-            pretty_print_claim_expr(f, sumcheck_id, e2)?;
-            write!(f, ")")?;
+            pretty_print_claim_expr(f, vars_ident, sumcheck_id, e2, false)?;
+            if group {
+                write!(f, ")")?;
+            }
         }
         ClaimExpr::Mul(e1, e2) => {
-            pretty_print_claim_expr(f, sumcheck_id, e1)?;
+            if group {
+                write!(f, "(")?;
+            }
+            pretty_print_claim_expr(f, vars_ident, sumcheck_id, e1, true)?;
             write!(f, " * ")?;
-            pretty_print_claim_expr(f, sumcheck_id, e2)?;
+            pretty_print_claim_expr(f, vars_ident, sumcheck_id, e2, true)?;
+            if group {
+                write!(f, ")")?;
+            }
         }
         ClaimExpr::Sub(e1, e2) => {
-            write!(f, "(")?;
-            pretty_print_claim_expr(f, sumcheck_id, e1)?;
+            if group {
+                write!(f, "(")?;
+            }
+            pretty_print_claim_expr(f, vars_ident, sumcheck_id, e1, false)?;
             write!(f, " - ")?;
-            pretty_print_claim_expr(f, sumcheck_id, e2)?;
-            write!(f, ")")?;
+            pretty_print_claim_expr(f, vars_ident, sumcheck_id, e2, true)?;
+            if group {
+                write!(f, ")")?;
+            }
         }
         ClaimExpr::CommittedVar(committed_polynomial) => {
-            let var_name = std::format!("{committed_polynomial:?}");
-            write!(f, "{sumcheck_id:?}.{var_name}")?;
+            let vars_type = sumcheck_ident(sumcheck_id);
+            let var_name = committed_var_ident(committed_polynomial);
+            write!(f, "{vars_ident}.{vars_type}.{var_name}")?;
         }
         ClaimExpr::VirtualVar(virtual_polynomial) => {
-            let var_name = std::format!("{virtual_polynomial:?}");
-            write!(f, "{sumcheck_id:?}.{var_name}")?;
+            let vars_type = sumcheck_ident(sumcheck_id);
+            let var_name = virtual_var_ident(virtual_polynomial);
+            write!(f, "{vars_ident}.{vars_type}.{var_name}")?;
         }
     }
 
@@ -206,7 +353,7 @@ impl<F: JoltField> AsModule for ZkLeanSumchecks<F> {
 
         Ok(Module {
             name: String::from("Sumchecks"),
-            imports: vec![],
+            imports: vec![String::from("ZkLean"), String::from("Jolt.R1CS")],
             contents,
         })
     }
