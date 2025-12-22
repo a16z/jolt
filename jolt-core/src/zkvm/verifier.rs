75e3300ececfe44107ebe711ff93e809c9c1ba1c
danielwlz <183745461+danielwlz@users.noreply.github.com>
2025-12-22 15:04:36 +0800

    clippy

--- jolt-core/src/zkvm/verifier.rs
+++ jolt-core/src/zkvm/verifier.rs
@@ -141,7 +141,7 @@ impl<'a, F: JoltField, PCS: CommitmentScheme<Field = F>, ProofTranscript: Transc
         proof
             .proof_config
             .validate_for_trace(proof.trace_length.log_2(), one_hot_params.ram_k.log_2())
-            .map_err(|msg| ProofVerifyError::InvalidProofConfig(msg))?;
+            .map_err(ProofVerifyError::InvalidProofConfig)?;
 
         Ok(Self {
             trusted_advice_commitment,
