7ce51e20d856777386778a1cb3eb94d8b6612c66
Zachary DeStefano <zachdestefano@gmail.com>
2026-01-08 16:01:43 -0500

    fix bug introduced in tests

--- jolt-inlines/secp256k1/src/tests.rs
+++ jolt-inlines/secp256k1/src/tests.rs
@@ -86,7 +86,7 @@ mod sequence_tests {
     #[test]
     fn test_endomorphism_consistency() {
         // get q and its endomorphism
-        let q = Secp256k1Point::from_u64_arr_unchecked(&[
+        let q = Secp256k1Point::from_u64_arr(&[
             0x0012563f32ed0216,
             0xee00716af6a73670,
             0x91fc70e34e00e6c8,
@@ -95,26 +95,29 @@ mod sequence_tests {
             0xcb1b42d72491e47f,
             0xdc7f31262e4ba2b7,
             0xdc7b004d3bb2800d,
-        ]);
+        ])
+        .unwrap();
         let endo_q = q.endomorphism();
         // check that endo_q.y = q.y
         assert_eq!(endo_q.y(), q.y());
         // check that endo_q.x = beta * q.x
-        let beta = Secp256k1Fq::from_u64_arr_unchecked(&[
+        let beta = Secp256k1Fq::from_u64_arr(&[
             0xc1396c28719501ee,
             0x9cf0497512f58995,
             0x6e64479eac3434e9,
             0x7ae96a2b657c0710,
-        ]);
+        ])
+        .unwrap();
         let expected_x = beta.mul(&q.x());
         assert_eq!(endo_q.x(), expected_x);
         // check that lambda * q == endo_q
-        let lambda = Secp256k1Fr::from_u64_arr_unchecked(&[
+        let lambda = Secp256k1Fr::from_u64_arr(&[
             0xdf02967c1b23bd72,
             0x122e22ea20816678,
             0xa5261c028812645a,
             0x5363ad4cc05c30e0,
-        ]);
+        ])
+        .unwrap();
         let endo_q_expected = fr_point_mul(&lambda, &q);
         assert_eq!(endo_q.x(), endo_q_expected.x());
         assert_eq!(endo_q.y(), endo_q_expected.y());
@@ -123,7 +126,7 @@ mod sequence_tests {
     #[test]
     fn test_glv_decomposition() {
         // get q and its endomorphism
-        let q = Secp256k1Point::from_u64_arr_unchecked(&[
+        let q = Secp256k1Point::from_u64_arr(&[
             0x0012563f32ed0216,
             0xee00716af6a73670,
             0x91fc70e34e00e6c8,
@@ -132,15 +135,17 @@ mod sequence_tests {
             0xcb1b42d72491e47f,
             0xdc7f31262e4ba2b7,
             0xdc7b004d3bb2800d,
-        ]);
+        ])
+        .unwrap();
         let endo_q = q.endomorphism();
         // and an arbitrary scalar k
-        let k = Secp256k1Fr::from_u64_arr_unchecked(&[
+        let k = Secp256k1Fr::from_u64_arr(&[
             0x1234567890ABCDEF,
             0x0FEDCBA987654321,
             0x1111111111111111,
             0x2222222222222222,
-        ]);
+        ])
+        .unwrap();
         // check that k * q == k1 * q + k2 * endo_q
         let expected = fr_point_mul(&k, &q);
         let decomp = Secp256k1Point::decompose_scalar(&k);
@@ -159,25 +164,28 @@ mod sequence_tests {
 
     #[test]
     fn test_ecdsa_verify() {
-        let z = Secp256k1Fr::from_u64_arr_unchecked(&[
+        let z = Secp256k1Fr::from_u64_arr(&[
             0x9088f7ace2efcde9,
             0xc484efe37a5380ee,
             0xa52e52d7da7dabfa,
             0xb94d27b9934d3e08,
-        ]);
-        let r = Secp256k1Fr::from_u64_arr_unchecked(&[
+        ])
+        .unwrap();
+        let r = Secp256k1Fr::from_u64_arr(&[
             0xb8fc413b4b967ed8,
             0x248d4b0b2829ab00,
             0x587f69296af3cd88,
             0x3a5d6a386e6cf7c0,
-        ]);
-        let s = Secp256k1Fr::from_u64_arr_unchecked(&[
+        ])
+        .unwrap();
+        let s = Secp256k1Fr::from_u64_arr(&[
             0x66a82f274e3dcafc,
             0x299a02486be40321,
             0x6212d714118f617e,
             0x9d452f63cf91018d,
-        ]);
-        let q = Secp256k1Point::from_u64_arr_unchecked(&[
+        ])
+        .unwrap();
+        let q = Secp256k1Point::from_u64_arr(&[
             0x0012563f32ed0216,
             0xee00716af6a73670,
             0x91fc70e34e00e6c8,
@@ -186,7 +194,8 @@ mod sequence_tests {
             0xcb1b42d72491e47f,
             0xdc7f31262e4ba2b7,
             0xdc7b004d3bb2800d,
-        ]);
+        ])
+        .unwrap();
         assert!(crate::sdk::ecdsa_verify(z, r, s, q).is_ok());
     }
 }
