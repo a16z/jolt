1a528d2b5aa968223735212ab754eb4d0b814e80
Zachary DeStefano <zachdestefano@gmail.com>
2026-01-22 15:36:48 -0500

    mild cleanup

--- examples/secp256k1-ecdsa-verify/guest/src/lib.rs
+++ examples/secp256k1-ecdsa-verify/guest/src/lib.rs
@@ -5,7 +5,7 @@ use jolt_inlines_secp256k1::{ecdsa_verify, Secp256k1Error, Secp256k1Fr, Secp256k
 
 // verifies an secp256k1 ECDSA signature
 // given message hash z, signature (r, s), and public key Q
-// all inputs are little-endian u64 arrays in normal form
+// all inputs are little-endian u64 arrays in normal form, validated by the ecdsa_verify function
 // returns Ok(()) if signature is valid, Err(SignatureError) otherwise
 #[jolt::provable(memory_size = 100000, max_trace_length = 400000)]
 fn secp256k1_ecdsa_verify(
@@ -14,21 +14,9 @@ fn secp256k1_ecdsa_verify(
     s: [u64; 4],
     q: [u64; 8],
 ) -> Result<(), Secp256k1Error> {
-    start_cycle_tracking("convert inputs");
     let z = Secp256k1Fr::from_u64_arr(&z)?;
     let r = Secp256k1Fr::from_u64_arr(&r)?;
     let s = Secp256k1Fr::from_u64_arr(&s)?;
     let q = Secp256k1Point::from_u64_arr(&q)?;
-    end_cycle_tracking("convert inputs");
-    /*let mut x = q.x();
-    let mut y = q.y();
-    for _ in 0..300 {
-        //q = q.double_and_add(&q);
-        x = x.mul(&y);
-    }
-    if x.is_zero() {
-        return Err(Secp256k1Error::RxMismatch); // dummy error for now, just to get cycle counts
-    }*/
-    //Ok(())
     ecdsa_verify(z, r, s, q)
 }
