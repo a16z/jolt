5c6790d7066e3f265be69ecd434d9d2290d10665
Sagar Dhawan <sdhawan@a16z.com>
2026-02-02 10:34:49 -0600

    rebase

--- jolt-core/src/zkvm/mod.rs
+++ jolt-core/src/zkvm/mod.rs
@@ -123,14 +123,14 @@ pub fn fiat_shamir_preamble(
     trace_length: usize,
     transcript: &mut impl Transcript,
 ) {
-    transcript.append_u64(program_io.memory_layout.max_input_size);
-    transcript.append_u64(program_io.memory_layout.max_output_size);
-    transcript.append_u64(program_io.memory_layout.heap_size);
-    transcript.append_bytes(&program_io.inputs);
-    transcript.append_bytes(&program_io.outputs);
-    transcript.append_u64(program_io.panic as u64);
-    transcript.append_u64(ram_K as u64);
-    transcript.append_u64(trace_length as u64);
+    transcript.append_u64(b"max_input_size", program_io.memory_layout.max_input_size);
+    transcript.append_u64(b"max_output_size", program_io.memory_layout.max_output_size);
+    transcript.append_u64(b"heap_size", program_io.memory_layout.heap_size);
+    transcript.append_bytes(b"inputs", &program_io.inputs);
+    transcript.append_bytes(b"outputs", &program_io.outputs);
+    transcript.append_u64(b"panic", program_io.panic as u64);
+    transcript.append_u64(b"ram_K", ram_K as u64);
+    transcript.append_u64(b"trace_length", trace_length as u64);
 }
 
 #[cfg(feature = "prover")]
