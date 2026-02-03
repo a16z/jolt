d33b117274d760c3174a5020a71c1be4b1c5c348
Ben Hamlin <hamlinb@galois.com>
2026-02-03 12:10:29 -0800

    Bump zklean version to use new generic lookup_mle

--- zklean-extractor/package-template/Jolt/Util.lean
+++ zklean-extractor/package-template/Jolt/Util.lean
@@ -1,12 +1,5 @@
 import ZKLean
 
--- XXX We keep this here for now because zklean does not yet handle 64-bit MLEs in lookup_mle
-def lookup_mle_64bit [ZKField f] (tbl : LookupTableMLE f 128) (left right : ZKExpr f) : ZKBuilder f (ZKExpr f) :=
-  let res := evalLookupTableMLE tbl
-    (ZKField.field_to_bits (num_bits := 64) left.eval)
-    (ZKField.field_to_bits (num_bits := 64) right.eval)
-  pure (ZKExpr.Field res)
-
 def mux [ZKField f]
   (cases : Array (ZKExpr f × ZKExpr f))
   : ZKBuilder f (ZKExpr f) :=
@@ -20,8 +13,8 @@ def mux_mles [ZKField f]
   (mle_cases : Array (ZKExpr f × ((Vector f 128) -> f)))
   : ZKBuilder f (ZKExpr f) := do
   let cases : Array (ZKExpr f × ZKExpr f) <- mle_cases.foldl (fun acc ((flag, table) : (ZKExpr f × (Vector f 128 -> f))) => do
-    let mle := LookupTableMLE.mk interleaving table
-    let term <- lookup_mle_64bit mle left right
+    let mle : LookupTableMLE f (64 + 64) := .mk interleaving table
+    let term <- ZKBuilder.lookup_mle mle left right
     let a <- acc
     pure (a ++ #[(flag, term)])) (pure #[] : ZKBuilder f (Array (ZKExpr f × ZKExpr f)))
   mux cases
