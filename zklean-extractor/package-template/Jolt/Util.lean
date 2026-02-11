91e762f59e47d9b3debb09ed4a61d90cbc38f8d7
Ben Hamlin <hamlinb@galois.com>
2026-02-11 10:10:07 -0800

    Use mapM instead of foldl to make Lean mux_mles function more concise

--- zklean-extractor/package-template/Jolt/Util.lean
+++ zklean-extractor/package-template/Jolt/Util.lean
@@ -10,9 +10,8 @@ def mux_mles [ZKField f]
   (left right : ZKExpr f)
   (mle_cases : Array (ZKExpr f × ((Vector f 128) -> f)))
   : ZKBuilder f (ZKExpr f) := do
-  let cases : Array (ZKExpr f × ZKExpr f) <- mle_cases.foldl (fun acc ((flag, table) : (ZKExpr f × (Vector f 128 -> f))) => do
+  let cases <- mle_cases.mapM (fun (flag, table) => do
     let mle : LookupTableMLE f (64 + 64) := .mk interleaving table
     let term <- ZKBuilder.lookup_mle mle left right
-    let a <- acc
-    pure (a ++ #[(flag, term)])) (pure #[] : ZKBuilder f (Array (ZKExpr f × ZKExpr f)))
+    pure (flag, term))
   pure (mux cases)
