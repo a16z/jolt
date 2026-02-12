dc94f8d68d6c940c5f9507fad37c490c41a681e8
markosg04 <mgeorghiades@a16z.com>
2026-02-12 14:25:42 -0500

    feat: blanket impls for transript crate

--- crates/jolt-transcript/src/blanket.rs
+++ crates/jolt-transcript/src/blanket.rs
@@ -3,14 +3,12 @@
 use crate::transcript::{AppendToTranscript, Transcript};
 
 impl AppendToTranscript for [u8] {
-    #[inline]
     fn append_to_transcript<T: Transcript>(&self, transcript: &mut T) {
         transcript.append_bytes(self);
     }
 }
 
 impl<const N: usize> AppendToTranscript for [u8; N] {
-    #[inline]
     fn append_to_transcript<T: Transcript>(&self, transcript: &mut T) {
         transcript.append_bytes(self);
     }
@@ -18,7 +16,6 @@ impl<const N: usize> AppendToTranscript for [u8; N] {
 
 /// Absorbs as 8 big-endian bytes.
 impl AppendToTranscript for u64 {
-    #[inline]
     fn append_to_transcript<T: Transcript>(&self, transcript: &mut T) {
         transcript.append_bytes(&self.to_be_bytes());
     }
@@ -26,7 +23,6 @@ impl AppendToTranscript for u64 {
 
 /// Absorbs as 16 big-endian bytes.
 impl AppendToTranscript for u128 {
-    #[inline]
     fn append_to_transcript<T: Transcript>(&self, transcript: &mut T) {
         transcript.append_bytes(&self.to_be_bytes());
     }
@@ -34,7 +30,6 @@ impl AppendToTranscript for u128 {
 
 /// Absorbs as 8 big-endian bytes (cast to `u64`).
 impl AppendToTranscript for usize {
-    #[inline]
     fn append_to_transcript<T: Transcript>(&self, transcript: &mut T) {
         transcript.append_bytes(&(*self as u64).to_be_bytes());
     }
