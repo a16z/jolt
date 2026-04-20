e88e5e1a325ed4fe94ed43c690398c41a01bf2f1
Michael Zhu <mchl.zhu.96@gmail.com>
2026-04-20 11:16:29 -0400

    Make get_frobenius_coefficients a const fn

--- crates/jolt-crypto/src/ec/bn254/glv/constants.rs
+++ crates/jolt-crypto/src/ec/bn254/glv/constants.rs
@@ -20,7 +20,7 @@ pub struct FrobeniusCoefficients {
     pub psi3_coef3: Fq2,
 }
 
-pub fn get_frobenius_coefficients() -> FrobeniusCoefficients {
+pub const fn get_frobenius_coefficients() -> FrobeniusCoefficients {
     FrobeniusCoefficients {
         psi1_coef2: Fq2::new(
             MontFp!(
