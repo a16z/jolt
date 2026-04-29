4f804a50912ae30f39ebd34d85c7e1a5c7b38703
Ari <software@randomwalks.xyz>
2026-04-29 09:55:25 +0100

    Fix: Completeness issue in edge case identified from FV.

--- tracer/src/instruction/divw.rs
+++ tracer/src/instruction/divw.rs
@@ -142,7 +142,7 @@ impl RISCVTrace for DIVW {
         asm.emit_i::<VirtualSignExtendWord>(*t1, *a2, 0); // Sign-extend quotient to 64-bit
         asm.emit_b::<VirtualAssertEQ>(*t1, *a2, 0); // Assert quotient was valid 32-bit
 
-        asm.emit_i::<SRAI>(*t2, *a3, 31); // Sign bit of remainder
+        asm.emit_i::<SRAI>(*t2, *a3, 32); // Remainder advice fits in u32
         asm.emit_b::<VirtualAssertEQ>(*t2, 0, 0); // sign bit of remainder advice should be 0
 
         asm.emit_i::<SRAI>(*t2, *t4, 31); // Sign bit of dividend
